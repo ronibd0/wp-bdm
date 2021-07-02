3242bf00de42dbacfd2326baa6c576b59f7cd5a4
imnavanath <navanathb@bsf.io>
2021-07-02 18:17:11 +0530

    refactor: Added responsive or single colors class support for adding compat CSS

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '848672f841a24cc170264782596c38a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'a08c5110beb55360e43cb3aced02a21f');
