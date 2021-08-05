49fa61b328e577c4a7c18fde61f456e9cad874ea
ShubhamW <shubhamw@bsf.io>
2021-08-05 19:15:26 +0530

    fix: font value 0 not working on frontend

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -151,7 +151,7 @@ if ( ! function_exists( 'astra_get_font_css_value' ) ) {
 	 */
 	function astra_get_font_css_value( $value, $unit = 'px', $device = 'desktop' ) {
 
-		// If value is empty or 0 then return blank.
+		// If value is empty then return blank.
 		if ( '' == $value ) {
 			return '';
 		}
