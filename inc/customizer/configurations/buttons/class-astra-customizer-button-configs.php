3b0a4a93856c8297a75a2fb53341c339938d0695
patilvikasj <patilvikasj@gmail.com>
2021-08-10 17:30:34 +0530

    Fix: border radius not working

--- inc/customizer/configurations/buttons/class-astra-customizer-button-configs.php
+++ inc/customizer/configurations/buttons/class-astra-customizer-button-configs.php
@@ -425,7 +425,6 @@ if ( ! class_exists( 'Astra_Customizer_Button_Configs' ) ) {
 					'default'     => astra_get_option( 'button-radius' ),
 					'type'        => 'control',
 					'section'     => 'section-buttons',
-					'transport'   => 'postMessage',
 					'control'     => 'ast-slider',
 					'priority'    => 19,
 					'suffix'      => 'px',
