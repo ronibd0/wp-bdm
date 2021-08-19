235b2ec5f9e1c1f894f490915c54d67b022585c5
imnavanath <navanathb@bsf.io>
2021-08-19 13:40:12 +0530

    chore: build grunt

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'efae51311e5f9c0da65bcb59fa9d66e1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '4856ef1c5f072258d50ee847152b1c5e');
