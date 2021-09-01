d67a664c86802fc60a9c8a8365726da3094b7bb1
premanshup <premanshup@bsf.io>
2021-09-01 17:56:15 +0530

    Implement theme.json CSS variables for Global Color Palette and remove 'editor-color-palette' theme support.

--- inc/class-astra-global-palette.php
+++ inc/class-astra-global-palette.php
@@ -24,7 +24,6 @@ class Astra_Global_Palette {
 	 * @since x.x.x
 	 */
 	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'support_editor_color_palette' ) );
 		add_filter( 'astra_theme_customizer_js_localize', array( $this, 'localize_variables' ) );
 		add_filter( 'astra_before_foreground_color_generation', array( $this, 'get_color_by_palette_variable' ) );
 		$this->includes();
@@ -173,54 +172,6 @@ class Astra_Global_Palette {
 		}
 
 		return $palette_style;
-	}
-
-	/**
-	 * Modify color palette from Gutenberg.
-	 *
-	 * @since x.x.x
-	 * @return void
-	 */
-	public function support_editor_color_palette() {
-		$global_palette = astra_get_option( 'global-color-palette' );
-		$editor_palette = $this->format_global_palette( $global_palette );
-
-		// Editor Color Palette.
-		add_theme_support( 'editor-color-palette', $editor_palette );
-	}
-
-	/**
-	 * Format color palette data required to pass for Gutenberg palette.
-	 *
-	 * @since x.x.x
-	 * @param array $global_palette global palette data.
-	 * @return array
-	 */
-	public function format_global_palette( $global_palette ) {
-		$editor_palette    = array();
-		$extra_color_index = 1;
-		$color_index       = 0;
-
-		if ( isset( $global_palette['palette'] ) ) {
-			foreach ( $global_palette['palette'] as $key => $color ) {
-
-				if ( isset( $global_palette['labels'][ $color_index ] ) ) {
-					$label = $global_palette['labels'][ $color_index ];
-				} else {
-					$label = __( 'Extra Color', 'astra' ) . $extra_color_index;
-					$extra_color_index++;
-				}
-
-				$editor_palette[] = array(
-					'name'  => $label,
-					'slug'  => str_replace( '--', '', self::get_css_variable_prefix() ) . $key,
-					'color' => 'var(' . self::get_css_variable_prefix() . $key . ')',
-				);
-				$color_index++;
-			}
-		}
-
-		return $editor_palette;
 	}
 
 	/**
