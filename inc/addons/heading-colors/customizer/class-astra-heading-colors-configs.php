346cf30b3b15fc13f31a3aa3b666aad1e9ba0637
patilvikasj <patilvikasj@gmail.com>
2021-09-03 19:23:25 +0530

    Class exists check before calling class method

--- inc/addons/heading-colors/customizer/class-astra-heading-colors-configs.php
+++ inc/addons/heading-colors/customizer/class-astra-heading-colors-configs.php
@@ -30,7 +30,7 @@ if ( ! class_exists( 'Astra_Heading_Colors_Configs' ) ) {
 		 */
 		public function register_configuration( $configurations, $wp_customize ) {
 
-			$_section = ( defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'colors-and-background' ) && ! astra_is_content_bg_option_to_load() ) ? 'section-colors-body' : 'section-colors-background';
+			$_section = ( class_exists( 'Astra_Ext_Extension' ) && Astra_Ext_Extension::is_active( 'colors-and-background' ) && ! astra_is_content_bg_option_to_load() ) ? 'section-colors-body' : 'section-colors-background';
 
 			$_configs = array(
 
