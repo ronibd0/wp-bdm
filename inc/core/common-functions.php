7f60dc322b1c692f139b7102488fc5f1c0255a62
premanshup <premanshup@bsf.io>
2021-09-07 12:31:14 +0530

    psalm error resolved

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -1544,9 +1544,9 @@ function astra_zero_font_size_case() {
  *
  * @since  2.5.4
  * @param string $version   WordPress version to compare with the current version.
- * @param string $compare   Comparison value i.e > or < etc.
+ * @param mixed $compare   Comparison value i.e > or < etc.
  * @return bool|null            True/False based on the  $version and $compare value.
  */
-function astra_wp_version_compare( $version, $compare = '' ) {
+function astra_wp_version_compare( $version, $compare ) {
 	return version_compare( get_bloginfo( 'version' ), $version, $compare );
 }
