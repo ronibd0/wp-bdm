73106164c7d9b2a73170fbe5479e895db1e271a7
Nikhil <email@nikhilchavan.com>
2021-09-16 16:25:45 +0530

    Indent using tabs

--- tests/e2e/e2e-plugin/customizer-settings/namespace.php
+++ tests/e2e/e2e-plugin/customizer-settings/namespace.php
@@ -3,20 +3,20 @@
 namespace Astra\E2E\Customizer_Settings;
 
 function bootstrap() {
-    add_action(
-        'init',
-        function () {
-            if ( is_customize_preview() ) {
-                add_action( 'wp_footer', __NAMESPACE__ . '\\dump_customizer_settings' );
-            }
-        }
-    );
+	add_action(
+		'init',
+		function () {
+			if ( is_customize_preview() ) {
+				add_action( 'wp_footer', __NAMESPACE__ . '\\dump_customizer_settings' );
+			}
+		}
+	);
 }
 
 function dump_customizer_settings() {
-    $customizer_settings = get_option( 'astra-settings', array() );
+	$customizer_settings = get_option( 'astra-settings', array() );
 
-    echo '<pre>';
-    echo wp_json_encode( $customizer_settings, JSON_PRETTY_PRINT );
-    echo '</pre>';
+	echo '<pre>';
+	echo wp_json_encode( $customizer_settings, JSON_PRETTY_PRINT );
+	echo '</pre>';
 }
