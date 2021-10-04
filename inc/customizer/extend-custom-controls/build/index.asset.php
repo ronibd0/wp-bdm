51a96b34a30f18434ec57e7cafe35921727b3c12
Vikas Patil <vikasp@bsf.io>
2021-10-04 12:10:55 +0530

    AST-959 - Set H1-H6 typography options on click of Typography Presets (#3368)
    * WIP: h1-h6 typography set on Preset change
    
    * Set h1-h6 Font options on click of Typography Presets
    
    * chore: changelog addded
    
    Co-authored-by: imnavanath <navanathb@bsf.io>
    Co-authored-by: premanshup <premanshup@bsf.io>

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '92795d0913206dc7b3ed1bb24eb3e6ac');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '742f4d456b5bdddb153df5f77484da03');
