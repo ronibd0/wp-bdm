6947c1cd2f1e47abbe4238cc805cb4a93c64ba0a
Simran <simranc@bsf.io>
2021-10-09 10:30:10 +0530

    Test case for Single post section background color

---
+++ tests/e2e/specs/customizer/Blog-single-post/section-background-color.test.js
@@ -0,0 +1,39 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( 'Section baclground color option under the customizer', () => {
+	it( 'section background color option should apply correctly', async () => {
+		const backgroundcolor = {
+			'enable-related-posts': 'true',
+			'related-posts-background-color': 'rgb(255, 255, 2)',
+		};
+		await setCustomize( backgroundcolor );
+		await createNewPost( {
+			postType: 'post',
+			title: 'sample-post',
+
+		} );
+		await publishPost();
+
+		await createNewPost( {
+			postType: 'post',
+			title: 'test-post',
+
+		} );
+		await publishPost();
+
+		await page.goto( createURL( 'test-post' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.evaluate( () => {
+			window.scrollBy( 0, window.innerHeight );
+		} );
+
+		await page.waitForSelector( '.ast-single-related-posts-container' );
+		await expect( {
+			selector: '.ast-single-related-posts-container',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundcolor[ 'related-posts-background-color' ] }`,
+		);
+	} );
+} );
