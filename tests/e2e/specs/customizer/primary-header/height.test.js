5865344b56361c547f594d6feadda07f9fe7dbf5
Simran <simranc@bsf.io>
2021-10-10 16:48:16 +0530

    Test case for Primary header height

---
+++ tests/e2e/specs/customizer/primary-header/height.test.js
@@ -0,0 +1,39 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( 'Primary header height setting in customizer', () => {
+	it( 'height should apply correctly', async () => {
+		const primaryheaderHeight = {
+			'hb-header-height': {
+				desktop: 90,
+				tablet: 90,
+				mobile: 90,
+				'desktop-unit': 'px',
+				'tablet-unit': 'px',
+				'mobile-unit': 'px',
+			},
+
+		};
+		await setCustomize( primaryheaderHeight );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-primary-header-bar .site-primary-header-wrap' );
+		await expect( {
+			selector: '.ast-primary-header-bar .site-primary-header-wrap',
+			property: 'min-height',
+		} ).cssValueToBe( `${ primaryheaderHeight[ 'hb-header-height' ].desktop }${ primaryheaderHeight[ 'hb-header-height' ][ 'desktop-unit' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-primary-header-bar .site-primary-header-wrap',
+			property: 'min-height',
+		} ).cssValueToBe( `${ primaryheaderHeight[ 'hb-header-height' ].tablet }${ primaryheaderHeight[ 'hb-header-height' ][ 'tablet-unit' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-primary-header-bar .site-primary-header-wrap',
+			property: 'min-height',
+		} ).cssValueToBe( `${ primaryheaderHeight[ 'hb-header-height' ].mobile }${ primaryheaderHeight[ 'hb-header-height' ][ 'mobile-unit' ] }`,
+		);
+	} );
+} );
