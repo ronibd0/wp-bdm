fb040d3f0223ca056680ae8250db25c79da0df38
akshaylakh <68679840+akshaylakh@users.noreply.github.com>
2021-10-12 12:46:00 +0530

    AST-918 - Typography & color test for before title breadcrumb position (#3133)
    * Typography & color test for before title breadcrumb position
    
    * renamed file
    
    * fixed linting errors
    
    * fixed mix sapace error
    
    * deleted unused files
    
    * line-height error changes
    
    * breadcrumb position fix
    
    * updated selectors
    
    * updated selectors
    
    Co-authored-by: Premanshu Pandey <36841355+premanshup@users.noreply.github.com>

---
+++ tests/e2e/specs/customizer/breadcrumbs/before-title-breadcrumb.js
@@ -0,0 +1,69 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/set-customize';
+describe( 'breadcrumb typography settings in the customizer', () => {
+	it( 'breadcrumb typography should apply corectly', async () => {
+		const beforebreadcrumbFont = {
+			'breadcrumb-position': 'astra_entry_top',
+			'breadcrumb-font-family': 'Pacifico, handwriting',
+			'breadcrumb-font-weight': '800',
+			'breadcrumb-text-transform': 'lowercase',
+			'breadcrumb-font-size': {
+				desktop: 50,
+				tablet: 22,
+				mobile: 18,
+				'desktop-unit': 'px',
+				'tablet-unit': 'px',
+				'mobile-unit': 'px',
+			},
+			'breadcrumb-line-height': '70px',
+			'breadcrumb-active-color-responsive': {
+				desktop: 'rgb(255, 77, 0)',
+				tablet: 'rgb(0, 11, 255)',
+				mobile: 'rgb(7, 140, 0)',
+			},
+		};
+		await setCustomize( beforebreadcrumbFont );
+		await createNewPost( {
+			postType: 'page',
+			title: 'breadcrumb',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/breadcrumb' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-breadcrumbs-wrapper .breadcrumbs' );
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper, .ast-breadcrumbs-wrapper a',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ beforebreadcrumbFont[ 'breadcrumb-font-size' ].desktop }${ beforebreadcrumbFont[ 'breadcrumb-font-size' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper',
+			property: 'font-weight',
+		} ).cssValueToBe(
+			`${ beforebreadcrumbFont[ 'breadcrumb-font-weight' ] }`,
+		);
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper',
+			property: 'line-height',
+		} ).cssValueToBe(
+			`${ beforebreadcrumbFont[ 'breadcrumb-line-height' ] }`,
+		);
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper',
+			property: 'text-transform',
+		} ).cssValueToBe(
+			`${ beforebreadcrumbFont[ 'breadcrumb-text-transform' ] }`,
+		);
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper ',
+			property: 'font-family',
+		} ).cssValueToBe(
+			`${ beforebreadcrumbFont[ 'breadcrumb-font-family' ] }` );
+		await expect( {
+			selector: '.ast-breadcrumbs-wrapper .trail-items .trail-end',
+			property: 'color',
+		} ).cssValueToBe( `${ beforebreadcrumbFont[ 'breadcrumb-active-color-responsive' ].desktop }` );
+	} );
+} );
