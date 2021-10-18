2362c4c254aa8180d848e799add91c3a6734a7e6
Simran <simranc@bsf.io>
2021-10-18 14:39:58 +0530

    Test case for Single post title option added responsive but getting expected and received value difference error in tablet and mobile case

---
+++ tests/e2e/specs/customizer/blog-single-post/post-title-font.test.js
@@ -0,0 +1,93 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+import { responsiveFontSize } from '../../../utils/responsive-utils';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+describe( 'Post title font option under the customizer', () => {
+	it( 'post title font option should apply correctly', async () => {
+		const posttitlefont = {
+			'enable-related-posts': true,
+			'related-posts-title-font-family': 'Abel',
+			'related-posts-title-text-transform': 'uppercase',
+			'related-posts-title-font-weight': '400',
+			'related-posts-title-font-size': {
+				desktop: 72,
+				tablet: 42,
+				mobile: 32,
+				'desktop-unit': 'px',
+				'tablet-unit': 'px',
+				'mobile-unit': 'px',
+			},
+
+		};
+		await setCustomize( posttitlefont );
+		await createNewPost( {
+			postType: 'post',
+			title: 'sample-post',
+
+		} );
+		await publishPost();
+
+		await createNewPost( {
+			postType: 'post',
+			title: 'test-post',
+
+		} );
+		await publishPost();
+
+		await page.goto( createURL( 'sample-post' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( ' .ast-separate-container .ast-single-related-posts-container ' );
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
+			property: 'font-family',
+		} ).cssValueToBe( `${ posttitlefont[ 'related-posts-title-font-family' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
+			property: 'text-transform',
+		} ).cssValueToBe( `${ posttitlefont[ 'related-posts-title-text-transform' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
+			property: 'font-weight',
+		} ).cssValueToBe( `${ posttitlefont[ 'related-posts-title-font-weight' ] }`,
+		);
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ posttitlefont[ 'related-posts-title-font-size' ].desktop }${ posttitlefont[ 'related-posts-title-font-size' ][ 'desktop-unit' ] }`,
+		);
+
+		await setBrowserViewport( 'medium' );
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a ',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				posttitlefont[ 'related-posts-title-font-size' ].tablet,
+			) }${
+				posttitlefont[ 'related-posts-title-font-size' ][ 'tablet-unit' ]
+			}`,
+		);
+
+		await setBrowserViewport( 'small' );
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a ',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ await responsiveFontSize(
+				posttitlefont[ 'related-posts-title-font-size' ].mobile,
+			) }${
+				posttitlefont[ 'related-posts-title-font-size' ][ 'mobile-unit' ]
+			}`,
+		);
+	} );
+} );
