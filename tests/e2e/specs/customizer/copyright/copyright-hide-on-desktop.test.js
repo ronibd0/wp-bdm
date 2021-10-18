7281dac8f25641e876fec9d87fed1ee8d62e9394
Simran <simranc@bsf.io>
2021-10-18 17:37:59 +0530

    Added responsive but getting error

--- tests/e2e/specs/customizer/copyright/copyright-hide-on-desktop.test.js
+++ tests/e2e/specs/customizer/copyright/copyright-hide-on-desktop.test.js
@@ -1,5 +1,7 @@
-import { createURL, setBrowserViewport } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/set-customize';
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+import { responsiveFontSize } from '../../../utils/responsive-utils';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 describe( 'copyright hide on desktop settings in the customizer', () => {
 	it( 'copyright hide on desktop setting should apply correctly', async () => {
 		const copyrighthideonDesktop = {
@@ -15,11 +17,8 @@ describe( 'copyright hide on desktop settings in the customizer', () => {
 		await page.waitForSelector(
 			'.site-below-footer-wrap[data-section="section-below-footer-builder',
 		);
-
-		await setBrowserViewport( 'large' );
 		await expect( {
-			selector:
-				'.site-below-footer-wrap[data-section="section-below-footer-builder',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder',
 			property: 'display',
 		} ).cssValueToBe(
 			`${ copyrighthideonDesktop[ 'section-footer-copyright-hide-desktop' ] }`,
@@ -44,11 +43,14 @@ describe( 'copyright hide on desktop settings in the customizer', () => {
 
 		await setBrowserViewport( 'medium' );
 		await expect( {
-			selector:
-				'.site-below-footer-wrap[data-section="section-below-footer-builder',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder',
 			property: 'display',
 		} ).cssValueToBe(
-			`${ copyrighthideonTablet[ 'section-footer-copyright-hide-tablet' ] }`,
+			`${ await responsiveFontSize(
+				copyrighthideonTablet[ 'section-footer-copyright-hide-tablet' ].tablet,
+			) }${
+				copyrighthideonTablet[ 'section-footer-copyright-hide-tablet' ][ 'tablet-unit' ]
+			}`,
 		);
 	} );
 
@@ -70,11 +72,14 @@ describe( 'copyright hide on desktop settings in the customizer', () => {
 
 		await setBrowserViewport( 'small' );
 		await expect( {
-			selector:
-				'.site-below-footer-wrap[data-section="section-below-footer-builder',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder',
 			property: 'display',
 		} ).cssValueToBe(
-			`${ copyrighthideonMobile[ 'section-footer-copyright-hide-mobile' ] }`,
+			`${ await responsiveFontSize(
+				copyrighthideonMobile[ 'section-footer-copyright-hide-mobile' ].mobile,
+			) }${
+				copyrighthideonMobile[ 'section-footer-copyright-hide-mobile' ][ 'mobile-unit' ]
+			}`,
 		);
 	} );
 } );
