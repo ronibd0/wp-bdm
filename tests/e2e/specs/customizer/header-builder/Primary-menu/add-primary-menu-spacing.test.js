6ff9c915d475233481cfde3335f2e3a9e9eef5f6
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-19 17:36:16 +0530

    added responsive test cases for menu spacing

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-spacing.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-spacing.test.js
@@ -1,86 +1,78 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost, publishPost, setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Add menu spacing left', () => {
-	it( 'menu spacing should be added properly on left', async () => {
-		const menuSpacing = {
+describe( 'Add primary menu spacing for desktop view', () => {
+	it( 'primary menu spacing should be added properly in desktop view', async () => {
+		const menuPadding = {
+			'header-menu1-menu-spacing': '20px',
+		};
+		await setCustomize( menuPadding );
+		await createNewPost( {
+			postType: 'page',
+			title: 'Test Page - desktop',
+			content: 'This is simple test page for desktop view',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'large' );
+		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+		await expect( {
+			selector: '.menu-link',
+			property: 'padding',
+		} ).cssValueToBe(
+			`${ menuPadding[ 'header-menu1-menu-spacing' ] }`,
+		);
+	} );
+} );
+describe( 'Add primary menu spacing for tablet view', () => {
+	it( 'primary menu spacing should be added properly in tablet view', async () => {
+		const menuPadding = {
 			'header-menu1-menu-spacing': '30px',
 		};
-		await setCustomize( menuSpacing );
+		await setCustomize( menuPadding );
 		await createNewPost( {
 			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Sample Page',
-			content: 'This is Sample test page',
+			title: 'Test Page - tablet',
+			content: 'This is simple test page for tablet view',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
+		await setBrowserViewport( 'medium' );
 		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
 		await expect( {
 			selector: '.menu-link',
-			property: 'padding-left',
+			property: 'padding',
 		} ).cssValueToBe(
-			`${ menuSpacing[ 'header-menu1-menu-spacing' ] }`,
+			`${ menuPadding[ 'header-menu1-menu-spacing' ] }`,
 		);
 	} );
 } );
-describe( 'Add primary menu spacing right', () => {
-	it( 'primary menu spacing should be added properly on right', async () => {
-		const menuSpacing = {
-			'header-menu1-menu-spacing': '10px',
-		};
-		await setCustomize( menuSpacing );
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '.menu-link',
-			property: 'padding-right',
-		} ).cssValueToBe(
-			`${ menuSpacing[ 'header-menu1-menu-spacing' ] }`,
-		);
-	} );
-} );
-describe( 'Add menu spacing top', () => {
-	it( 'menu spacing should be added properly on top', async () => {
-		const menuSpacing = {
+describe( 'Add primary menu spacing for mobile view', () => {
+	it( 'primary menu spacing should be added properly in mobile view', async () => {
+		const menuPadding = {
 			'header-menu1-menu-spacing': '40px',
 		};
-		await setCustomize( menuSpacing );
+		await setCustomize( menuPadding );
+		await createNewPost( {
+			postType: 'page',
+			title: 'Test Page - mobile',
+			content: 'This is simple test page for mobile view',
+		} );
+		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item a' );
 		await expect( {
 			selector: '.menu-link',
-			property: 'padding-top',
+			property: 'padding',
 		} ).cssValueToBe(
-			`${ menuSpacing[ 'header-menu1-menu-spacing' ] }`,
-		);
-	} );
-} );
-describe( 'Add menu spacing bottom', () => {
-	it( 'menu spacing should be added properly on bottom', async () => {
-		const menuSpacing = {
-			'header-menu1-menu-spacing': '50px',
-		};
-		await setCustomize( menuSpacing );
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '.menu-link',
-			property: 'padding-bottom',
-		} ).cssValueToBe(
-			`${ menuSpacing[ 'header-menu1-menu-spacing' ] }`,
+			`${ menuPadding[ 'header-menu1-menu-spacing' ] }`,
 		);
 	} );
 } );
