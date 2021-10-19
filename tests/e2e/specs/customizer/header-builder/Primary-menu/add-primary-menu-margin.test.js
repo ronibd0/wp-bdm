8b195316b5330a08e9d5745250972b9668c6cb6c
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-19 16:40:50 +0530

    modified test cases to add hamburger menu button click event

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-margin.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-margin.test.js
@@ -1,15 +1,15 @@
 import { createURL, createNewPost, publishPost, setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Add primary menu margin-left for desktop view', () => {
-	it( 'primary menu margin-left should be added properly in desktop view', async () => {
+describe( 'Add primary menu margin for desktop view', () => {
+	it( 'primary menu margin should be added properly in desktop view', async () => {
 		const menuMargin = {
 			'section-hb-menu-1-margin': '20px',
 		};
 		await setCustomize( menuMargin );
 		await createNewPost( {
 			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
+			title: 'Test Page - desktop',
+			content: 'This is simple test page for desktop view',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
@@ -19,146 +19,58 @@ describe( 'Add primary menu margin-left for desktop view', () => {
 		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
 		await expect( {
 			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-left',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'medium' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-left',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'small' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-left',
+			property: 'margin',
 		} ).cssValueToBe(
 			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
 		);
 	} );
 } );
-describe( 'Add primary menu margin-right for desktop view', () => {
-	it( 'primary menu margin-right should be added properly in desktop view', async () => {
+describe( 'Add primary menu margin for tablet view', () => {
+	it( 'primary menu margin should be added properly in tablet view', async () => {
 		const menuMargin = {
-			'section-hb-menu-1-margin': '20px',
+			'section-hb-menu-1-margin': '30px',
 		};
 		await setCustomize( menuMargin );
 		await createNewPost( {
 			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
+			title: 'Test Page - tablet',
+			content: 'This is simple test page for tablet view',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await setBrowserViewport( 'large' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-right',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
 		await setBrowserViewport( 'medium' );
 		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
 		await expect( {
 			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-right',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'small' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-right',
+			property: 'margin',
 		} ).cssValueToBe(
 			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
 		);
 	} );
 } );
-describe( 'Add primary menu margin-top for desktop view', () => {
-	it( 'primary menu margin-top should be added properly in desktop view', async () => {
+describe( 'Add primary menu margin for mobile view', () => {
+	it( 'primary menu margin should be added properly in mobile view', async () => {
 		const menuMargin = {
-			'section-hb-menu-1-margin': '20px',
+			'section-hb-menu-1-margin': '40px',
 		};
 		await setCustomize( menuMargin );
 		await createNewPost( {
 			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
+			title: 'Test Page - mobile',
+			content: 'This is simple test page for mobile view',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await setBrowserViewport( 'large' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-top',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'medium' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-top',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
 		await setBrowserViewport( 'small' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item a' );
 		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-top',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-	} );
-} );
-describe( 'Add primary menu margin-bottom for desktop view', () => {
-	it( 'primary menu margin-bottom should be added properly in desktop view', async () => {
-		const menuMargin = {
-			'section-hb-menu-1-margin': '20px',
-		};
-		await setCustomize( menuMargin );
-		await createNewPost( {
-			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
-		} );
-		await publishPost();
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await setBrowserViewport( 'large' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-bottom',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'medium' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-bottom',
-		} ).cssValueToBe(
-			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
-		);
-		await setBrowserViewport( 'small' );
-		await page.waitForSelector( '#ast-desktop-header .main-navigation a' );
-		await expect( {
-			selector: '#ast-hf-menu-1 .main-header-menu',
-			property: 'margin-bottom',
+			selector: '.main-header-menu',
+			property: 'margin',
 		} ).cssValueToBe(
 			`${ menuMargin[ 'section-hb-menu-1-margin' ] }`,
 		);
