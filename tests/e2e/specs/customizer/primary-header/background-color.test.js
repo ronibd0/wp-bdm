83635c755fb5f9cbec6c8b696f274e3043dd4f10
Simran <simranc@bsf.io>
2021-10-20 17:20:59 +0530

    Test case for primary header background color, getting bootstrap error

---
+++ tests/e2e/specs/customizer/primary-header/background-color.test.js
@@ -0,0 +1,42 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+describe( 'Primary header background color setting in customizer', () => {
+	it( 'background color should apply correctly', async () => {
+		const backgroundColor = {
+			'hb-header-bg-obj-responsive': {
+				desktop: {
+					'background-color': 'rgb(198, 236, 1)',
+				},
+				tablet: {
+					'background-color': 'rgb(198, 236, 1)',
+				},
+				mobile: {
+					'background-color': 'rgb(198, 236, 1)',
+				},
+			},
+		};
+		await setCustomize( backgroundColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '#ast-desktop-header .ast-primary-header-bar' );
+		await expect( {
+			selector: '#ast-desktop-header .ast-primary-header-bar',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-header-bg-obj-responsive' ].desktop }` );
+
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '#ast-desktop-header .ast-primary-header-bar',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-header-bg-obj-responsive' ].tablet }` );
+
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '#ast-desktop-header .ast-primary-header-bar',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-header-bg-obj-responsive' ].mobile }` );
+	} );
+} );
