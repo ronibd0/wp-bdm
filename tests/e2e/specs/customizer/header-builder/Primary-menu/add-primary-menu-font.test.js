dc7252fe43762c0e06f47fff9b1391d40a37d256
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-21 10:46:56 +0530

    added ressponsive case for primary menu font

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-font.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-font.test.js
@@ -2,8 +2,10 @@ import {
 	createURL,
 	createNewPost,
 	publishPost,
+	setBrowserViewport,
 } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../../utils/set-customize';
+import { setCustomize } from '../../../../utils/customize';
+import { responsiveFontSize } from '../../../../utils/responsive-utils';
 describe( 'Primary menu typography settings in customizer', () => {
 	it( 'primary menu typgraphy settings should be applied properly', async () => {
 		const menuFont = {
@@ -18,19 +20,13 @@ describe( 'Primary menu typography settings in customizer', () => {
 			},
 			'header-menu1-font-weight': '800',
 			'header-menu1-text-transform': 'uppercase',
-			'header-menu1-line-height': '0.99',
+			'header-menu1-line-height': '50px',
 		};
 		await setCustomize( menuFont );
 		await createNewPost( {
 			postType: 'page',
 			title: 'Test Page',
-			content: 'This is simple test page'
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Sample Page',
-			content: 'This is Sample test page'
+			content: 'This is simple test page',
 		} );
 		await publishPost();
 		await page.goto( createURL( '/' ), {
@@ -60,6 +56,38 @@ describe( 'Primary menu typography settings in customizer', () => {
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ menuFont[ 'header-menu1-font-size' ].desktop }${ menuFont[ 'header-menu1-font-size' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '#ast-hf-menu-1 .menu-link',
+			property: 'line-height',
+		} ).cssValueToBe(
+			`${ menuFont[ 'header-menu1-line-height' ] }`,
+		);
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .ast-nav-menu' );
+		await expect( {
+			selector: '.menu-link',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ responsiveFontSize(
+				menuFont[ 'header-menu1-font-size' ].tablet,
+			) }${
+				menuFont[ 'header-menu1-font-size' ][ 'tablet-unit' ]
+			}`,
+		);
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .ast-nav-menu' );
+		await expect( {
+			selector: '#ast-hf-mobile-menu .menu-link',
+			property: 'font-size',
+		} ).cssValueToBe(
+			`${ responsiveFontSize(
+				menuFont[ 'header-menu1-font-size' ].mobile,
+			) }${
+				menuFont[ 'header-menu1-font-size' ][ 'mobile-unit' ]
+			}`,
 		);
 	} );
 } );
