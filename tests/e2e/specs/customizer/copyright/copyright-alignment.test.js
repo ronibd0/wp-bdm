944d154a6fc8ce8c60d5efcfec3e7663652492c1
Simran <simranc@bsf.io>
2021-10-21 17:57:30 +0530

    Added responsive in copyright margin test case

--- tests/e2e/specs/customizer/copyright/copyright-alignment.test.js
+++ tests/e2e/specs/customizer/copyright/copyright-alignment.test.js
@@ -1,6 +1,7 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/customize';
 import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'copyright alignment settings in the customizer', () => {
 	it( 'copyright left alignment should apply correctly', async () => {
 		const copyrightAlignment = {
@@ -16,12 +17,9 @@ describe( 'copyright alignment settings in the customizer', () => {
 			waitUntil: 'networkidle0',
 		} );
 
-		await page.evaluate( () => {
-			window.scrollBy( 0, window.innerHeight );
-		} );
-
 		await page.waitForSelector( '.ast-footer-copyright' );
-
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.ast-footer-copyright',
 			property: 'text-align',
@@ -59,7 +57,8 @@ describe( 'copyright alignment settings in the customizer', () => {
 		} );
 
 		await page.waitForSelector( '.ast-footer-copyright' );
-
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.ast-footer-copyright',
 			property: 'text-align',
@@ -97,7 +96,8 @@ describe( 'copyright alignment settings in the customizer', () => {
 		} );
 
 		await page.waitForSelector( '.ast-footer-copyright' );
-
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.ast-footer-copyright',
 			property: 'text-align',
