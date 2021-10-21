457a366ae0a02945bf3f9ea6a4bdd1e3c0ff6afd
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-21 17:56:53 +0530

    added test case for primary menu color

---
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
@@ -0,0 +1,47 @@
+import {
+	createURL,
+	createNewPost,
+	publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Primary menu color settings in the customizer', () => {
+	it( 'primary menu color should apply corectly', async () => {
+		const menuColor = {
+			'header-menu-text-colors': 'rgb(255, 255, 27)',
+		};
+		await setCustomize( menuColor );
+		await createNewPost( {
+			postType: 'page',
+			title: 'Home',
+			content: 'This is a home page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'About Us',
+			content: 'This is about us page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'Our Services',
+			content: 'This is services page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'Contact Us',
+			content: 'This is contact us page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '#ast-desktop-header #ast-hf-menu-1' );
+		await expect( {
+			selector: '.menu-link',
+			property: 'color',
+		} ).cssValueToBe(
+			`rgb(255, 255, 27)`,
+		);
+	} );
+} );
