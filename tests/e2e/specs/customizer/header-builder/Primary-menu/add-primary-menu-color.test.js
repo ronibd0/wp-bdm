43f77792a7863143f01c0a3691a9ff3ebe8725dd
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-21 22:41:27 +0530

    changed the control

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
@@ -7,15 +7,7 @@ describe( 'Primary menu color settings in the customizer', () => {
 	it( 'primary menu color should apply corectly', async () => {
 		const menuColor = {
 			'header-menu1-color-responsive': {
-				desktop: {
-					'text-color': 'rgb(255, 255, 27)',
-				},
-				tablet: {
-					'text-color': 'rgb(255, 255, 27)',
-				},
-				mobile: {
-					'text-color': 'rgb(255, 255, 27)',
-				},
+				desktop: 'rgb(255, 0, 0)',
 			},
 		};
 		await setCustomize( menuColor );
@@ -25,33 +17,15 @@ describe( 'Primary menu color settings in the customizer', () => {
 			content: 'This is a home page',
 		} );
 		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'About Us',
-			content: 'This is about us page',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Our Services',
-			content: 'This is services page',
-		} );
-		await publishPost();
-		await createNewPost( {
-			postType: 'page',
-			title: 'Contact Us',
-			content: 'This is contact us page',
-		} );
-		await publishPost();
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '#ast-desktop-header #ast-hf-menu-1' );
 		await expect( {
-			selector: '.menu-link',
+			selector: '#ast-hf-menu-1 .main-header-menu .page_item .menu-link',
 			property: 'color',
 		} ).cssValueToBe(
-			`${ menuColor[ 'header-menu1-color-responsive' ].desktop[ 'text-color' ] }`,
+			`${ menuColor[ 'header-menu1-color-responsive' ].desktop }`,
 		);
 	} );
 } );
