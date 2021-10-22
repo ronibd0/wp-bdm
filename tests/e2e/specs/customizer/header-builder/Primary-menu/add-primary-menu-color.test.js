c1d141ca6e9c74a8de161e64c0e306c3f748b171
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-10-22 09:41:43 +0530

    modified selectors for tablet and mobile view

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
@@ -1,13 +1,16 @@
 import {
 	createURL,
 	createNewPost,
-	publishPost } from '@wordpress/e2e-test-utils';
+	publishPost,
+	setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Primary menu color settings in the customizer', () => {
 	it( 'primary menu color should apply corectly', async () => {
 		const menuColor = {
 			'header-menu1-color-responsive': {
-				desktop: 'rgb(255, 0, 0)',
+				desktop: 'rgb(183, 1, 129)',
+				tablet: 'rgb(183, 1, 129)',
+				mobile: 'rgb(183, 1, 129)',
 			},
 		};
 		await setCustomize( menuColor );
@@ -26,6 +29,24 @@ describe( 'Primary menu color settings in the customizer', () => {
 			property: 'color',
 		} ).cssValueToBe(
 			`${ menuColor[ 'header-menu1-color-responsive' ].desktop }`,
+		);
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item' );
+		await expect( {
+			selector: '.page_item a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ menuColor[ 'header-menu1-color-responsive' ].tablet }`,
+		);
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item' );
+		await expect( {
+			selector: '.page_item a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ menuColor[ 'header-menu1-color-responsive' ].mobile }`,
 		);
 	} );
 } );
