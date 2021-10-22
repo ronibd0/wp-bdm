c24794fccab9bc245e6f782b3009c68d3ac371ac
Simran <simranc@bsf.io>
2021-10-22 17:43:50 +0530

    Fixed bootstrap error

--- tests/e2e/specs/customizer/footer-builder/below-footer/background-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/background-color.test.js
@@ -8,7 +8,13 @@ describe( 'below footer background color settings in the customizer', () => {
 		const backgroundcolor = {
 			'hbb-footer-bg-obj-responsive': {
 				desktop: {
-					'background-color': 'rgba(227, 236, 156, 1)',
+					'background-color': 'rgb(210, 248, 249)',
+				},
+				tablet: {
+					'background-color': 'rgb(210, 248, 249)',
+				},
+				mobile: {
+					'background-color': 'rgb(210, 248, 249)',
 				},
 			},
 		};
@@ -17,14 +23,27 @@ describe( 'below footer background color settings in the customizer', () => {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
-
 		await setBrowserViewport( 'large' );
-
 		await scrollToElement( '#colophon' );
-
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'hbb-footer-bg-obj-responsive' ].desktop }` );
+		} ).cssValueToBe( `${ backgroundcolor[ 'hbb-footer-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
+
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
+		await setBrowserViewport( 'medium' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundcolor[ 'hbb-footer-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
+
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
+		await setBrowserViewport( 'small' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
+			property: 'background-color',
+		} ).cssValueToBe( `${ backgroundcolor[ 'hbb-footer-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
 	} );
 } );
