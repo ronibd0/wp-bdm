dfb9ec13cafee452e808dd361563c460fcd94fb2
Simran <simranc@bsf.io>
2021-10-23 09:21:47 +0530

    Fixed folder structure problem

--- tests/e2e/specs/customizer/blog/single-post/content-colors.test.js
+++ tests/e2e/specs/customizer/blog/single-post/content-colors.test.js
@@ -1,5 +1,5 @@
 import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/set-customize';
+import { setCustomize } from '../../../../utils/customize';
 describe( 'Section Content color option under the customizer', () => {
 	it( 'content color option should apply correctly', async () => {
 		const contentcolor = {
