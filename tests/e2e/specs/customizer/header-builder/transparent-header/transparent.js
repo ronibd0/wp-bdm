3734d2a599ca69040bb8379c8e7200a5ca8a36e8
akshaylakh <akshayl@bsf.io>
2021-10-25 14:37:59 +0530

    added test case formenu & menu background color

--- tests/e2e/specs/customizer/header-builder/transparent-header/transparent.js
+++ tests/e2e/specs/customizer/header-builder/transparent-header/transparent.js
@@ -1,5 +1,9 @@
-import { createURL } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../../utils/set-customize';
+import { createNewPost,
+	createURL,
+	publishPost,
+} from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 
 describe( 'transparent header settings in the customizer', () => {
 	it( 'transparent header setting should apply correctly', async () => {
@@ -14,17 +18,29 @@ describe( 'transparent header settings in the customizer', () => {
 			'transparent-header-main-sep-color': 'rgb(11, 12, 13)',
 			'transparent-header-bg-color-responsive': {
 				desktop: 'rgb(255, 214, 170)',
-				tablet: 'rgb(220, 198, 198)',
-				mobile: 'rgb(220, 110, 110)',
+				tablet: 'rgb(255, 214, 170)',
+				mobile: 'rgb(255, 214, 170)',
 			},
 			'transparent-header-color-site-title-responsive': {
 				desktop: 'rgb(129, 67, 54)',
-				tablet: 'rgb(22, 19, 19)',
-				mobile: 'rgb(6, 11, 11)',
+				tablet: 'rgb(129, 67, 54)',
+				mobile: 'rgb(129, 67, 54)',
+			},
+			'transparent-menu-color-responsive': {
+				desktop: 'rgb(12, 6, 54)',
+				tablet: 'rgb(12, 6, 54)',
+				mobile: 'rgb(12, 6, 54)',
+			},
+			'transparent-menu-bg-color-responsive': {
+				desktop: 'rgb(236, 236, 38)',
+				tablet: 'rgb(236, 236, 38)',
+				mobile: 'rgb(236, 236, 38)',
 			},
 		};
 		await setCustomize( transparentColorBorder );
-		await page.goto( createURL( '/' ), {
+		await createNewPost( { postType: 'page', title: 'transparent page' } );
+		await publishPost();
+		await page.goto( createURL( '/transparent-page' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector(
@@ -55,13 +71,56 @@ describe( 'transparent header settings in the customizer', () => {
 		} ).cssValueToBe(
 			`${ transparentColorBorder[ 'transparent-header-bg-color-responsive' ].desktop }`,
 		);
-
+		// await setBrowserViewport( 'medium' );
+		// await expect( {
+		// 	selector: '.ast-theme-transparent-header .main-header-bar',
+		// 	property: 'background-color',
+		// } ).cssValueToBe(
+		// 	`${ transparentColorBorder[ 'transparent-menu-bg-color-responsive' ].tablet }`,
+		// );
+		// await setBrowserViewport( 'small' );
+		// await expect( {
+		// 	selector: '.ast-theme-transparent-header .main-header-bar',
+		// 	property: 'background-color',
+		// } ).cssValueToBe(
+		// 	`${ transparentColorBorder[ 'transparent-menu-bg-color-responsive' ].mobile }`,
+		// );
 		//to test transparent header site title normal
+		await setBrowserViewport( 'large' );
 		await expect( {
 			selector: '.ast-theme-transparent-header .site-title a',
 			property: 'color',
 		} ).cssValueToBe(
 			`${ transparentColorBorder[ 'transparent-header-color-site-title-responsive' ].desktop }`,
+		);
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '.ast-theme-transparent-header .site-title a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ transparentColorBorder[ 'transparent-header-color-site-title-responsive' ].tablet }`,
+		);
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.ast-theme-transparent-header .site-title a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ transparentColorBorder[ 'transparent-header-color-site-title-responsive' ].mobile }`,
+		);
+		//to test transparent header menu text/link color
+		await setBrowserViewport( 'large' );
+		await expect( {
+			selector: '.ast-theme-transparent-header .main-header-menu .menu-link',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ transparentColorBorder[ 'transparent-menu-color-responsive' ].desktop }`,
+		);
+		//to test transparent header menu text/link color
+		await expect( {
+			selector: '.ast-theme-transparent-header .main-header-menu .menu-link',
+			property: 'background-color',
+		} ).cssValueToBe(
+			`${ transparentColorBorder[ 'transparent-menu-bg-color-responsive' ].desktop }`,
 		);
 	} );
 } );
