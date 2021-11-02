3d312232c5b0b112a48a2d81d50fecd48093c95a
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-11-02 08:06:02 +0530

    added active color test cases

--- tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
+++ tests/e2e/specs/customizer/header-builder/Primary-menu/add-primary-menu-color.test.js
@@ -5,10 +5,12 @@ import {
 	setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Primary menu color settings in the customizer', () => {
-	it( 'primary menu color should apply corectly', async () => {
+	it( 'primary menu normal color should apply corectly', async () => {
 		const menuColor = {
 			'header-menu1-color-responsive': {
 				desktop: 'rgb(183, 1, 129)',
+			},
+			'header-mobile-menu-color-responsive': {
 				tablet: 'rgb(183, 1, 129)',
 				mobile: 'rgb(183, 1, 129)',
 			},
@@ -37,7 +39,7 @@ describe( 'Primary menu color settings in the customizer', () => {
 			selector: '.page_item a',
 			property: 'color',
 		} ).cssValueToBe(
-			`${ menuColor[ 'header-menu1-color-responsive' ].tablet }`,
+			`${ menuColor[ 'header-mobile-menu-color-responsive' ].tablet }`,
 		);
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );
@@ -46,7 +48,45 @@ describe( 'Primary menu color settings in the customizer', () => {
 			selector: '.page_item a',
 			property: 'color',
 		} ).cssValueToBe(
-			`${ menuColor[ 'header-menu1-color-responsive' ].mobile }`,
+			`${ menuColor[ 'header-mobile-menu-color-responsive' ].mobile }`,
+		);
+	} );
+	it( 'primary menu active color should apply corectly', async () => {
+		const menuColor = {
+			'header-menu1-a-color-responsive': {
+				desktop: 'rgb(0, 0, 0)',
+			},
+			'header-mobile-menu-a-color-responsive': {
+				tablet: 'rgb(0, 0, 0)',
+				mobile: 'rgb(0, 0, 0)',
+			},
+		};
+		await setCustomize( menuColor );
+		await page.click( '.menu-link' );
+		await page.waitForSelector( '#ast-desktop-header' );
+		await expect( {
+			selector: '#primary-site-navigation #ast-hf-menu-1 .main-header-menu .menu-link',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ menuColor[ 'header-menu1-a-color-responsive' ].desktop }`,
+		);
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item' );
+		await expect( {
+			selector: '.page_item a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ menuColor[ 'header-mobile-menu-a-color-responsive' ].tablet }`,
+		);
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '#ast-hf-mobile-menu .menu-item' );
+		await expect( {
+			selector: '.page_item a',
+			property: 'color',
+		} ).cssValueToBe(
+			`${ menuColor[ 'header-mobile-menu-a-color-responsive' ].mobile }`,
 		);
 	} );
 } );
