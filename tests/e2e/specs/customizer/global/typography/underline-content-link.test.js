ab89150f58154d00284c208d32016315595812d3
Simran <simranc@bsf.io>
2021-11-09 17:40:50 +0530

    Test case for typography underline content link option

---
+++ tests/e2e/specs/customizer/global/typography/underline-content-link.test.js
@@ -0,0 +1,38 @@
+import { createURL, createNewPost, publishPost, insertBlock } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Underline content links option under the customizer', () => {
+	it( 'underline content links option should apply correctly', async () => {
+		const underlineLinks = {
+			'underline-content-links': 'none',
+		};
+
+		await setCustomize( underlineLinks );
+		await createNewPost( {
+			postType: 'post',
+			title: 'sample-post',
+		} );
+		await insertBlock( 'HTML' );
+		await page.keyboard.type( '<a href="url">https://wpastra.com/</a>' );
+		await publishPost();
+
+		await setCustomize( underlineLinks );
+		await createNewPost( {
+			postType: 'post',
+			title: 'test-post',
+		} );
+		await insertBlock( 'HTML' );
+		await page.keyboard.type( '<a href="url">https://wpastra.com/</a>' );
+		await publishPost();
+
+		await page.goto( createURL( 'test-post' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '.ast-single-post .entry-content a' );
+		await expect( {
+			selector: '.ast-single-post .entry-content a',
+			property: 'text-decoration',
+		} ).cssValueToBe( `${ underlineLinks[ 'underline-content-links' ] }`,
+		);
+	} );
+} );
