7af77bcb6f47f3e0a4b7f3f6b6f44a390248bb05
Snehal Kolhe <snehalk@bsf.io>
2021-11-11 15:08:05 +0530

    body and heading typography code separated

--- tests/e2e/specs/customizer/global/typography/preset3.test.js
+++ tests/e2e/specs/customizer/global/typography/preset3.test.js
@@ -6,21 +6,15 @@ import {
 } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { TPOGRAPHY_TEST_POST_CONTENT } from '../../../../utils/post';
-
-describe( 'Global typography preset-3 style in the customizer', () => {
-	it( 'body and heading styling should be applied correctly', async () => {
+ describe( 'Global typography preset-3 style in the customizer', () => {
+	it( 'body style should be applied correctly', async () => {
 		const globaltypographyPreset3 = {
 			'typography-presets': 'Preset3',
 			'body-font-family': "'Roboto,sans-serif'",
 			'body-font-weight': '400',
 			'body-text-transform': 'lowercase',
 			'body-line-height': '25px',
-			'headings-font-family': "'Barlow Semi Condensed,sans-serif'",
-			'headings-font-weight': '600',
-			'headings-text-transform': 'uppercase',
-			'headings-line-height': '40px',
 		};
-
 		await setCustomize( globaltypographyPreset3 );
 
 		await createNewPost( { postType: 'post', title: 'preset3' } );
@@ -51,6 +45,23 @@ describe( 'Global typography preset-3 style in the customizer', () => {
 			property: 'line-height',
 		} ).cssValueToBe( `${ globaltypographyPreset3[ 'body-line-height' ] }`,
 		);
+	} );
+	it( 'heading style should be applied correctly', async () => {
+		const globaltypographyPreset3 = {
+			'headings-font-family': "'Barlow Semi Condensed,sans-serif'",
+			'headings-font-weight': '600',
+			'headings-text-transform': 'uppercase',
+			'headings-line-height': '40px',
+		};
+		await setCustomize( globaltypographyPreset3 );
+
+		await createNewPost( { postType: 'post', title: 'preset3' } );
+		await setPostContent( TPOGRAPHY_TEST_POST_CONTENT );
+		await publishPost();
+		await page.goto( createURL( 'preset3' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( 'h1, .entry-content h1' );
 		await expect( {
 			selector: 'h1, .entry-content h1',
 			property: 'font-family',
