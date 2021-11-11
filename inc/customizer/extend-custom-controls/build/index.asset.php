2d9823f639844af157f47190ac7ec8b3040f5286
Navanath Bhosale <navanathb@bsf.io>
2021-11-11 09:29:19 +0530

    chore: cherry picked color-group-control-hiding PR 3701 (#3716)

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '2fc2bf28e81ad799d7ba834c4b6c3a27');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'de7f26a0b1b10c3000d687990f19ec47');
