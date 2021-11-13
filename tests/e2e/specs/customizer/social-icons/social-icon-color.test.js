c3c86e9c96c2b11c3bc5aeddbbf3bb5aed620b40
Simran <simranc@bsf.io>
2021-11-13 14:26:10 +0530

    Added responsive but getting error

--- tests/e2e/specs/customizer/social-icons/social-icon-color.test.js
+++ tests/e2e/specs/customizer/social-icons/social-icon-color.test.js
@@ -1,7 +1,7 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/customize';
 describe( 'Social Icons in the customizer', () => {
-	it( 'social icon color should apply correctly', async () => {
+	it( 'social icon color for desktop should apply correctly', async () => {
 		const socialiconColor = {
 			'header-social-1-color': {
 				desktop: 'rgb(111, 80, 199)',
@@ -27,6 +27,62 @@ describe( 'Social Icons in the customizer', () => {
 			selector: '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element svg',
 			property: 'fill',
 		} ).cssValueToBe( `${ socialiconColor[ 'header-social-1-color' ].desktop }`,
+		);
+	} );
+	it( 'social icon color for tablet should apply correctly', async () => {
+		const socialiconColor = {
+			'header-social-1-color': {
+				tablet: 'rgb(111, 80, 199)',
+			},
+			'header-mobile-items': {
+				primary: {
+					primary_center: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( socialiconColor );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element svg' );
+
+		await expect( {
+			selector: '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element svg',
+			property: 'fill',
+		} ).cssValueToBe( `${ socialiconColor[ 'header-social-1-color' ].tablet }`,
+		);
+	} );
+	it( 'social icon color for mobile should apply correctly', async () => {
+		const socialiconColor = {
+			'header-social-1-color': {
+				mobile: 'rgb(111, 80, 199)',
+			},
+			'header-mobile-items': {
+				primary: {
+					primary_center: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( socialiconColor );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element svg' );
+
+		await expect( {
+			selector: '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element svg',
+			property: 'fill',
+		} ).cssValueToBe( `${ socialiconColor[ 'header-social-1-color' ].mobile }`,
 		);
 	} );
 } );
