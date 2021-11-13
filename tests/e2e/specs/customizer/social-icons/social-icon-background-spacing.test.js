00011b32bdc160c5119d905cca0bbd224767a76d
Simran <simranc@bsf.io>
2021-11-13 16:07:21 +0530

    test case for social icon background spacing

---
+++ tests/e2e/specs/customizer/social-icons/social-icon-background-spacing.test.js
@@ -0,0 +1,30 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+describe( 'Social Icons in the customizer', () => {
+	it( 'social icon background spacing should apply correctly', async () => {
+		const socialiconbackSpacing = {
+			'header-social-1-bg-space': '48px',
+			'header-desktop-items': {
+				primary: {
+					primary_center: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( socialiconbackSpacing );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		await page.waitForSelector( '.ast-header-social-1-wrap .header-social-inner-wrap .ast-builder-social-element' );
+
+		await expect( {
+			selector: '.ast-header-social-1-wrap .header-social-inner-wrap .ast-builder-social-element',
+			property: 'padding',
+		} ).cssValueToBe( `${ socialiconbackSpacing[ 'header-social-1-bg-space' ] }`,
+		);
+	} );
+} );
