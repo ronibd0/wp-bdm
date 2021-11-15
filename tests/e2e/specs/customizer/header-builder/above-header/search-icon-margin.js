5be5b35504cb4060997964d6459038c98be660c3
Snehal Kolhe <snehalk@bsf.io>
2021-11-15 13:01:15 +0530

    added e2e test case for search icon margin in the customizer

---
+++ tests/e2e/specs/customizer/header-builder/above-header/search-icon-margin.js
@@ -0,0 +1,51 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Search-icon margin in the customizer', () => {
+	it( 'search-icon margin for desktop should apply correctly', async () => {
+		const searchiconMargin = {
+			'section-header-search-margin': {
+				desktop: {
+					top: '30',
+					right: '30',
+					bottom: '30',
+					left: '30',
+				},
+				'desktop-unit': 'px',
+			},
+			'header-desktop-items': {
+				above: {
+					above_left: {
+						0: 'search',
+
+					},
+				},
+			},
+		};
+		await setCustomize( searchiconMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-hfb-header .ast-header-search' );
+		await expect( {
+			selector: '.ast-header-search',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ searchiconMargin[ 'section-header-search-margin' ].desktop.top }${ searchiconMargin[ 'section-header-search-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-search',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ searchiconMargin[ 'section-header-search-margin' ].desktop.right }${ searchiconMargin[ 'section-header-search-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-search',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ searchiconMargin[ 'section-header-search-margin' ].desktop.bottom }${ searchiconMargin[ 'section-header-search-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-search',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ searchiconMargin[ 'section-header-search-margin' ].desktop.left }${ searchiconMargin[ 'section-header-search-margin' ][ 'desktop-unit' ] }`,
+		);
+	} );
+} );
