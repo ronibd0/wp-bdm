5d50c3433a844198e0cc9b229df4e8dd8f18a2d2
Snehal Kolhe <snehalk@bsf.io>
2021-11-18 18:12:24 +0530

    E2E test case for customizing HTML1 element margin in the above header section

---
+++ tests/e2e/specs/customizer/header-builder/above-header/html1-margin.test.js
@@ -0,0 +1,153 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+describe( 'HTML1 widget margin in the above header section under the customizer', () => {
+	it( 'html1 widget margincfor desktop should apply correctly', async () => {
+		const htmlMargin = {
+			'header-html-1': '<p>HTML Text color |<a href="https://wpastra.com/" rel="nofollow noopener" target="_blank">Astra QA</a></p>',
+			'section-hb-html-1-margin': {
+				desktop: {
+					top: 50,
+					right: 60,
+					bottom: 50,
+					left: 65,
+				},
+				'desktop-unit': 'px',
+			},
+			'header-desktop-items': {
+				above: {
+					above_left: {
+						0: 'html-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( htmlMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-header-html-1' );
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].desktop.top }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].desktop.right }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].desktop.bottom }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'desktop-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].desktop.left }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'desktop-unit' ] }`,
+		);
+	} );
+
+	it( 'html1 widget margin for tablet should apply correctly', async () => {
+		const htmlMargin = {
+			'header-html-1': '<p>HTML Text color |<a href="https://wpastra.com/" rel="nofollow noopener" target="_blank">Astra QA</a></p>',
+			'section-hb-html-1-margin': {
+				tablet: {
+					top: 30,
+					right: 60,
+					bottom: 30,
+					left: 60,
+				},
+				'tablet-unit': 'px',
+			},
+			'header-mobile-items': {
+				above: {
+					above_left: {
+						0: 'html-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( htmlMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-header-html-1' );
+		await setBrowserViewport( 'medium' );
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].tablet.top }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].tablet.right }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].tablet.bottom }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'tablet-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].tablet.left }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'tablet-unit' ] }`,
+		);
+	} );
+
+	it( 'html1 widget margin for mobile should apply correctly', async () => {
+		const htmlMargin = {
+			'header-html-1': '<p>HTML Text color |<a href="https://wpastra.com/" rel="nofollow noopener" target="_blank">Astra QA</a></p>',
+			'section-hb-html-1-margin': {
+				mobile: {
+					top: 30,
+					right: 60,
+					bottom: 30,
+					left: 60,
+				},
+				'mobile-unit': 'px',
+			},
+			'header-mobile-items': {
+				above: {
+					above_left: {
+						0: 'html-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( htmlMargin );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-header-html-1' );
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-top',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].mobile.top }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-right',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].mobile.right }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-bottom',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].mobile.bottom }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'mobile-unit' ] }`,
+		);
+		await expect( {
+			selector: '.ast-header-html-1',
+			property: 'margin-left',
+		} ).cssValueToBe( `${ htmlMargin[ 'section-hb-html-1-margin' ].mobile.left }${ htmlMargin[ 'section-hb-html-1-margin' ][ 'mobile-unit' ] }`,
+		);
+	} );
+} );
