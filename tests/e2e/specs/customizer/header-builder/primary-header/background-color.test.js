298ca1bad8934f29eff47f748ff7254512534a69
Simran <simranc@bsf.io>
2021-11-25 11:13:38 +0530

    change folder structure

--- tests/e2e/specs/customizer/header-builder/primary-header/background-color.test.js
+++ tests/e2e/specs/customizer/header-builder/primary-header/background-color.test.js
@@ -1,6 +1,6 @@
 import { createURL } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/customize';
-import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Primary header background color setting in customizer', () => {
 	it( 'background color should apply correctly', async () => {
 		const backgroundColor = {
