75829f2891c69a32cb8316b9a241e159ae0d9d82
Snehal Kolhe <snehalk@bsf.io>
2021-11-26 18:03:15 +0530

    e2e test case for page meta option disabling primary header on page

---
+++ tests/e2e/specs/customizer/astra-meta-settings/disable-primary-header.test.js
@@ -0,0 +1,34 @@
+import {createURL,
+	createNewPost,
+	publishPost,
+} from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../utils/customize';
+
+describe( 'site layout meta setting', () => {
+	it( 'site layout meta setting', async () => {
+		const astraMetaSetting = {
+			'inspector-select-control-19': 'left-sidebar',
+		};
+		await setCustomize( astraMetaSetting );
+		await createNewPost( {
+			postType: 'page',
+			title: 'QA',
+		} );
+		await page.waitForSelector( '.interface-pinned-items .components-button:not(:first-child)' );
+		await page.click( '.interface-pinned-items .components-button:not(:first-child)' );
+		await page.waitForSelector( '#astra_settings_meta_box > div:nth-child(2)' );
+		await page.click( '#astra_settings_meta_box > div:nth-child(2) > div > div > div > div.components-input-control__container.css-ygaqem-Container.e1cr7zh11' );
+		await page.click('#astra_settings_meta_box > div:nth-child(5) > div.components-base-control.components-toggle-control.ast-main-header-display.css-wdf2ti-Wrapper.e1puf3u0');
+        await publishPost();
+		await page.goto( createURL( '/QA' ), {
+			waitUntil: 'networkidle0',
+		} );
+
+		// await page.waitForSelector( '.entry-content' );
+		// await expect( {
+		// 	selector: '.ast-above-header-bar',
+		// 	property: 'border-bottom-color',
+		// } ).cssValueToBe( `${ bottomBorder[ 'hba-header-bottom-border-color' ] }`,
+		// );
+	} );
+} );
