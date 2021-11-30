860ed38837ed0231c55ee20109eafb201830d54b
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-11-30 14:32:58 +0530

    added test case for creating footer menu

--- tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-menu/add-footer-menu-padding.test.js
@@ -1,11 +1,41 @@
-import { createURL } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
-import { createNewMenu } from '../../../../utils/create-menu';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Add footer menu spacing for desktop view', () => {
 	it( 'footer menu spacing should be added properly in desktop view', async () => {
-		await createNewMenu();
+		await createNewPost( {
+			postType: 'page',
+			title: 'Test Page',
+			content: 'This is simple test page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'Sub Test Page',
+			content: 'This is simple sub test page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/wp-admin/nav-menus.php' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await scrollToElement( '#nav-menu-footer' );
+		if ( await page.$( '.menu-delete' ) ) {
+			await page.click( '.menu-delete' );
+		}
+		await page.focus( '#menu-name' );
+		await page.type( '#menu-name', 'Menu' );
+		await page.focus( '#locations-footer_menu' );
+		await page.click( '#locations-footer_menu' );
+		await page.click( '#save_menu_footer' );
+		await page.waitForSelector( '.accordion-section-content ' );
+		await page.focus( '#page-tab' );
+		await page.click( '#page-tab' );
+		await page.click( '#submit-posttype-page' );
+		await scrollToElement( '#nav-menu-footer' );
+		await page.waitForSelector( '.publishing-action' );
+		await page.focus( '#save_menu_footer' );
+		await page.click( '#save_menu_footer' );
 		const footerMenuSpacing = {
 			'footer-main-menu-spacing': {
 				desktop: {
@@ -25,15 +55,6 @@ describe( 'Add footer menu spacing for desktop view', () => {
 			},
 		};
 		await setCustomize( footerMenuSpacing );
-		/*await createNewPost( {
-			postType: 'page',
-			title: 'Test Page',
-			content: 'This is simple test page',
-		} );
-		await publishPost();
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );*/
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
