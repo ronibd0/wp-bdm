e6a19e9f029666eec861cf1ca93cba28b20b04bf
Snehal Kolhe <snehalk@bsf.io>
2021-12-01 10:22:56 +0530

    assertion added for header

--- tests/e2e/specs/customizer/astra-meta-settings/disable-primary-header.test.js
+++ tests/e2e/specs/customizer/astra-meta-settings/disable-primary-header.test.js
@@ -1,4 +1,4 @@
-import {createURL,
+import { createURL,
 	createNewPost,
 	publishPost,
 } from '@wordpress/e2e-test-utils';
@@ -7,36 +7,78 @@ import { setCustomize } from '../../../utils/customize';
 describe( 'site layout meta setting', () => {
 	it( 'site layout meta setting', async () => {
 		const astraMetaSetting = {
-			'inspector-select-control-19': 'left-sidebar',
+			'header-desktop-items': {
+				above: {
+					above_left: {
+						0: 'menu-2',
+
+					},
+				},
+				primary: {
+					primary_center: {
+						0: 'search',
+					},
+				},
+				below: {
+					below_right: {
+						1: 'button-1',
+					},
+				},
+			},
+
 		};
 		await setCustomize( astraMetaSetting );
 		await createNewPost( {
 			postType: 'page',
 			title: 'QA',
+			content: 'Test page to disable header',
 		} );
 		//Astra button setting click action
 		await page.waitForSelector( '.interface-pinned-items .components-button:not(:first-child)' );
 		await page.click( '.interface-pinned-items .components-button:not(:first-child)' );
-		//sidebar setting 
+		//sidebar setting
 		await page.waitForSelector( '#astra_settings_meta_box > div:nth-child(2)' );
 		await page.click( '#astra_settings_meta_box > div:nth-child(2) > div > div > div > div.components-input-control__container.css-ygaqem-Container.e1cr7zh11' );
+
+		//content layout
+		await page.waitForSelector( '#astra_settings_meta_box > div:nth-child(3)' );
+		await page.click( '#astra_settings_meta_box > div:nth-child(3) > div > div > div > div.components-input-control__container.css-ygaqem-Container.e1cr7zh11' );
+
 		//primary header
-		await page.click('#astra_settings_meta_box > div:nth-child(5) > div.components-base-control.components-toggle-control.ast-main-header-display.css-wdf2ti-Wrapper.e1puf3u0');
+		await page.click( '#astra_settings_meta_box > div:nth-child(5) > div.components-base-control.components-toggle-control.ast-main-header-display.css-wdf2ti-Wrapper.e1puf3u0' );
 
 		// await page.select( '#astra_settings_meta_box > div:nth-child(2) > div', authorNameValue);
 		// await page.waitForSelector( '#astra_settings_meta_box > div:nth-child(2)' );
 		// await page.click( '#astra_settings_meta_box > div:nth-child(2) > div > div > div > div.components-input-control__container.css-ygaqem-Container.e1cr7zh11' );
 		// await page.click('#astra_settings_meta_box > div:nth-child(5) > div.components-base-control.components-toggle-control.ast-main-header-display.css-wdf2ti-Wrapper.e1puf3u0');
-        await publishPost();
+
+		await publishPost();
 		await page.goto( createURL( '/QA' ), {
 			waitUntil: 'networkidle0',
 		} );
 
-		// await page.waitForSelector( '.entry-content' );
-		// await expect( {
-		// 	selector: '.ast-above-header-bar',
-		// 	property: 'border-bottom-color',
-		// } ).cssValueToBe( `${ bottomBorder[ 'hba-header-bottom-border-color' ] }`,
-		// );
+		//assertion for above header
+		await page.waitForSelector( '.ast-above-header-bar' );
+		await expect( {
+			selector: '.ast-above-header-bar',
+			property: 'display',
+		} ).cssValueToBe( `block`,
+		);
+
+		//assertion for primary header
+		await page.waitForSelector( '#ast-desktop-header .ast-desktop-header-content' );
+		await expect( {
+			selector: '#ast-desktop-header .ast-desktop-header-content',
+			property: 'display',
+		} ).cssValueToBe( `none`,
+		);
+
+		//assertion for below header
+		await page.waitForSelector( '.ast-below-header-bar' );
+		await expect( {
+			selector: '.ast-below-header-bar',
+			property: 'display',
+		} ).cssValueToBe( `block`,
+		);
 	} );
 } );
