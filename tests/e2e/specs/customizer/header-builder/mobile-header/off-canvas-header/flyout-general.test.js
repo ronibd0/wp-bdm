c80a4bcd24ce9dfc6990ad3175e0c6d370ad1cb6
Simran <simranc@bsf.io>
2021-12-06 14:44:39 +0530

    test case for off canvas flyout header general settings

---
+++ tests/e2e/specs/customizer/header-builder/mobile-header/off-canvas-header/flyout-general.test.js
@@ -0,0 +1,172 @@
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+describe( 'off canvas flyout header type settings in the customizer', () => {
+	it( 'flyout header alignment right for mobile should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'flex-end',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-flex-end .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-end .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+
+	it( 'flyout header alignment center for mobile should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-center .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-center .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+
+	it( 'flyout header alignment left for mobile should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'flex-start',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-flex-start .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-start .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+
+	it( 'flyout header alignment right for tablet should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'flex-end',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-flex-end .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-end .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+
+	it( 'flyout header alignment center for tablet should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'center',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-center .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-center .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+
+	it( 'flyout header alignment left for tablet should apply correctly', async () => {
+		const flyoutAlignment = {
+			'mobile-header-type': 'off-canvas',
+			'header-offcanvas-content-alignment': 'flex-start',
+		};
+		await setCustomize( flyoutAlignment );
+		await createNewPost( {
+			postType: 'page',
+			title: 'sample-page',
+		} );
+		await publishPost();
+		await createNewPost( {
+			postType: 'page',
+			title: 'test-page',
+		} );
+		await publishPost();
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.content-align-flex-start .ast-builder-layout-element' );
+		await expect( {
+			selector: '.content-align-flex-start .ast-builder-layout-element',
+			property: 'justify-content',
+		} ).cssValueToBe( `${ flyoutAlignment[ 'header-offcanvas-content-alignment' ] }` );
+	} );
+} );
