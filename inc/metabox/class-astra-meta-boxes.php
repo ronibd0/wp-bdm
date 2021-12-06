dff15f09f7bbbb07e2f62446ca946dfb96a46d4a
Navanath Bhosale <navanathb@bsf.io>
2021-12-06 10:46:25 +0530

    chore: Build release checkpoints for v3.7.6 release

--- inc/metabox/class-astra-meta-boxes.php
+++ inc/metabox/class-astra-meta-boxes.php
@@ -71,7 +71,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 		/**
 		 * Register Post Meta options support.
 		 *
-		 * @since x.x.x
+		 * @since 3.7.6
 		 * @param array|mixed $args the post type args.
 		 * @param string      $post_type the post type.
 		 */
