b2efe8c91b4ec7f020e63a8654efcde723ddbaf2
Simran <simranc@bsf.io>
2021-12-13 17:56:17 +0530

    added test case for post and archive page

--- tests/e2e/specs/customizer/header-builder/transparent-header/disable-on-pages.test.js
+++ tests/e2e/specs/customizer/header-builder/transparent-header/disable-on-pages.test.js
@@ -1,6 +1,28 @@
 import { createURL,	createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'transparent header in the customizer', () => {
+	it( 'disable on archive should apply corectly', async () => {
+		const Disableonarchive = {
+			'transparent-header-enable': 1,
+			'transparent-header-disable-archive': {
+				position: 'absolute',
+				left : 0,
+				right : 0,
+
+			},
+		};
+		await setCustomize( Disableonarchive );
+		await page.goto( createURL( '/author/admin' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe(
+			`${ Disableonarchive[ 'transparent-header-disable-archive' ].position }` );
+	} );
 	it( 'disable on pages should apply corectly', async () => {
 		const Disableonpages = {
 			'transparent-header-enable': 1,
@@ -24,5 +46,29 @@ describe( 'transparent header in the customizer', () => {
 			property: 'position',
 		} ).cssValueToBe(
 			`${ Disableonpages[ 'transparent-header-disable-page' ].position }` );
+	} );
+	it( 'disable on posts should apply corectly', async () => {
+		const Disableonposts = {
+			'transparent-header-enable': 1,
+			'transparent-header-disable-posts': {
+				position: 'absolute',
+				left : 0,
+				right : 0,
+
+			},
+		};
+		await setCustomize( Disableonposts );
+		await createNewPost( { postType: 'post', title: 'QA' } );
+		await publishPost();
+		await page.goto( createURL( '/qa' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe(
+			`${ Disableonposts[ 'transparent-header-disable-posts' ].position }` );
 	} );
 } );
