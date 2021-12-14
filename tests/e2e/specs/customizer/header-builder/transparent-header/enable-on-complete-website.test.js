c24abe42374334dfac4b6f6a1df3e9afeb35b23f
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2021-12-14 14:14:42 +0530

    Update enable-on-complete-website.test.js

--- tests/e2e/specs/customizer/header-builder/transparent-header/enable-on-complete-website.test.js
+++ tests/e2e/specs/customizer/header-builder/transparent-header/enable-on-complete-website.test.js
@@ -1,6 +1,28 @@
 import { createURL,	createNewPost, publishPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'transparent header in the customizer', () => {
+	it( 'disable on archive should apply corectly', async () => {
+		const Disableonarchive = {
+			'transparent-header-enable': 1,
+			'transparent-header-disable-archive': {
+				position: 'absolute',
+				left: 0,
+				right: 0,
+
+			},
+		};
+		await setCustomize( Disableonarchive );
+		await page.goto( createURL( '/author/admin' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe(
+			`${ Disableonarchive[ 'transparent-header-disable-archive' ].position }` );
+	} );
 	it( 'disable on latest posts page should apply corectly', async () => {
 		const Disableonlatestpostpage = {
 			'transparent-header-enable': 1,
