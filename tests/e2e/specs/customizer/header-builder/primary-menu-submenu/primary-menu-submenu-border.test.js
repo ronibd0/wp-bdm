971fe72a15df01cb6f80bd5b40a337f56887260f
Prajakta Patwardhan <prajaktapa@bsf.io>
2021-12-23 22:20:40 +0530

    added test cases for sub menu border

---
+++ tests/e2e/specs/customizer/header-builder/primary-menu-submenu/primary-menu-submenu-border.test.js
@@ -0,0 +1,64 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { createNewMenu } from '../../../../utils/create-menu';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Add sub menu for primary menu and add border to the sub menu', () => {
+	it( 'sub menu should be added successfully', async () => {
+		await createNewMenu();
+		//center alignment for desktop, tablet and mobile
+		const headerMenuAlignment = {
+			'header-desktop-items': {
+				primary: {
+					primary_right: {
+						0: 'menu-1',
+					},
+				},
+			},
+		};
+		await setCustomize( headerMenuAlignment );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '#primary-site-navigation' );
+		await expect( true ).toBe( true );
+	} );
+	it( 'border to the submenu should be added correctly', async () => {
+		const submenuBorder = {
+			'header-menu1-submenu-border': {
+				top: 10,
+				bottom: 10,
+				left: 10,
+				right: 10,
+			},
+		};
+		await setCustomize( submenuBorder );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.hover( '.menu-link' );
+		await expect( {
+			selector: '.sub-menu',
+			property: 'border-top-width',
+		} ).cssValueToBe(
+			`${ submenuBorder[ 'header-menu1-submenu-border' ].top + 'px' }`,
+		);
+		await expect( {
+			selector: '.sub-menu',
+			property: 'border-left-width',
+		} ).cssValueToBe(
+			`${ submenuBorder[ 'header-menu1-submenu-border' ].left + 'px' }`,
+		);
+		await expect( {
+			selector: '.sub-menu',
+			property: 'border-bottom-width',
+		} ).cssValueToBe(
+			`${ submenuBorder[ 'header-menu1-submenu-border' ].bottom + 'px' }`,
+		);
+		await expect( {
+			selector: '.sub-menu',
+			property: 'border-right-width',
+		} ).cssValueToBe(
+			`${ submenuBorder[ 'header-menu1-submenu-border' ].right + 'px' }`,
+		);
+	} );
+} );
+
