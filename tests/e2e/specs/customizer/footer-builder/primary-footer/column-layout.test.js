b2a009e76c93121c51e74c1363cf669c24eb9c4b
Simran <simranc@bsf.io>
2021-12-31 10:49:05 +0530

    test case for primary footer columns and layout

---
+++ tests/e2e/specs/customizer/footer-builder/primary-footer/column-layout.test.js
@@ -0,0 +1,33 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../../utils/scroll-to-element';
+describe( 'primary footer column and layout setting in customizer', () => {
+	it( 'column and dlayout should apply correctly', async () => {
+		const Primaryfooter = {
+			'hb-footer-column': '2',
+			'hb-footer-layout': {
+				desktop: '3-rheavy',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_1: {
+						0: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( Primaryfooter );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-builder-grid-row-3-equal .ast-builder-grid-row' );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.ast-builder-grid-row-3-equal .ast-builder-grid-row',
+			property: 'grid-template-columns',
+		} ).cssValueToBe( `${ Primaryfooter[ 'hb-footer-layout' ].desktop }`,
+		);
+	} );
+} );
