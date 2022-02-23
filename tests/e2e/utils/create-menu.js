4fbac653d867a973156a648d57c31876c05f00e7
Simran <simranc@bsf.io>
2022-02-23 12:03:43 +0530

    modified code

--- tests/e2e/utils/create-menu.js
+++ tests/e2e/utils/create-menu.js
@@ -2,35 +2,23 @@ import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
 import { publishPost } from './publish-post';
 import { scrollToElement } from './scroll-to-element';
 export const createNewMenu = async () => {
-	await createNewPost( {
-		postType: 'page',
-		title: 'Footer page 1',
-	} );
+	await createNewPost( { postType: 'page', title: 'Footer page 1' } );
 	await publishPost();
-	await createNewPost( {
-		postType: 'page',
-		title: 'Footer page 2',
-	} );
+	await createNewPost( { postType: 'page', title: 'Footer page 2' } );
 	await publishPost();
 	await page.goto( createURL( '/wp-admin/nav-menus.php' ), {
 		waitUntil: 'networkidle0',
 	} );
-	await page.waitForSelector( '#nav-menu-footer' );
-	await scrollToElement( '#nav-menu-footer' );
-	if ( await page.$( '.menu-delete' ) ) {
-		await page.click( '.menu-delete' );
-	}
+	await page.click( '.menu-delete' );
 	await page.focus( '#menu-name' );
-	await page.type( '#menu-name', 'Menu' );
+	await page.type( 'Footer Menu' );
 	await page.focus( '#locations-footer_menu' );
 	await page.click( '#locations-footer_menu' );
 	await page.click( '#save_menu_footer' );
-	await page.waitForSelector( '.accordion-section-content ' );
 	await page.focus( '#page-tab' );
 	await page.click( '#page-tab' );
 	await page.click( '#submit-posttype-page' );
 	await scrollToElement( '#nav-menu-footer' );
-	await page.waitForSelector( '.publishing-action' );
 	await page.focus( '#save_menu_footer' );
 	await page.click( '#save_menu_footer' );
 };
