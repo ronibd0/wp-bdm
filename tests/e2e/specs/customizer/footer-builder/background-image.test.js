27a57deb82c6378966e45d480e4a078544fa760c
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-02-28 11:46:43 +0530

    Update background-image.test.js
    Added CamelCase for constant and Added different images for different devices

--- tests/e2e/specs/customizer/footer-builder/background-image.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-image.test.js
@@ -1,10 +1,10 @@
 import { createURL } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/customize';
-import { setBrowserViewport } from '../../../utils/set-browser-viewport';
-import { scrollToElement } from '../../../utils/scroll-to-element';
-describe( 'footer builder backgeround image setting in customizer', () => {
+import { setCustomize } from '../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../../../utils/scroll-to-element';
+describe( 'Footer builder backgeround image setting in customizer', () => {
 	it( 'background image for desktop should apply correctly', async () => {
-		const footerBuilder = {
+		const FooterBuilder = {
 			'section-footer-builder-layout-padding': 60,
 			'footer-bg-obj-responsive': {
 				desktop: {
@@ -17,7 +17,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://media.istockphoto.com/photos/stone-background-portrait-backdrop-studio-backdrop-picture-id1306943029?k=20&m=1306943029&s=612x612&w=0&h=WR7E2p1DRhz79IYsAZIZSAPlbV0hv3AiPcllfbNYAnE=',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -26,7 +26,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://media.istockphoto.com/photos/aqua-painted-background-picture-id157618601?k=20&m=157618601&s=612x612&w=0&h=pQMfRgQxWiXNPOa2illjsIK-HMWYVj6UCH6u4CqHDjQ=',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -43,7 +43,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( footerBuilder );
+		await setCustomize( FooterBuilder );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -54,7 +54,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ footerBuilder[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
@@ -62,7 +62,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ footerBuilder[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
@@ -70,7 +70,7 @@ describe( 'footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ footerBuilder[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 } );
