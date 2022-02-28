bb2ea2a0b2098bcafe0ad10918a104342963e07d
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-02-28 12:01:37 +0530

    Update background-gradient.test.js

--- tests/e2e/specs/customizer/footer-builder/background-gradient.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-gradient.test.js
@@ -1,7 +1,7 @@
 import { createURL } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../../../utils/customize';
-import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
-import { scrollToElement } from '../../../../../utils/scroll-to-element';
+import { setCustomize } from '../../../utils/customize';
+import { setBrowserViewport } from '../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Footer builder background gradient setting in customizer', () => {
 	it( 'background gradient should apply correctly', async () => {
 		const FooterBuilder = {
