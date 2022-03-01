fb38d2674e5b1ab866ef43d374e7bb2ee7a61004
Simran <simranc@bsf.io>
2022-03-01 10:29:47 +0530

    added responsive test case for tablet and mobile it takes same color

--- tests/e2e/specs/customizer/header-builder/social-icons/social-icon-background-color.test.js
+++ tests/e2e/specs/customizer/header-builder/social-icons/social-icon-background-color.test.js
@@ -1,10 +1,13 @@
 import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Social Icons in the customizer', () => {
-	it( 'social icon background color for desktop should apply correctly', async () => {
-		const socialiconbackColor = {
+	it( 'social icon background color should apply correctly', async () => {
+		const BackgroundColor= {
 			'header-social-1-bg-color': {
-				desktop: 'rgb(198, 68, 47)',
+				desktop: 'rgb(213, 132, 234)',
+				tablet: 'rgb(180, 230, 100)',
+				mobile: 'rgb(180, 230, 100)',
 			},
 			'header-desktop-items': {
 				primary: {
@@ -14,8 +17,16 @@ describe( 'Social Icons in the customizer', () => {
 					},
 				},
 			},
+			'header-mobile-items': {
+				primary: {
+					primary_center: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
 		};
-		await setCustomize( socialiconbackColor );
+		await setCustomize( BackgroundColor);
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
@@ -23,9 +34,22 @@ describe( 'Social Icons in the customizer', () => {
 
 		await page.waitForSelector( '.ast-header-social-1-wrap .ast-builder-social-element' );
 		await expect( {
+			selector: '.ast-header-social-1-wrap .ast-builder-social-element',
+			property: 'background-color',
+		} ).cssValueToBe( `${ BackgroundColor[ 'header-social-1-bg-color' ].desktop }` );
+
+		await page.waitForSelector( '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element' );
+		await setBrowserViewport( 'medium' );
+		await expect( {
 			selector: '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element',
 			property: 'background-color',
-		} ).cssValueToBe( `${ socialiconbackColor[ 'header-social-1-bg-color' ].desktop }`,
-		);
+		} ).cssValueToBe( `${ BackgroundColor[ 'header-social-1-bg-color' ].tablet }` );
+
+		await page.waitForSelector( '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element' );
+		await setBrowserViewport( 'small' );
+		await expect( {
+			selector: '.ast-header-social-1-wrap .ast-social-color-type-custom .ast-builder-social-element',
+			property: 'background-color',
+		} ).cssValueToBe( `${ BackgroundColor[ 'header-social-1-bg-color' ].mobile }` );
 	} );
 } );
