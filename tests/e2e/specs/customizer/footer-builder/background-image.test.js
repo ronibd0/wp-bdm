4ac6a8e3f5575639cb9d4af6536a69d1885c6742
Simran <simranc@bsf.io>
2022-03-01 10:55:07 +0530

    added resources suggested by premanshu but image is not visible on front end

--- tests/e2e/specs/customizer/footer-builder/background-image.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-image.test.js
@@ -3,12 +3,12 @@ import { setCustomize } from '../../../utils/customize';
 import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Footer builder backgeround image setting in customizer', () => {
-	it( 'background image for desktop should apply correctly', async () => {
+	it( 'background image should apply correctly', async () => {
 		const FooterBuilder = {
 			'section-footer-builder-layout-padding': 60,
 			'footer-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pxhere.com/en/photo/9381',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -17,7 +17,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://media.istockphoto.com/photos/stone-background-portrait-backdrop-studio-backdrop-picture-id1306943029?k=20&m=1306943029&s=612x612&w=0&h=WR7E2p1DRhz79IYsAZIZSAPlbV0hv3AiPcllfbNYAnE=',
+					'background-image': 'https://pxhere.com/en/photo/937815',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -26,7 +26,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://media.istockphoto.com/photos/aqua-painted-background-picture-id157618601?k=20&m=157618601&s=612x612&w=0&h=pQMfRgQxWiXNPOa2illjsIK-HMWYVj6UCH6u4CqHDjQ=',
+					'background-image': 'https://pxhere.com/en/photo/1435825',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
