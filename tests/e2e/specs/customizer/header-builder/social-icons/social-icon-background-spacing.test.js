24b6e3a1993709789787b8d6a5f06663a0be2746
Simran <simranc@bsf.io>
2022-03-02 09:58:23 +0530

    changed constant

--- tests/e2e/specs/customizer/header-builder/social-icons/social-icon-background-spacing.test.js
+++ tests/e2e/specs/customizer/header-builder/social-icons/social-icon-background-spacing.test.js
@@ -2,7 +2,7 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Social Icons in the customizer', () => {
 	it( 'social icon background spacing should apply correctly', async () => {
-		const socialiconbackSpacing = {
+		const socialIconbackSpacing = {
 			'header-social-1-bg-space': '48px',
 			'header-desktop-items': {
 				primary: {
@@ -13,7 +13,7 @@ describe( 'Social Icons in the customizer', () => {
 				},
 			},
 		};
-		await setCustomize( socialiconbackSpacing );
+		await setCustomize( socialIconbackSpacing );
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
@@ -24,7 +24,7 @@ describe( 'Social Icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-header-social-1-wrap .header-social-inner-wrap .ast-builder-social-element',
 			property: 'padding',
-		} ).cssValueToBe( `${ socialiconbackSpacing[ 'header-social-1-bg-space' ] }`,
+		} ).cssValueToBe( `${ socialIconbackSpacing[ 'header-social-1-bg-space' ] }`,
 		);
 	} );
 } );
