16d4fe789eacddbfdb69f42b6942e8cfbd9c46f3
Simran <simranc@bsf.io>
2022-03-02 09:51:45 +0530

    changed constant

--- tests/e2e/specs/customizer/header-builder/social-icons/social-icon-margin.test.js
+++ tests/e2e/specs/customizer/header-builder/social-icons/social-icon-margin.test.js
@@ -2,13 +2,13 @@ import { createURL, setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Social Icons in the customizer', () => {
 	it( 'social icon margin for desktop should apply correctly', async () => {
-		const SocialIconMargin = {
+		const socialIconMargin = {
 			'section-hb-social-icons-1-margin': {
 				desktop: {
-					top: '40',
-					right: '40',
-					bottom: '40',
-					left: '40',
+					top: '35',
+					right: '35',
+					bottom: '35',
+					left: '35',
 				},
 				'desktop-unit': 'px',
 			},
@@ -20,7 +20,7 @@ describe( 'Social Icons in the customizer', () => {
 				},
 			},
 		};
-		await setCustomize( SocialIconMargin );
+		await setCustomize( socialIconMargin );
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
@@ -29,33 +29,33 @@ describe( 'Social Icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-top',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.top }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.top }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-right',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.right }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.right }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-bottom',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.bottom }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.bottom }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
 		);
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-left',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.left }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
 		);
 	} );
 
 	it( 'social icon margin for tablet should apply correctly', async () => {
-		const SocialIconMargin = {
+		const socialIconMargin = {
 			'section-hb-social-icons-1-margin': {
 				tablet: {
-					top: '35',
-					right: '35',
-					bottom: '35',
-					left: '35',
+					top: '25',
+					right: '25',
+					bottom: '25',
+					left: '25',
 				},
 				'tablet-unit': 'px',
 			},
@@ -68,7 +68,7 @@ describe( 'Social Icons in the customizer', () => {
 				},
 			},
 		};
-		await setCustomize( SocialIconMargin );
+		await setCustomize( socialIconMargin );
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
@@ -78,29 +78,29 @@ describe( 'Social Icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-top',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.top }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.top }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-right',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.right }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.right }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-bottom',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.bottom }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.bottom }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-left',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.left }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
 	} );
 
 	it( 'social icon margin for mobile should apply correctly', async () => {
-		const SocialIconMargin = {
+		const socialIconMargin = {
 			'section-hb-social-icons-1-margin': {
 				mobile: {
-					top: '35',
-					right: '35',
-					bottom: '35',
-					left: '35',
+					top: '25',
+					right: '25',
+					bottom: '25',
+					left: '25',
 				},
 				'mobile-unit': 'px',
 			},
@@ -112,7 +112,7 @@ describe( 'Social Icons in the customizer', () => {
 				},
 			},
 		};
-		await setCustomize( SocialIconMargin );
+		await setCustomize( socialIconMargin );
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
@@ -122,18 +122,18 @@ describe( 'Social Icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-top',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.top }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.top }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-right',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.right }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.right }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-bottom',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.bottom }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.bottom }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-left',
-		} ).cssValueToBe( `${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.left }${ SocialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].mobile.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'mobile-unit' ] }` );
 	} );
 } );
