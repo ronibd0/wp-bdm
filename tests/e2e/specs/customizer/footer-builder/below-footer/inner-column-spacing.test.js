0c3f29e0edf1eff51397fc4ecaf15b0fbbb4deb7
Simran <simranc@bsf.io>
2022-03-04 13:22:55 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/below-footer/inner-column-spacing.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/inner-column-spacing.test.js
@@ -4,45 +4,42 @@ import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Below footer inner column spacing setting in customizer', () => {
 	it( 'spacing should apply correctly', async () => {
-		const innercolumnSpacing = {
+		const innerColumnSpacing = {
+			'hbb-footer-column': 2,
 			'hbb-inner-spacing':
 			{
-				desktop: '50',
-				tablet: '50',
-				mobile: '50',
+				desktop: '74',
+				tablet: '32',
+				mobile: '80',
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
 			},
 		};
-		await setCustomize( innercolumnSpacing );
+		await setCustomize( innerColumnSpacing );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hbb-inner-spacing' ].desktop }${ innercolumnSpacing[ 'hbb-inner-spacing' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hbb-inner-spacing' ].desktop }${ innerColumnSpacing[ 'hbb-inner-spacing' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hbb-inner-spacing' ].tablet }${ innercolumnSpacing[ 'hbb-inner-spacing' ][ 'tablet-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hbb-inner-spacing' ].tablet }${ innerColumnSpacing[ 'hbb-inner-spacing' ][ 'tablet-unit' ] }` );
 
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hbb-inner-spacing' ].mobile }${ innercolumnSpacing[ 'hbb-inner-spacing' ][ 'mobile-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hbb-inner-spacing' ].mobile }${ innerColumnSpacing[ 'hbb-inner-spacing' ][ 'mobile-unit' ] }` );
 	} );
 } );
