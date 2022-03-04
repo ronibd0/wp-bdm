76705f2111baab15d2412241040224889b6471a2
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-04 21:09:49 +0530

    Update vertical-alignment.test.js
    Done changes in describe

--- tests/e2e/specs/customizer/footer-builder/below-footer/vertical-alignment.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/vertical-alignment.test.js
@@ -2,7 +2,7 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'below footer vertical alignment setting in customizer', () => {
+describe( 'Below footer vertical alignment setting in customizer', () => {
 	it( 'verical alignment top should apply correctly', async () => {
 		const verticalAlignment = {
 			'footer-copyright-editor': 'Copyright [copyright] [current_year] [site_title] | Powered by [theme_author] Copyright [copyright] [current_year] [site_title] | Powered by [theme_author]',
