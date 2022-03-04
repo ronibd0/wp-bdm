e83ed2ce52204e884f719c0ff27ffba367f52844
Simran <simranc@bsf.io>
2022-03-04 12:40:52 +0530

    modified some code, change const name

--- tests/e2e/specs/customizer/footer-builder/below-footer/vertical-alignment.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/vertical-alignment.test.js
@@ -2,68 +2,55 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'Below footer vertical alignment setting in customizer', () => {
+describe( 'below footer vertical alignment setting in customizer', () => {
 	it( 'verical alignment top should apply correctly', async () => {
 		const verticalAlignment = {
 			'hbb-footer-vertical-alignment': 'flex-start',
 		};
 		await setCustomize( verticalAlignment );
-
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await setBrowserViewport( 'large' );
-
 		await scrollToElement( '#colophon' );
-
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
-			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }`,
-
-		);
+		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }` );
 	} );
+
 	it( 'verical alignment center should apply correctly', async () => {
 		const verticalAlignment = {
 			'hbb-footer-vertical-alignment': 'center',
 		};
 		await setCustomize( verticalAlignment );
-
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await setBrowserViewport( 'medium' );
-
+		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
-			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }`,
-
-		);
+		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }` );
 	} );
+
 	it( 'verical alignment bottom should apply correctly', async () => {
 		const verticalAlignment = {
 			'hbb-footer-vertical-alignment': 'flex-end',
 		};
 		await setCustomize( verticalAlignment );
-
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await setBrowserViewport( 'small' );
-
+		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
-			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .site-footer-section',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"] .ast-builder-grid-row',
 			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }`,
-
-		);
+		} ).cssValueToBe( `${ verticalAlignment[ 'hbb-footer-vertical-alignment' ] }` );
 	} );
 } );
