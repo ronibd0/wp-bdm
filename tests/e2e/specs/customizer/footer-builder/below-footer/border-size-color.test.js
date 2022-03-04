a584d17add07d69363f7a48b77eba25b806343a1
Simran <simranc@bsf.io>
2022-03-04 12:52:29 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/below-footer/border-size-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/border-size-color.test.js
@@ -2,30 +2,26 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'Below footer border size setting in customizer', () => {
-	it( 'top border size should apply correctly', async () => {
+describe( 'below footer border size setting in customizer', () => {
+	it( 'top border size and color should apply correctly', async () => {
 		const borderSizeColor = {
-			'hbb-footer-separator': '50',
-			'hbb-footer-top-border-color': 'rgb(255, 79, 88)',
+			'hbb-footer-separator': '60',
+			'hbb-footer-top-border-color': 'rgba(227, 16, 195, 0.57)',
 		};
 		await setCustomize( borderSizeColor );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
 		await setBrowserViewport( 'large' );
-
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
 			property: 'border-top-width',
-		} ).cssValueToBe( `${ borderSizeColor[ 'hbb-footer-separator' ] + 'px' }`,
-		);
+		} ).cssValueToBe( `${ borderSizeColor[ 'hbb-footer-separator' ] + 'px' }` );
 		await expect( {
 			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
 			property: 'border-top-color',
-		} ).cssValueToBe( `${ borderSizeColor[ 'hbb-footer-top-border-color' ] }`,
-
-		);
+		} ).cssValueToBe( `${ borderSizeColor[ 'hbb-footer-top-border-color' ] }` );
 	} );
 } );
