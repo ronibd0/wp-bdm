7d93848d841ee1df48b4efc3349435f1c83bd2b6
Simran <simranc@bsf.io>
2022-03-08 10:50:18 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/above-footer/background-gradient.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/background-gradient.test.js
@@ -2,12 +2,12 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'above footer background gradient setting in customizer', () => {
+describe( 'Above footer background gradient setting in customizer', () => {
 	it( 'background gradient should apply correctly', async () => {
-		const aboveFooter = {
+		const aboveFooterGradient = {
 			'hba-footer-bg-obj-responsive': {
 				desktop: {
-					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 31%, rgb(155, 81, 224) 64%)',
+					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 0%, rgb(114, 192, 121) 46%, rgb(155, 81, 224) 100%)',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'contain',
@@ -15,7 +15,7 @@ describe( 'above footer background gradient setting in customizer', () => {
 					'background-type': 'gradient',
 				},
 				tablet: {
-					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 31%, rgb(155, 81, 224) 64%)',
+					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 0%, rgb(189, 48, 135) 41%, rgb(155, 81, 224) 53%)',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'contain',
@@ -23,7 +23,7 @@ describe( 'above footer background gradient setting in customizer', () => {
 					'background-type': 'gradient',
 				},
 				mobile: {
-					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 31%, rgb(155, 81, 224) 64%)',
+					'background-color': 'linear-gradient(135deg, rgb(6, 147, 227) 33%, rgb(168, 224, 81) 73%)',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'contain',
@@ -39,34 +39,30 @@ describe( 'above footer background gradient setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( aboveFooter );
+		await setCustomize( aboveFooterGradient );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"]' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"]' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`${ aboveFooter[ 'hba-footer-bg-obj-responsive' ].desktop[ 'background-color' ] }`,
-		);
+		} ).cssValueToBe( `${ aboveFooterGradient[ 'hba-footer-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
+
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`${ aboveFooter[ 'hba-footer-bg-obj-responsive' ].tablet[ 'background-color' ] }`,
-		);
+		} ).cssValueToBe( `${ aboveFooterGradient[ 'hba-footer-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
+
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`${ aboveFooter[ 'hba-footer-bg-obj-responsive' ].mobile[ 'background-color' ] }`,
-		);
+		} ).cssValueToBe( `${ aboveFooterGradient[ 'hba-footer-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
 	} );
 } );
