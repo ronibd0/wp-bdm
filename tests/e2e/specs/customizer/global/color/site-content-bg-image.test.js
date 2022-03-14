c617c3141cdd8543f2ce69896bad0d22f58e9aa6
Snehal Kolhe <snehalk@bsf.io>
2022-03-14 16:43:16 +0530

    pulled latest changes

--- tests/e2e/specs/customizer/global/color/site-content-bg-image.test.js
+++ tests/e2e/specs/customizer/global/color/site-content-bg-image.test.js
@@ -3,10 +3,10 @@ import { createURL,	createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Testing site background image and content background image setting under the customizer', () => {
 	it( 'site background image should apply correctly', async () => {
-		const sitebgImage = {
+		const siteBgImage = {
 			'site-layout-outside-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/427622bb63c4a43e4.03862800-300x200.jpeg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -14,7 +14,7 @@ describe( 'Testing site background image and content background image setting un
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/359622bb65ece8859.46427938-300x200.jpeg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -22,7 +22,7 @@ describe( 'Testing site background image and content background image setting un
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/254622bb604263235.63460046-200x300.jpeg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -32,7 +32,7 @@ describe( 'Testing site background image and content background image setting un
 			},
 		};
 
-		await setCustomize( sitebgImage );
+		await setCustomize( siteBgImage );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -41,29 +41,29 @@ describe( 'Testing site background image and content background image setting un
 			selector: '.ast-separate-container',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ sitebgImage[ 'site-layout-outside-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ siteBgImage[ 'site-layout-outside-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: '.ast-separate-container',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ sitebgImage[ 'site-layout-outside-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ siteBgImage[ 'site-layout-outside-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '.ast-separate-container',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ sitebgImage[ 'site-layout-outside-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ siteBgImage[ 'site-layout-outside-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 
 	it( 'content background image should apply correctly', async () => {
-		const contentimage = {
+		const contentImage = {
 			'content-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/532622cf651b6f630.19503963-200x300.jpg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -71,7 +71,7 @@ describe( 'Testing site background image and content background image setting un
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/684622d0af70938e9.11913397-300x226.jpg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -79,7 +79,7 @@ describe( 'Testing site background image and content background image setting un
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/623622bb5e8c163d5.06448947-300x200.jpeg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -88,7 +88,7 @@ describe( 'Testing site background image and content background image setting un
 				},
 			},
 		};
-		await setCustomize( contentimage );
+		await setCustomize( contentImage );
 		await createNewPost( {
 			postType: 'post',
 			title: 'test',
@@ -103,21 +103,21 @@ describe( 'Testing site background image and content background image setting un
 			selector: '.ast-separate-container .ast-article-post',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ contentimage[ 'content-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ contentImage[ 'content-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: '.ast-separate-container .ast-article-post',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ contentimage[ 'content-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ contentImage[ 'content-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '.ast-separate-container .ast-article-post',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ contentimage[ 'content-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ contentImage[ 'content-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 } );
