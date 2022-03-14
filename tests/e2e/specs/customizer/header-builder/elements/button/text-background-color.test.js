d2a5a9ad9ea8b510bdda56b2f4edf5d300fb6c7f
Simran <simranc@bsf.io>
2022-03-14 16:28:59 +0530

    test case for header builder button element Text and background color option

--- tests/e2e/specs/customizer/header-builder/elements/button/text-background-color.test.js
+++ tests/e2e/specs/customizer/header-builder/elements/button/text-background-color.test.js
@@ -2,7 +2,7 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
 describe( 'Header builder button setting in customizer', () => {
-	it( 'border width, color and radius should apply correctly', async () => {
+	it( 'text and background color should apply correctly', async () => {
 		const buttonBorder = {
 			'header-button1-text-color': {
 				desktop: 'rgb(230, 12, 12)',
@@ -13,13 +13,6 @@ describe( 'Header builder button setting in customizer', () => {
 				desktop: 'rgb(232, 205, 205)',
 				tablet: 'rgb(243, 239, 217)',
 				mobile: 'rgb(232, 205, 205)',
-			},
-			'header-button1-border-radius': 60,
-			'header-button1-border-size': {
-				top: '80',
-				right: '70',
-				bottom: '60',
-				left: '50',
 			},
 			'header-desktop-items': {
 				above: {
@@ -40,31 +33,6 @@ describe( 'Header builder button setting in customizer', () => {
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		// Border width
-		await page.waitForSelector( '.ast-header-button-1 .ast-custom-button' );
-		await expect( {
-			selector: '.ast-header-button-1 .ast-custom-button',
-			property: 'border-top-width',
-		} ).cssValueToBe( `${ buttonBorder[ 'header-button1-border-size' ].top + 'px' }` );
-		await expect( {
-			selector: '.ast-header-button-1 .ast-custom-button',
-			property: 'border-right-width',
-		} ).cssValueToBe( `${ buttonBorder[ 'header-button1-border-size' ].right + 'px' }` );
-		await expect( {
-			selector: '.ast-header-button-1 .ast-custom-button',
-			property: 'border-bottom-width',
-		} ).cssValueToBe( `${ buttonBorder[ 'header-button1-border-size' ].bottom + 'px' }` );
-		await expect( {
-			selector: '.ast-header-button-1 .ast-custom-button',
-			property: 'border-left-width',
-		} ).cssValueToBe( `${ buttonBorder[ 'header-button1-border-size' ].left + 'px' }` );
-
-		// Border radius
-		await expect( {
-			selector: '.ast-header-button-1 .ast-custom-button',
-			property: 'border-radius',
-		} ).cssValueToBe( `${ buttonBorder[ 'header-button1-border-radius' ] + 'px' }` );
-
 		// Text-color
 		await expect( {
 			selector: '.ast-header-button-1 .ast-custom-button',
