fe4e9f0babd4c18c14f3b8fd13178c277c30dba8
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-16 17:51:07 +0530

    Update section-background-color.test.js

--- tests/e2e/specs/customizer/blog/single-post/section-background-color.test.js
+++ tests/e2e/specs/customizer/blog/single-post/section-background-color.test.js
@@ -2,38 +2,25 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Section baclground color option under the customizer', () => {
 	it( 'section background color option should apply correctly', async () => {
-		const backgroundcolor = {
-			'enable-related-posts': 'true',
-			'related-posts-background-color': 'rgb(255, 255, 2)',
+		const backgroundColor = {
+			'enable-related-posts': 1,
+			'related-posts-background-color': 'rgb(237, 236, 184)',
 		};
-		await setCustomize( backgroundcolor );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
+		await setCustomize( backgroundColor );
+		await createNewPost( { postType: 'post', title: 'sample-post' } );
 		await publishPost();
-
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
+		await createNewPost( { postType: 'post', title: 'test-post' } );
 		await publishPost();
-
-		await page.goto( createURL( 'test-post' ), {
+		await page.goto( createURL( '/test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
-
 		await page.waitForSelector( '.ast-single-related-posts-container' );
 		await expect( {
 			selector: '.ast-single-related-posts-container',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'related-posts-background-color' ] }`,
-		);
+		} ).cssValueToBe( `${ backgroundColor[ 'related-posts-background-color' ] }` );
 	} );
 } );
