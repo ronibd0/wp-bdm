028dec1a3b4ac709ca0b35c7e43155f3bc88db4d
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-16 18:03:29 +0530

    Update single-post-meta-font.test.js

--- tests/e2e/specs/customizer/Blog/single-post/single-post-meta-font.test.js
+++ tests/e2e/specs/customizer/Blog/single-post/single-post-meta-font.test.js
@@ -4,8 +4,8 @@ import { responsiveFontSize } from '../../../../utils/responsive-utils';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Meta font option under the customizer', () => {
 	it( 'meta font option should apply correctly', async () => {
-		const Metafont = {
-			'enable-related-posts': 'true',
+		const metaFont = {
+			'enable-related-posts': 1,
 			'related-posts-meta-font-family': 'Abel',
 			'related-posts-meta-text-transform': 'uppercase',
 			'related-posts-meta-font-weight': '400',
@@ -18,71 +18,54 @@ describe( 'Meta font option under the customizer', () => {
 				'mobile-unit': 'px',
 			},
 		};
-		await setCustomize( Metafont );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
+		await setCustomize( metaFont );
+		await createNewPost( { postType: 'post', title: 'sample-post' } );
 		await publishPost();
-
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
+		await createNewPost( { postType: 'post', title: 'test-post' } );
 		await publishPost();
 
 		await page.goto( createURL( 'test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
 		await page.waitForSelector( '.ast-related-post-content .entry-meta *' );
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'font-family',
-		} ).cssValueToBe( `${ Metafont[ 'related-posts-meta-font-family' ] }`,
-		);
+		} ).cssValueToBe( `${ metaFont[ 'related-posts-meta-font-family' ] }` );
 
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'text-transform',
-		} ).cssValueToBe( `${ Metafont[ 'related-posts-meta-text-transform' ] }`,
-		);
+		} ).cssValueToBe( `${ metaFont[ 'related-posts-meta-text-transform' ] }` );
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'font-weight',
-		} ).cssValueToBe( `${ Metafont[ 'related-posts-meta-font-weight' ] }`,
-		);
+		} ).cssValueToBe( `${ metaFont[ 'related-posts-meta-font-weight' ] }` );
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'font-size',
-		} ).cssValueToBe( `${ Metafont[ 'related-posts-meta-font-size' ].desktop }${ Metafont[ 'related-posts-meta-font-size' ][ 'desktop-unit' ] }` );
+		} ).cssValueToBe( `${ metaFont[ 'related-posts-meta-font-size' ].desktop }${ metaFont[ 'related-posts-meta-font-size' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
-
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				Metafont[ 'related-posts-meta-font-size' ].tablet,
+				metaFont[ 'related-posts-meta-font-size' ].tablet,
 			) }${
-				Metafont[ 'related-posts-meta-font-size' ][ 'tablet-unit' ]
-			}`,
-		);
+				metaFont[ 'related-posts-meta-font-size' ][ 'tablet-unit' ]
+			}` );
 
 		await setBrowserViewport( 'small' );
-
 		await expect( {
 			selector: '.ast-related-post-content .entry-meta *',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				Metafont[ 'related-posts-meta-font-size' ].mobile,
+				metaFont[ 'related-posts-meta-font-size' ].mobile,
 			) }${
-				Metafont[ 'related-posts-meta-font-size' ][ 'mobile-unit' ]
-			}`,
-		);
+				metaFont[ 'related-posts-meta-font-size' ][ 'mobile-unit' ]
+			}` );
 	} );
 } );
