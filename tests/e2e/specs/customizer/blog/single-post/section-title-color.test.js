741c8930e3eeb0ecf57e46b68404236555f8db5a
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-16 17:55:36 +0530

    Update section-title-color.test.js

--- tests/e2e/specs/customizer/blog/single-post/section-title-color.test.js
+++ tests/e2e/specs/customizer/blog/single-post/section-title-color.test.js
@@ -2,38 +2,27 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Section title color option under the customizer', () => {
 	it( 'section title color option should apply correctly', async () => {
-		const titlecolor = {
-			'enable-related-posts': 'true',
+		const titleColor = {
+			'enable-related-posts': 1,
 			'related-posts-title-color': 'rgb(228, 25, 25)',
 		};
-		await setCustomize( titlecolor );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
+		await setCustomize( titleColor );
+		await createNewPost( { postType: 'post', title: 'sample-post' } );
 		await publishPost();
 
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
+		await createNewPost( { postType: 'post', title: 'test-post' } );
 		await publishPost();
 
 		await page.goto( createURL( 'test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
-
 		await page.waitForSelector( ' .ast-separate-container .ast-single-related-posts-container ' );
 		await expect( {
 			selector: '.ast-related-posts-title',
 			property: 'color',
-		} ).cssValueToBe( `${ titlecolor[ 'related-posts-title-color' ] }`,
-		);
+		} ).cssValueToBe( `${ titleColor[ 'related-posts-title-color' ] }` );
 	} );
 } );
