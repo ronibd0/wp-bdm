500e8e0a5745cddc1620b96b7dd9d8a713074100
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-16 17:59:08 +0530

    Update section-title-font.test.js

--- tests/e2e/specs/customizer/blog/single-post/section-title-font.test.js
+++ tests/e2e/specs/customizer/blog/single-post/section-title-font.test.js
@@ -4,8 +4,8 @@ import { responsiveFontSize } from '../../../../utils/responsive-utils';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 describe( 'Section title font option under the customizer', () => {
 	it( 'section title font option should apply correctly', async () => {
-		const sectiontitlefont = {
-			'enable-related-posts': 'true',
+		const sectionTitleFont = {
+			'enable-related-posts': 1,
 			'related-posts-section-title-font-family': 'Abel',
 			'related-posts-section-title-text-transform': 'uppercase',
 			'related-posts-section-title-font-weight': '400',
@@ -17,79 +17,62 @@ describe( 'Section title font option under the customizer', () => {
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
 			},
-
 		};
-		await setCustomize( sectiontitlefont );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
+		await setCustomize( sectionTitleFont );
+		await createNewPost( { postType: 'post', title: 'sample-post' } );
 		await publishPost();
 
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
+		await createNewPost( { postType: 'post', title: 'test-post' } );
 		await publishPost();
 
 		await page.goto( createURL( 'test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
 		await page.evaluate( () => {
 			window.scrollBy( 0, 150 );
 		} );
-
 		await page.waitForSelector( ' .ast-separate-container .ast-single-related-posts-container ' );
 		await expect( {
 			selector: '.ast-single-related-posts-container .ast-related-posts-title-section .ast-related-posts-title',
 			property: 'font-family',
-		} ).cssValueToBe( `${ sectiontitlefont[ 'related-posts-section-title-font-family' ] }`,
-		);
+		} ).cssValueToBe( `${ sectionTitleFont[ 'related-posts-section-title-font-family' ] }` );
 
 		await expect( {
 			selector: '.ast-single-related-posts-container .ast-related-posts-title-section .ast-related-posts-title',
 			property: 'text-transform',
-		} ).cssValueToBe( `${ sectiontitlefont[ 'related-posts-section-title-text-transform' ] }`,
-		);
+		} ).cssValueToBe( `${ sectionTitleFont[ 'related-posts-section-title-text-transform' ] }` );
 
 		await expect( {
 			selector: '.ast-single-related-posts-container .ast-related-posts-title-section .ast-related-posts-title',
 			property: 'font-weight',
-		} ).cssValueToBe( `${ sectiontitlefont[ 'related-posts-section-title-font-weight' ] }`,
-		);
+		} ).cssValueToBe( `${ sectionTitleFont[ 'related-posts-section-title-font-weight' ] }` );
 
 		await expect( {
 			selector: '.ast-related-posts-title',
 			property: 'font-size',
-		} ).cssValueToBe( `${ sectiontitlefont[ 'related-posts-section-title-font-size' ].desktop }${ sectiontitlefont[ 'related-posts-section-title-font-size' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ sectionTitleFont[ 'related-posts-section-title-font-size' ].desktop }${ sectionTitleFont[ 'related-posts-section-title-font-size' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
-
 		await expect( {
 			selector: '.ast-related-posts-title',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				sectiontitlefont[ 'related-posts-section-title-font-size' ].tablet,
+				sectionTitleFont[ 'related-posts-section-title-font-size' ].tablet,
 			) }${
-				sectiontitlefont[ 'related-posts-section-title-font-size' ][ 'tablet-unit' ]
+				sectionTitleFont[ 'related-posts-section-title-font-size' ][ 'tablet-unit' ]
 			}`,
 		);
 
 		await setBrowserViewport( 'small' );
-
 		await expect( {
 			selector: '.ast-related-posts-title',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
-				sectiontitlefont[ 'related-posts-section-title-font-size' ].mobile,
+				sectionTitleFont[ 'related-posts-section-title-font-size' ].mobile,
 			) }${
-				sectiontitlefont[ 'related-posts-section-title-font-size' ][ 'mobile-unit' ]
+				sectionTitleFont[ 'related-posts-section-title-font-size' ][ 'mobile-unit' ]
 			}`,
 		);
 	} );
