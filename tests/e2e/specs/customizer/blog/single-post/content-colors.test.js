f59fafb2cda9a54c5f64fd7de3a6716cf5cc6f2e
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-17 09:01:05 +0530

    Update content-colors.test.js
    modified code

--- tests/e2e/specs/customizer/blog/single-post/content-colors.test.js
+++ tests/e2e/specs/customizer/blog/single-post/content-colors.test.js
@@ -2,47 +2,33 @@ import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Section Content color option under the customizer', () => {
 	it( 'content color option should apply correctly', async () => {
-		const contentcolor = {
-			'enable-related-posts': 'true',
+		const contentColor = {
+			'enable-related-posts': 1,
 			'related-posts-text-color': 'rgb(138, 13, 0)',
 			'related-posts-meta-color': 'rgb(138, 13, 0)',
 			'related-posts-meta-link-hover-color': 'rgb(172, 36, 143)',
 		};
-		await setCustomize( contentcolor );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
+		await setCustomize( contentColor );
+		await createNewPost( { postType: 'post', title: 'sample-post' } );
 		await publishPost();
-
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
+		await createNewPost( { postType: 'post', title: 'test-post' } );
 		await publishPost();
-
 		await page.goto( createURL( 'test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
-
 		await page.waitForSelector( '.ast-related-post-content .entry-header .ast-related-post-title a' );
 		await expect( {
 			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
 			property: 'color',
-		} ).cssValueToBe( `${ contentcolor[ 'related-posts-text-color' ] }`,
-		);
+		} ).cssValueToBe( `${ contentColor[ 'related-posts-text-color' ] }` );
 
 		await expect( {
 			selector: ' .ast-related-post-content .entry-meta *',
 			property: 'color',
-		} ).cssValueToBe( `${ contentcolor[ 'related-posts-meta-color' ] }`,
-		);
+		} ).cssValueToBe( `${ contentColor[ 'related-posts-meta-color' ] }` );
 		await page.goto( createURL( 'test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -56,6 +42,6 @@ describe( 'Section Content color option under the customizer', () => {
 		await expect( {
 			selector: '#main > div.ast-single-related-posts-container > div.ast-related-posts-wrapper > article > div > div > header > div > span.comments-link > a',
 			property: 'color',
-		} ).cssValueToBe( `${ contentcolor[ 'related-posts-meta-link-hover-color' ] }` );
+		} ).cssValueToBe( `${ contentColor[ 'related-posts-meta-link-hover-color' ] }` );
 	} );
 } );
