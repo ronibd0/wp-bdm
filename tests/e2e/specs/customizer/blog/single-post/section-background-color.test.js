6a9935f9679f4e721a86ccd762d8b9ddb3b416b5
Simran <simranc@bsf.io>
2022-03-22 13:40:32 +0530

    added publishPost in existing test case and chamge const

--- tests/e2e/specs/customizer/blog/single-post/section-background-color.test.js
+++ tests/e2e/specs/customizer/blog/single-post/section-background-color.test.js
@@ -1,39 +1,27 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Section baclground color option under the customizer', () => {
+import { publishPost } from '../../../../utils/publish-post';
+describe( 'Section background color option under the customizer', () => {
 	it( 'section background color option should apply correctly', async () => {
-		const backgroundcolor = {
-			'enable-related-posts': 'true',
-			'related-posts-background-color': 'rgb(255, 255, 2)',
+		const backgroundColor = {
+			'enable-related-posts': 1,
+			'related-posts-background-color': 'rgb(243, 254, 215)',
 		};
-		await setCustomize( backgroundcolor );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
-		await publishPost();
-
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
-		await publishPost();
-
-		await page.goto( createURL( 'test-post' ), {
+		await setCustomize( backgroundColor );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'sample-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'test-post' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/test-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-
-		await page.evaluate( () => {
-			window.scrollBy( 0, window.innerHeight );
-		} );
-
 		await page.waitForSelector( '.ast-single-related-posts-container' );
 		await expect( {
 			selector: '.ast-single-related-posts-container',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'related-posts-background-color' ] }`,
-		);
+		} ).cssValueToBe( `${ backgroundColor[ 'related-posts-background-color' ] }` );
 	} );
 } );
