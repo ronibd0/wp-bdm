eb3a7ad0dfe6b9c7faa31c620584236f32a19ece
Snehal Kolhe <snehalk@bsf.io>
2022-03-24 11:03:13 +0530

    PR comment and gradient changes fixed

--- tests/e2e/specs/customizer/footer-builder/background-gradient.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-gradient.test.js
@@ -4,7 +4,7 @@ import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Footer builder background gradient setting in customizer', () => {
 	it( 'background gradient should apply correctly', async () => {
-		const FooterBuilder = {
+		const FooterBuilderBgGradient = {
 			'section-footer-builder-layout-padding': 60,
 			'footer-bg-obj-responsive': {
 				desktop: {
@@ -16,7 +16,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 					'background-type': 'gradient',
 				},
 				tablet: {
-					'background-color': 'linear-gradient(155deg, rgb(6, 147, 327) 31%, rgb(155, 81, 224) 64%)',
+					'background-color': 'linear-gradient(135deg, rgb(125, 209, 150) 0%, rgb(237, 211, 156) 96%)',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'contain',
@@ -24,7 +24,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 					'background-type': 'gradient',
 				},
 				mobile: {
-					'background-color': 'linear-gradient(125deg, rgb(6, 147, 221) 31%, rgb(155, 81, 224) 64%)',
+					'background-color': 'linear-gradient(135deg, rgb(201, 188, 234) 0%, rgb(245, 103, 136) 96%)',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'contain',
@@ -47,7 +47,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( FooterBuilder );
+		await setCustomize( FooterBuilderBgGradient );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -58,7 +58,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`${ FooterBuilder[ 'footer-bg-obj-responsive' ].desktop[ 'background-color' ] }`,
+			`${ FooterBuilderBgGradient[ 'footer-bg-obj-responsive' ].desktop[ 'background-color' ] }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
@@ -67,7 +67,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`${ FooterBuilder[ 'footer-bg-obj-responsive' ].tablet[ 'background-color' ] }`,
+			`${ FooterBuilderBgGradient[ 'footer-bg-obj-responsive' ].tablet[ 'background-color' ] }`,
 		);
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
@@ -76,7 +76,7 @@ describe( 'Footer builder background gradient setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`${ FooterBuilder[ 'footer-bg-obj-responsive' ].mobile[ 'background-color' ] }`,
+			`${ FooterBuilderBgGradient[ 'footer-bg-obj-responsive' ].mobile[ 'background-color' ] }`,
 		);
 	} );
 } );
