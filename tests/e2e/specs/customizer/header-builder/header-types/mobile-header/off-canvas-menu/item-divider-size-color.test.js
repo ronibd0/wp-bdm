9352e871259d55024d408eb490b83e8c282a573d
Simran <simranc@bsf.io>
2022-03-24 11:38:01 +0530

    added test case for divider size and color

---
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/item-divider-size-color.test.js
@@ -0,0 +1,38 @@
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../../utils/set-browser-viewport';
+import { publishPost } from '../../../../../../utils/publish-post';
+describe( 'Off canvas menu item divider settings in the customizer', () => {
+	it( 'off canvas menu item divider size and color should apply corectly for after header', async () => {
+		const offCanvas = {
+			'header-mobile-menu-submenu-item-border': 1,
+			'header-mobile-menu-submenu-item-b-size': '7',
+			'header-mobile-menu-submenu-item-b-color': 'rgb(220, 36, 241)'
+		};
+		await setCustomize( offCanvas );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test-1' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'page', title: 'test-2' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-mobile-header-content .ast-builder-menu-mobile .main-navigation .main-header-menu' );
+		await expect( {
+			selector: '.ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-mobile-header-content .ast-builder-menu-mobile .main-navigation .main-header-menu',
+			property: 'border-top-width',
+		} ).cssValueToBe( `${ offCanvas[ 'header-mobile-menu-submenu-item-b-size' ] }` + 'px' );
+
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await expect( {
+			selector: '.ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-builder-menu-mobile .main-navigation .main-header-menu, .ast-hfb-header .ast-mobile-header-content .ast-builder-menu-mobile .main-navigation .main-header-menu',
+			property: 'border-color',
+		} ).cssValueToBe( `${ offCanvas[ 'header-mobile-menu-submenu-item-b-color' ] }` );
+	} );
+} );
