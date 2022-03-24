91cd64d6fc3793658a4b7fd8a5b2d31a1ceab91f
Simran <simranc@bsf.io>
2022-03-24 14:22:13 +0530

    modified image and some code

--- tests/e2e/specs/customizer/header-builder/header-types/primary-header/primary-menu-background-image.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/primary-header/primary-menu-background-image.test.js
@@ -1,12 +1,13 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
 import { setCustomize } from '../../../../../utils/customize';
-describe( 'Primary menu background image color settings in the customizer', () => {
-	it( 'background gradient image should apply corectly', async () => {
-		const menuGradient = {
+import { publishPost } from '../../../../../utils/publish-post';
+describe( 'Primary menu settings in the customizer', () => {
+	it( 'background image should apply corectly', async () => {
+		const primaryMenuImage = {
 			'header-menu1-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/96622a0f432e6904.41498035-300x169.jpeg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -15,9 +16,12 @@ describe( 'Primary menu background image color settings in the customizer', () =
 				},
 			},
 		};
-		await setCustomize( menuGradient );
-		await createNewPost( { postType: 'page', title: 'test' } );
-		await publishPost();
+		await setCustomize( primaryMenuImage );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -25,15 +29,13 @@ describe( 'Primary menu background image color settings in the customizer', () =
 		await expect( {
 			selector: '.ast-builder-menu-1 .main-header-menu',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ menuGradient[ 'header-menu1-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-menu1-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }` );
 	} );
-	it( 'background gradient color for mobile and tablet should apply corectly', async () => {
-		const menuGradient = {
+	it( 'background image for mobile and tablet should apply corectly', async () => {
+		const primaryMenuImage = {
 			'header-mobile-menu-bg-obj-responsive': {
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/4396229dc66495703.29624587-300x200.jpg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -41,7 +43,7 @@ describe( 'Primary menu background image color settings in the customizer', () =
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/279622a6365c1a804.96566561-300x225.jpg',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -50,29 +52,29 @@ describe( 'Primary menu background image color settings in the customizer', () =
 				},
 			},
 		};
-		await setCustomize( menuGradient );
-		await createNewPost( { postType: 'page', title: 'test' } );
-		await publishPost();
+		await setCustomize( primaryMenuImage );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await setBrowserViewport( 'medium' );
 		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( ' .main-header-menu' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu' );
 		await expect( {
-			selector: '.main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ menuGradient[ 'header-mobile-menu-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-mobile-menu-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }` );
+
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.main-header-menu' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu' );
 		await expect( {
-			selector: '.main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ menuGradient[ 'header-mobile-menu-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryMenuImage[ 'header-mobile-menu-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }` );
 	} );
 } );
