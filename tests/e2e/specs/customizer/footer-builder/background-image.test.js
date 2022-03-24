6763f9b4fa6cc8c7c9336c46da2f05cfe00d9c88
Snehal Kolhe <snehalk@bsf.io>
2022-03-24 10:08:59 +0530

    PR comment resolved

--- tests/e2e/specs/customizer/footer-builder/background-image.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-image.test.js
@@ -4,11 +4,11 @@ import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Footer builder backgeround image setting in customizer', () => {
 	it( 'background image should apply correctly', async () => {
-		const FooterBuilder = {
+		const FooterBuilderBgImage = {
 			'section-footer-builder-layout-padding': 60,
 			'footer-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://pxhere.com/en/photo/9381',
+					'background-image': 'https://pd.w.org/2022/03/96622a0f432e6904.41498035-300x169.jpeg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -17,7 +17,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://pxhere.com/en/photo/937815',
+					'background-image': 'https://pd.w.org/2022/03/279622a6365c1a804.96566561-300x225.jpg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -26,7 +26,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://pxhere.com/en/photo/1435825',
+					'background-image': 'https://pd.w.org/2022/03/4396229dc66495703.29624587-300x200.jpg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -50,7 +50,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( FooterBuilder );
+		await setCustomize( FooterBuilderBgImage );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -61,7 +61,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
@@ -69,7 +69,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
@@ -77,7 +77,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilder[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 } );
