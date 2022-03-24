9173ab170e83561a1b6c31c3186a4a5d2a08ea17
Snehal Kolhe <snehalk@bsf.io>
2022-03-24 13:55:35 +0530

    PR comment resolved

--- tests/e2e/specs/customizer/header-builder/header-types/primary-header/background-image.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/primary-header/background-image.test.js
@@ -3,10 +3,10 @@ import { setCustomize } from '../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
 describe( 'primary header backgeround image setting in customizer', () => {
 	it( 'background image for desktop should apply correctly', async () => {
-		const primaryHeader = {
+		const primaryHeaderBgImage = {
 			'hb-header-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/4376228e3cf277f20.99704501-300x225.jpeg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -15,7 +15,7 @@ describe( 'primary header backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/4136228f92e073825.69069021-rotated.jpg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -24,7 +24,7 @@ describe( 'primary header backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
+					'background-image': 'https://pd.w.org/2022/03/7856227840a9d3925.38199890-300x225.jpg',
 
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
@@ -34,7 +34,7 @@ describe( 'primary header backgeround image setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( primaryHeader );
+		await setCustomize( primaryHeaderBgImage );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -43,21 +43,21 @@ describe( 'primary header backgeround image setting in customizer', () => {
 			selector: '#ast-desktop-header .ast-primary-header-bar',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ primaryHeader[ 'hb-header-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ primaryHeaderBgImage[ 'hb-header-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: '#ast-desktop-header .ast-primary-header-bar',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ primaryHeader[ 'hb-header-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ primaryHeaderBgImage[ 'hb-header-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '#ast-desktop-header .ast-primary-header-bar',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ primaryHeader[ 'hb-header-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ primaryHeaderBgImage[ 'hb-header-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 } );
