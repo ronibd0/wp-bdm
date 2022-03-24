ec7c6c65554c8a9f3236a7c3c954430e675441bc
Simran <simranc@bsf.io>
2022-03-24 14:50:59 +0530

    modified code and changed publishpost()

--- tests/e2e/specs/customizer/breadcrumbs/after-disable-breadcrumb-on-various-pages.test.js
+++ tests/e2e/specs/customizer/breadcrumbs/after-disable-breadcrumb-on-various-pages.test.js
@@ -1,12 +1,13 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/customize';
+import { publishPost } from '../../../utils/publish-post';
 describe( 'breadcrumb settings in the customizer', () => {
 	it( 'disable breadcrumb on home page for after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-home-page': 1,
 		};
-		await setCustomize( insideBreadcrumb );
+		await setCustomize( afterBreadcrumb );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -16,13 +17,16 @@ describe( 'breadcrumb settings in the customizer', () => {
 	} );
 
 	it( 'disable breadcrumb on blog page for after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-blog-posts-page': 1,
 		};
-		await setCustomize( insideBreadcrumb );
-		await createNewPost( { postType: 'post', title: 'test' } );
-		await publishPost();
+		await setCustomize( afterBreadcrumb );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/test' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -34,13 +38,15 @@ describe( 'breadcrumb settings in the customizer', () => {
 	// GitHub action E2E fail case
 	// eslint-disable-next-line jest/no-commented-out-tests
 	// it( 'disable breadcrumb on search page after header position should apply corectly', async () => {
-	// 	const insideBreadcrumb = {
+	// 	const afterBreadcrumb = {
 	// 		'breadcrumb-position': 'astra_header_after',
 	// 		'breadcrumb-disable-archive': 1,
 	// 	};
-	// 	await setCustomize( insideBreadcrumb );
-	// 	await createNewPost( { postType: 'post', title: 'test' } );
-	// 	await publishPost();
+	// 	await setCustomize( afterBreadcrumb );
+	// let ppStatus = false;
+	// while ( false === ppStatus ) {
+	// 	await createNewPost( { postType: 'page', title: 'test' } );
+	// }
 	// 	await page.goto( createURL( '/' ), {
 	// 		waitUntil: 'networkidle0',
 	// 	} );
@@ -53,11 +59,16 @@ describe( 'breadcrumb settings in the customizer', () => {
 	// } );
 
 	it( 'disable breadcrumb on archive page after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-archive': 1,
 		};
-		await setCustomize( insideBreadcrumb );
+		await setCustomize( afterBreadcrumb );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'test' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/author/admin' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -67,13 +78,16 @@ describe( 'breadcrumb settings in the customizer', () => {
 	} );
 
 	it( 'disable breadcrumb on single page for after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-single-page': 1,
 		};
-		await setCustomize( insideBreadcrumb );
-		await createNewPost( { postType: 'page', title: 'single-page' } );
-		await publishPost();
+		await setCustomize( afterBreadcrumb );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'single-page' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/single-page' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -83,13 +97,16 @@ describe( 'breadcrumb settings in the customizer', () => {
 	} );
 
 	it( 'disable breadcrumb on single post for after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-single-post': 1,
 		};
-		await setCustomize( insideBreadcrumb );
-		await createNewPost( { postType: 'page', title: 'single-post' } );
-		await publishPost();
+		await setCustomize( afterBreadcrumb );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'single-post' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/single-post' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -99,13 +116,16 @@ describe( 'breadcrumb settings in the customizer', () => {
 	} );
 
 	it( 'disable breadcrumb on 404 page for after header position should apply corectly', async () => {
-		const insideBreadcrumb = {
+		const afterBreadcrumb = {
 			'breadcrumb-position': 'astra_header_after',
 			'breadcrumb-disable-404-page': 1,
 		};
-		await setCustomize( insideBreadcrumb );
-		await createNewPost( { postType: 'page', title: '404-page' } );
-		await publishPost();
+		await setCustomize( afterBreadcrumb );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: '404-page' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/12' ), {
 			waitUntil: 'networkidle0',
 		} );
