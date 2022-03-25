2bf3285a384eda047e9a9606f7ecc2f0c0ce6342
Simran <simranc@bsf.io>
2022-03-25 15:46:23 +0530

    modified publishPost(), modified code

--- tests/e2e/specs/customizer/blog/single-post/post-title-font.test.js
+++ tests/e2e/specs/customizer/blog/single-post/post-title-font.test.js
@@ -1,32 +1,37 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
-import { setCustomize } from '../../../utils/customize';
-import { responsiveFontSize } from '../../../utils/responsive-utils';
-import { setBrowserViewport } from '../../../utils/set-browser-viewport';
-describe( 'Post title font option under the customizer', () => {
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
+import { setCustomize } from '../../../../utils/customize';
+import { responsiveFontSize } from '../../../../utils/responsive-utils';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+describe( 'Related post option under the customizer', () => {
 	it( 'post title font option should apply correctly', async () => {
 		const postTitleFont = {
 			'enable-related-posts': 1,
-			'related-posts-title-font-family': 'Abel',
+			'related-posts-title-font-family': "'Kirang Haerang', display",
 			'related-posts-title-text-transform': 'uppercase',
 			'related-posts-title-font-weight': '400',
 			'related-posts-title-font-size': {
-				desktop: 50,
-				tablet: 50,
-				mobile: 50,
+				desktop: 60,
+				tablet: 40,
+				mobile: 20,
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
 			},
+			'related-posts-title-line-height': 0.99,
 		};
 		await setCustomize( postTitleFont );
-		await createNewPost( { postType: 'post', title: 'sample-post' } );
-		await publishPost();
-		await createNewPost( { postType: 'post', title: 'test-post' } );
-		await publishPost();
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'sample-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'test-post' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( 'sample-post' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( ' .ast-separate-container .ast-single-related-posts-container ' );
+		await page.waitForSelector( '.ast-related-post-content .entry-header .ast-related-post-title a' );
 		await expect( {
 			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
 			property: 'font-family',
@@ -61,7 +66,7 @@ describe( 'Post title font option under the customizer', () => {
 
 		await setBrowserViewport( 'small' );
 		await expect( {
-			selector: '.ast-related-post-content .entry-header .ast-related-post-title a ',
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a',
 			property: 'font-size',
 		} ).cssValueToBe(
 			`${ await responsiveFontSize(
@@ -69,5 +74,10 @@ describe( 'Post title font option under the customizer', () => {
 			) }${
 				postTitleFont[ 'related-posts-title-font-size' ][ 'mobile-unit' ]
 			}` );
+
+		await expect( {
+			selector: '.ast-related-post-content .entry-header .ast-related-post-title a ',
+			property: 'line-height',
+		} ).cssValueToBe( `${ postTitleFont[ 'related-posts-title-line-height' ] }` );
 	} );
 } );
