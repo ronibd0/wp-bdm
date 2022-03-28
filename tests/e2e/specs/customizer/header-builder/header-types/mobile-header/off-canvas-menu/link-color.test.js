fccb7a7beaa447b83b726a6c36af1b9a0bc89e1b
Simran <simranc@bsf.io>
2022-03-28 17:42:57 +0530

    added test case for active link color

--- tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/link-color.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/link-color.test.js
@@ -37,4 +37,39 @@ describe( 'Off canvas menu link color settings in the customizer', () => {
 			property: 'color',
 		} ).cssValueToBe( `${ offCanvasLinkColor[ 'header-mobile-menu-color-responsive' ].mobile }` );
 	} );
+
+	it( 'active link color should apply corectly for after header', async () => {
+		const offCanvasLinkColor = {
+			'header-mobile-menu-a-color-responsive': {
+				tablet: 'rgb(123, 6, 6)',
+				mobile: 'rgb(130, 92, 3)',
+			},
+		};
+		await setCustomize( offCanvasLinkColor );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test-1' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'page', title: 'test-2' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/test-1' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ offCanvasLinkColor[ 'header-mobile-menu-a-color-responsive' ].tablet }` );
+
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ offCanvasLinkColor[ 'header-mobile-menu-a-color-responsive' ].mobile }` );
+	} );
 } );
