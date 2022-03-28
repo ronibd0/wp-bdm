482c7e19f52cb71460bf3ddea84ab273aecf3ddc
Simran <simranc@bsf.io>
2022-03-28 17:18:02 +0530

    test case for off canvas menu link color

---
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/link-color.test.js
@@ -0,0 +1,40 @@
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../../../utils/customize';
+import { setBrowserViewport } from '../../../../../../utils/set-browser-viewport';
+import { publishPost } from '../../../../../../utils/publish-post';
+describe( 'Off canvas menu link color settings in the customizer', () => {
+	it( 'link color should apply corectly for after header', async () => {
+		const offCanvasLinkColor = {
+			'header-mobile-menu-color-responsive': {
+				tablet: 'rgb(133, 10, 158)',
+				mobile: 'rgb(6, 109, 16)',
+			},
+		};
+		await setCustomize( offCanvasLinkColor );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test-1' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'page', title: 'test-2' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'medium' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ offCanvasLinkColor[ 'header-mobile-menu-color-responsive' ].tablet }` );
+
+		await setBrowserViewport( 'small' );
+		await page.click( '.main-header-menu-toggle' );
+		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ offCanvasLinkColor[ 'header-mobile-menu-color-responsive' ].mobile }` );
+	} );
+} );
