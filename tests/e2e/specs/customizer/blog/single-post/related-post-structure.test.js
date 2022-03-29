fbd620f2bf8b156eb9bd1d0e9683912409be59be
Simran <simranc@bsf.io>
2022-03-29 13:48:42 +0530

    resolved PR comments

--- tests/e2e/specs/customizer/blog/single-post/related-post-structure.test.js
+++ tests/e2e/specs/customizer/blog/single-post/related-post-structure.test.js
@@ -1,35 +1,30 @@
-import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Single post font option under the customizer', () => {
+describe( 'Single post option under the customizer', () => {
 	it( 'related post structure option should apply correctly', async () => {
-		const relatedpostStructure = {
-			'enable-related-posts': true,
+		const relatedPostStructure = {
+			'enable-related-posts': 1,
 			'related-posts-structure': {
 				'title-meta': 1,
 			},
 		};
-		await setCustomize( relatedpostStructure );
-		await createNewPost( {
-			postType: 'post',
-			title: 'sample-post',
-
-		} );
-		await publishPost();
-
-		await createNewPost( {
-			postType: 'post',
-			title: 'test-post',
-
-		} );
-		await publishPost();
-		await page.goto( createURL( 'sample-post' ), {
+		await setCustomize( relatedPostStructure );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'post', title: 'test-post' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'sample-post' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/sample-post' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.evaluate( () => {
 			window.scrollBy( 0, window.innerHeight );
 		} );
 		await page.waitForSelector( '.entry-header' );
-		const RelatedtitleMeta = await page.$eval( '.entry-header', ( element ) => element.getAttribute( '.ast-related-post-content .entry-header' ) );
-		await expect( RelatedtitleMeta ).toBeNull( );
+		const relatedTitleMeta = await page.$eval( '.entry-header', ( element ) => element.getAttribute( '.ast-related-post-content .entry-header' ) );
+		await expect( relatedTitleMeta ).toBeNull( );
 	} );
 } );
