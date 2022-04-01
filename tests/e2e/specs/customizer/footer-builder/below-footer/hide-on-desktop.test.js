21c676be8bf6e3149f071abdbf6ea936a5115ef0
Simran <simranc@bsf.io>
2022-04-01 17:28:49 +0530

    changed assertion

--- tests/e2e/specs/customizer/footer-builder/below-footer/hide-on-desktop.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/hide-on-desktop.test.js
@@ -3,54 +3,48 @@ import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Below footer hide on desktop setting in customizer', () => {
-	it( 'should hide on desktop', async () => {
+	it( 'below footer should hide on desktop', async () => {
 		const hideOnDesktop = {
-			'section-below-footer-builder-hide-desktop': 'none',
+			'section-below-footer-builder-hide-desktop': 1,
 		};
 		await setCustomize( hideOnDesktop );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-		await expect( {
-			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
-			property: 'display',
-		} ).cssValueToBe( `${ hideOnDesktop[ 'section-below-footer-builder-hide-desktop' ] }` );
+		await page.waitForSelector( '.site-footer' );
+		const desktop = await page.$eval( '.site-footer', ( element ) => element.getAttribute( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' ) );
+		await expect( desktop ).toBeNull( );
 	} );
 
-	it( 'should hide on tablet', async () => {
+	it( 'below footer should hide on tablet', async () => {
 		const hideOnTablet = {
-			'section-below-footer-builder-hide-tablet': 'none',
+			'section-below-footer-builder-hide-tablet': 1,
 		};
 		await setCustomize( hideOnTablet );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
-		await expect( {
-			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"]',
-			property: 'display',
-		} ).cssValueToBe( `${ hideOnTablet[ 'section-below-footer-builder-hide-tablet' ] }` );
+		await page.waitForSelector( '.site-footer' );
+		const tablet = await page.$eval( '.site-footer', ( element ) => element.getAttribute( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' ) );
+		await expect( tablet ).toBeNull( );
 	} );
 
-	it( 'should hide on mobile', async () => {
+	it( 'below footer should hide on mobile', async () => {
 		const hideOnMobile = {
-			'section-below-footer-builder-hide-mobile': 'none',
+			'section-below-footer-builder-hide-mobile': 1,
 		};
 		await setCustomize( hideOnMobile );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
-		await expect( {
-			selector: ' .site-below-footer-wrap[data-section="section-below-footer-builder"] ',
-			property: 'display',
-		} ).cssValueToBe( `${ hideOnMobile[ 'section-below-footer-builder-hide-mobile' ] }` );
+		await page.waitForSelector( '.site-footer' );
+		const tablet = await page.$eval( '.site-footer', ( element ) => element.getAttribute( '.site-below-footer-wrap[data-section="section-below-footer-builder"]' ) );
+		await expect( tablet ).toBeNull( );
 	} );
 } );
