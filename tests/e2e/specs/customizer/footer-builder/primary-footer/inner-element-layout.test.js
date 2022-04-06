5ba320709498444fce9de8def2e15896b54615bd
Simran <simranc@bsf.io>
2022-04-06 17:29:44 +0530

    test case for primary footer inner element layout option

---
+++ tests/e2e/specs/customizer/footer-builder/primary-footer/inner-element-layout.test.js
@@ -0,0 +1,97 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../../utils/scroll-to-element';
+describe( 'Primary footer inner element layout setting in customizer', () => {
+	it( 'inline layout should apply correctly', async () => {
+		const innerElementLayout = {
+			'hba-stack': {
+				desktop: 'inline',
+				tablet: 'inline',
+				mobile: 'inline',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_1: {
+						0: 'html-2',
+						1: 'social-icons-1',
+						2: 'html-1',
+					},
+				},
+			},
+		};
+		await setCustomize( innerElementLayout );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-inline .site-footer-section' );
+		await expect( {
+			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-inline .site-footer-section',
+			property: 'display',
+		} ).cssValueToBe( `flex` );
+
+		await setBrowserViewport( 'medium' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-tablet-inline .site-footer-section' );
+		await expect( {
+			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-tablet-inline .site-footer-section',
+			property: 'display',
+		} ).cssValueToBe( `flex` );
+
+		await setBrowserViewport( 'small' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-mobile-inline .site-footer-section' );
+		await expect( {
+			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"].ast-footer-row-mobile-inline .site-footer-section',
+			property: 'display',
+		} ).cssValueToBe( `flex` );
+	} );
+
+	it( 'stack layout should apply correctly', async () => {
+		const innerElementLayout = {
+			'hba-stack': {
+				desktop: 'stack',
+				tablet: 'stack',
+				mobile: 'stack',
+			},
+			'footer-desktop-items': {
+				above: {
+					above_1: {
+						0: 'html-2',
+						1: 'social-icons-1',
+						2: 'html-1',
+					},
+				},
+			},
+		};
+		await setCustomize( innerElementLayout );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-footer-row-stack' );
+		await expect( {
+			selector: '.ast-footer-row-stack',
+			property: 'display',
+		} ).cssValueToBe( `grid` );
+
+		await setBrowserViewport( 'medium' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-footer-row-tablet-stack' );
+		await expect( {
+			selector: '.ast-footer-row-tablet-stack',
+			property: 'display',
+		} ).cssValueToBe( `grid` );
+
+		await setBrowserViewport( 'small' );
+		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-footer-row-mobile-stack' );
+		await expect( {
+			selector: '.ast-footer-row-mobile-stack',
+			property: 'display',
+		} ).cssValueToBe( `grid` );
+	} );
+} );
