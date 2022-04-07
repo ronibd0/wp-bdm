a558d614b05e162bcf56d217a030354e96d9048f
Simran <simranc@bsf.io>
2022-04-07 18:08:39 +0530

    modified code

--- tests/e2e/specs/customizer/header-builder/transparent-header/enable-on-complete-website.test.js
+++ tests/e2e/specs/customizer/header-builder/transparent-header/enable-on-complete-website.test.js
@@ -2,7 +2,7 @@ import { createURL,	createNewPost } from '@wordpress/e2e-test-utils';
 import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
 describe( 'Transparent header in the customizer', () => {
-	it( 'enable on archive, 404 and search page should apply correctly', async () => {
+	it( 'disable on archive page should apply corectly', async () => {
 		const disableOnArchive = {
 			'transparent-header-enable': 1,
 			'transparent-header-disable-archive': 0,
@@ -10,37 +10,57 @@ describe( 'Transparent header in the customizer', () => {
 		await setCustomize( disableOnArchive );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'post', title: 'test' } );
+			await createNewPost( { postType: 'post', title: 'archive-page' } );
 			ppStatus = await publishPost();
 		}
 		await page.goto( createURL( '/author/admin' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disableArchive = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disableArchive ).toBeNull( );
-
-		await setCustomize( disableOnArchive );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
+	} );
+	it( 'disable on 404 page should apply corectly', async () => {
+		const disableOn404 = {
+			'transparent-header-enable': 1,
+			'transparent-header-disable-archive': 0,
+		};
+		await setCustomize( disableOn404 );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: '404-page' } );
+			ppStatus = await publishPost();
+		}
 		await page.goto( createURL( '/12' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disable404 = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disable404 ).toBeNull( );
-
-		await setCustomize( disableOnArchive );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
+	} );
+	it( 'disable on search page should apply corectly', async () => {
+		const disableOnSearch = {
+			'transparent-header-enable': 1,
+			'transparent-header-disable-archive': 0,
+		};
+		await setCustomize( disableOnSearch );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		//GitHub action E2E fail case
-		//eslint-disable-next-line jest/no-commented-out-tests
-		// await page.click( '.widget_search .search-form .search-field' );
-		// await page.keyboard.type( 'test' );
-		// await page.keyboard.press( 'Enter' );
-		// await page.waitForSelector( '.ast-theme-transparent-header' );
-		// const disableSearch = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		// await expect( disableSearch ).toBeNull( );
+		await page.click( '#wp-block-search__input-1' );
+		await page.keyboard.type( 'test' );
+		await page.keyboard.press( 'Enter' );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
 	} );
+
 	it( 'enable on blog page should apply correctly', async () => {
 		const disableOnBlogPage = {
 			'transparent-header-enable': 1,
@@ -49,16 +69,21 @@ describe( 'Transparent header in the customizer', () => {
 		await setCustomize( disableOnBlogPage );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'post', title: 'test' } );
+			await createNewPost( { postType: 'post', title: 'test-1' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'test-2' } );
 			ppStatus = await publishPost();
 		}
-		await page.goto( createURL( '/test' ), {
+		await page.goto( createURL( '/test-1' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disableblog = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disableblog ).toBeNull( );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
 	} );
+
 	it( 'enable on latest posts page should apply correctly', async () => {
 		const disableOnLatestPostPage = {
 			'transparent-header-enable': 1,
@@ -67,17 +92,21 @@ describe( 'Transparent header in the customizer', () => {
 		await setCustomize( disableOnLatestPostPage );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'post', title: 'sample' } );
-			await createNewPost( { postType: 'post', title: 'test' } );
+			await createNewPost( { postType: 'post', title: 'test-1' } );
+			ppStatus = await publishPost();
+			await createNewPost( { postType: 'post', title: 'test-2' } );
 			ppStatus = await publishPost();
 		}
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disablelatestPost = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disablelatestPost ).toBeNull( );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
 	} );
+
 	it( 'enable on pages should apply correctly', async () => {
 		const disableOnPages = {
 			'transparent-header-enable': 1,
@@ -86,16 +115,19 @@ describe( 'Transparent header in the customizer', () => {
 		await setCustomize( disableOnPages );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test' } );
+			await createNewPost( { postType: 'page', title: 'test-page' } );
 			ppStatus = await publishPost();
 		}
-		await page.goto( createURL( '/test' ), {
+		await page.goto( createURL( '/test-page' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disablePages = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disablePages ).toBeNull( );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
 	} );
+
 	it( 'enable on posts should apply correctly', async () => {
 		const disableOnPosts = {
 			'transparent-header-enable': 1,
@@ -104,14 +136,16 @@ describe( 'Transparent header in the customizer', () => {
 		await setCustomize( disableOnPosts );
 		let ppStatus = false;
 		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'post', title: 'test' } );
+			await createNewPost( { postType: 'post', title: 'test-posts' } );
 			ppStatus = await publishPost();
 		}
-		await page.goto( createURL( '/test' ), {
+		await page.goto( createURL( '/test-posts' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-theme-transparent-header' );
-		const disablePost = await page.$eval( '.ast-theme-transparent-header', ( element ) => element.getAttribute( '.ast-theme-transparent-header #masthead' ) );
-		await expect( disablePost ).toBeNull( );
+		await page.waitForSelector( '.ast-theme-transparent-header #masthead' );
+		await expect( {
+			selector: '.ast-theme-transparent-header #masthead',
+			property: 'position',
+		} ).cssValueToBe( `absolute` );
 	} );
 } );
