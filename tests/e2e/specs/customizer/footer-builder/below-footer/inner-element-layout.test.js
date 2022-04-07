362f6ebcac04af22049f5d4de0044b7774a951e4
Simran <simranc@bsf.io>
2022-04-07 18:36:08 +0530

    changed slector

--- tests/e2e/specs/customizer/footer-builder/below-footer/inner-element-layout.test.js
+++ tests/e2e/specs/customizer/footer-builder/below-footer/inner-element-layout.test.js
@@ -72,26 +72,26 @@ describe( 'Below footer inner element layout setting in customizer', () => {
 		} );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-		await page.waitForSelector( '.ast-footer-row-stack' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-stack .site-footer-section' );
 		await expect( {
-			selector: '.ast-footer-row-stack',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-stack .site-footer-section',
 			property: 'display',
-		} ).cssValueToBe( `grid` );
+		} ).cssValueToBe( `block` );
 
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
-		await page.waitForSelector( '.ast-footer-row-tablet-stack' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-tablet-stack .site-footer-section' );
 		await expect( {
-			selector: '.ast-footer-row-tablet-stack',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-tablet-stack .site-footer-section',
 			property: 'display',
-		} ).cssValueToBe( `grid` );
+		} ).cssValueToBe( `block` );
 
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
-		await page.waitForSelector( '.ast-footer-row-mobile-stack' );
+		await page.waitForSelector( '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-mobile-stack .site-footer-section' );
 		await expect( {
-			selector: '.ast-footer-row-mobile-stack',
+			selector: '.site-below-footer-wrap[data-section="section-below-footer-builder"].ast-footer-row-mobile-stack .site-footer-section',
 			property: 'display',
-		} ).cssValueToBe( `grid` );
+		} ).cssValueToBe( `block` );
 	} );
 } );
