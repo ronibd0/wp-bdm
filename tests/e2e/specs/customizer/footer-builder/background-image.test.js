6fa958114a97770749e0d9037f2a1f20963fe507
Simran <simranc@bsf.io>
2022-04-08 15:54:41 +0530

    changed const

--- tests/e2e/specs/customizer/footer-builder/background-image.test.js
+++ tests/e2e/specs/customizer/footer-builder/background-image.test.js
@@ -4,7 +4,7 @@ import { setBrowserViewport } from '../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../utils/scroll-to-element';
 describe( 'Footer builder backgeround image setting in customizer', () => {
 	it( 'background image should apply correctly', async () => {
-		const FooterBuilderBgImage = {
+		const footerBuilderBgImage = {
 			'section-footer-builder-layout-padding': 60,
 			'footer-bg-obj-responsive': {
 				desktop: {
@@ -50,7 +50,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( FooterBuilderBgImage );
+		await setCustomize( footerBuilderBgImage );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -61,7 +61,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
+			`url("${ footerBuilderBgImage[ 'footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
@@ -69,7 +69,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
+			`url("${ footerBuilderBgImage[ 'footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
 		);
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
@@ -77,7 +77,7 @@ describe( 'Footer builder backgeround image setting in customizer', () => {
 			selector: '.site-footer',
 			property: 'background-image',
 		} ).cssValueToBe(
-			`url("${ FooterBuilderBgImage[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
+			`url("${ footerBuilderBgImage[ 'footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
 		);
 	} );
 } );
