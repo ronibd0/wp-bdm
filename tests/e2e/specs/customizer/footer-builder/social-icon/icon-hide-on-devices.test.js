118e89b49985c0c33abbe109bf96c9a9a6774516
Simran <simranc@bsf.io>
2022-04-08 09:33:27 +0530

    changed property

--- tests/e2e/specs/customizer/footer-builder/social-icon/icon-hide-on-devices.test.js
+++ tests/e2e/specs/customizer/footer-builder/social-icon/icon-hide-on-devices.test.js
@@ -2,28 +2,19 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
-describe( 'social icons in the customizer', () => {
-	it( 'social icon hide on devices should apply correctly', async () => {
-		const hideOnDevices = {
-			'section-fb-social-icons-1-hide-desktop': {
-				desktop: 'none',
-			},
-			'section-fb-social-icons-1-hide-tablet': {
-				tablet: 'none',
-			},
-			'section-fb-social-icons-1-hide-mobile': {
-				mobile: 'none',
-			},
+describe( 'social icons hide on devices in the customizer', () => {
+	it( 'social icon hide on desktop should apply correctly', async () => {
+		const hideOnDesktop = {
+			'section-fb-social-icons-1-hide-desktop': 1,
 			'footer-desktop-items': {
 				primary: {
 					primary_2: {
 						0: 'social-icons-1',
-
 					},
 				},
 			},
 		};
-		await setCustomize( hideOnDevices );
+		await setCustomize( hideOnDesktop );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -33,20 +24,54 @@ describe( 'social icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-builder-layout-element[data-section="section-fb-social-icons-1"]',
 			property: 'display',
-		} ).cssValueToBe( `${ hideOnDevices[ 'section-fb-social-icons-1-hide-desktop' ].desktop }` );
+		} ).cssValueToBe( `none` );
+	} );
 
+	it( 'social icon hide on tablet should apply correctly', async () => {
+		const hideOnTablet = {
+			'section-fb-social-icons-1-hide-tablet': 1,
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( hideOnTablet );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-builder-layout-element[data-section="section-fb-social-icons-1"]' );
 		await expect( {
 			selector: '.ast-builder-layout-element[data-section="section-fb-social-icons-1"]',
 			property: 'display',
-		} ).cssValueToBe( `${ hideOnDevices[ 'section-fb-social-icons-1-hide-tablet' ].tablet }` );
+		} ).cssValueToBe( `none` );
+	} ); 
 
+	it( 'social icon hide on mobile should apply correctly', async () => {
+		const hideOnMobile = {
+			'section-fb-social-icons-1-hide-mobile': 1,
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( hideOnMobile );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-builder-layout-element[data-section="section-fb-social-icons-1"]' );
 		await expect( {
 			selector: '.ast-builder-layout-element[data-section="section-fb-social-icons-1"]',
 			property: 'display',
-		} ).cssValueToBe( `${ hideOnDevices[ 'section-fb-social-icons-1-hide-mobile' ].mobile }` );
+		} ).cssValueToBe( `none` );
 	} );
 } );
