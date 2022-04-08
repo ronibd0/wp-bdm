27217e52d158d026e6a1a93c965ec83c94e882c9
Simran <simranc@bsf.io>
2022-04-08 13:52:20 +0530

    resolved errors, modified code

--- tests/e2e/specs/customizer/footer-builder/above-footer/width.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/width.test.js
@@ -4,7 +4,7 @@ import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Above footer width setting in customizer', () => {
 	it( 'full width should apply correctly', async () => {
-		const AbovefooterWidth = {
+		const aboveFooterWidth = {
 			'hba-footer-layout-width': 'full',
 			'footer-desktop-items': {
 				above: {
@@ -14,23 +14,22 @@ describe( 'Above footer width setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( AbovefooterWidth );
+		await setCustomize( aboveFooterWidth );
 
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
-			property: 'width',
-		} ).cssValueToBe( `${ AbovefooterWidth[ 'hba-footer-layout-width' ] }`,
-		);
+			property: 'max-width',
+		} ).cssValueToBe( `100%` );
 	} );
 
 	it( 'content width should apply correctly', async () => {
-		const AbovefooterWidth = {
+		const aboveFooterWidth = {
 			'hba-footer-layout-width': 'content',
 			'footer-desktop-items': {
 				above: {
@@ -40,17 +39,16 @@ describe( 'Above footer width setting in customizer', () => {
 				},
 			},
 		};
-		await setCustomize( AbovefooterWidth );
+		await setCustomize( aboveFooterWidth );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
-			property: 'width',
-		} ).cssValueToBe( `${ AbovefooterWidth[ 'hba-footer-layout-width' ] }`,
-		);
+			property: 'max-width',
+		} ).cssValueToBe( `1200px` );
 	} );
 } );
