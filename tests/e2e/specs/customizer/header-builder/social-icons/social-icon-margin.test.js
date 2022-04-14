5c359192bd1e746368028fb2153cb71a9668cf6e
Simran <simranc@bsf.io>
2022-04-14 13:25:07 +0530

    modified code

--- tests/e2e/specs/customizer/header-builder/social-icons/social-icon-margin.test.js
+++ tests/e2e/specs/customizer/header-builder/social-icons/social-icon-margin.test.js
@@ -1,16 +1,30 @@
 import { createURL, setBrowserViewport } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
-describe( 'Social Icons in the customizer', () => {
+describe( 'Social icons in the customizer', () => {
 	it( 'social icon margin for desktop should apply correctly', async () => {
 		const socialIconMargin = {
 			'section-hb-social-icons-1-margin': {
 				desktop: {
+					top: '45',
+					right: '45',
+					bottom: '45',
+					left: '45',
+				},
+				tablet: {
 					top: '35',
 					right: '35',
 					bottom: '35',
 					left: '35',
 				},
+				mobile: {
+					top: '25',
+					right: '25',
+					bottom: '25',
+					left: '25',
+				},
 				'desktop-unit': 'px',
+				'tablet-unit': 'px',
+				'mobile-unit': 'px',
 			},
 			'header-desktop-items': {
 				primary: {
@@ -19,9 +33,15 @@ describe( 'Social Icons in the customizer', () => {
 					},
 				},
 			},
+			'header-mobile-items': {
+				primary: {
+					primary_center: {
+						0: 'social-icons-1',
+					},
+				},
+			},
 		};
 		await setCustomize( socialIconMargin );
-
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
@@ -29,51 +49,20 @@ describe( 'Social Icons in the customizer', () => {
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-top',
-		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.top }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.top }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-right',
-		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.right }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.right }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-bottom',
-		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.bottom }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.bottom }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }` );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-left',
-		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }`,
-		);
-	} );
+		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].desktop.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'desktop-unit' ] }` );
 
-	it( 'social icon margin for tablet should apply correctly', async () => {
-		const socialIconMargin = {
-			'section-hb-social-icons-1-margin': {
-				tablet: {
-					top: '25',
-					right: '25',
-					bottom: '25',
-					left: '25',
-				},
-				'tablet-unit': 'px',
-			},
-			'header-mobile-items': {
-				primary: {
-					primary_center: {
-						0: 'social-icons-1',
-
-					},
-				},
-			},
-		};
-		await setCustomize( socialIconMargin );
-
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await page.waitForSelector( '.ast-header-social-1-wrap' );
 		await setBrowserViewport( 'medium' );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
@@ -91,33 +80,7 @@ describe( 'Social Icons in the customizer', () => {
 			selector: '.ast-header-social-1-wrap',
 			property: 'margin-left',
 		} ).cssValueToBe( `${ socialIconMargin[ 'section-hb-social-icons-1-margin' ].tablet.left }${ socialIconMargin[ 'section-hb-social-icons-1-margin' ][ 'tablet-unit' ] }` );
-	} );
 
-	it( 'social icon margin for mobile should apply correctly', async () => {
-		const socialIconMargin = {
-			'section-hb-social-icons-1-margin': {
-				mobile: {
-					top: '25',
-					right: '25',
-					bottom: '25',
-					left: '25',
-				},
-				'mobile-unit': 'px',
-			},
-			'header-mobile-items': {
-				primary: {
-					primary_center: {
-						0: 'social-icons-1',
-					},
-				},
-			},
-		};
-		await setCustomize( socialIconMargin );
-
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await page.waitForSelector( '.ast-header-social-1-wrap' );
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '.ast-header-social-1-wrap',
