0734dda45e1a1f546f87d22aa8b156365496fdf2
Simran <simranc@bsf.io>
2022-04-15 15:07:23 +0530

    updated existing test case

---
+++ tests/e2e/specs/customizer/header-builder/primary-menu/color.test.js
@@ -0,0 +1,48 @@
+import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
+import { setCustomize } from '../../../../utils/customize';
+describe( 'Primary menu setting in customizer', () => {
+	it( 'primary menu color should apply correctly', async () => {
+		const primaryMenuColor = {
+			'header-menu1-color-responsive': {
+				desktop: 'rgb(11, 129, 52)',
+			},
+		};
+		await setCustomize( primaryMenuColor );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test-1' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-builder-menu-1 .menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-1 .menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ primaryMenuColor[ 'header-menu1-color-responsive' ].desktop }` );
+	} );
+
+	it( 'primary menu active color should apply correctly', async () => {
+		const primaryMenuColor = {
+			'header-menu1-a-color-responsive': {
+				desktop: 'rgb(131, 15, 166)',
+			},
+		};
+		await setCustomize( primaryMenuColor );
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( { postType: 'page', title: 'test-1' } );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( '/test-1' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-builder-menu-1 .menu-item.current-menu-item > .menu-link' );
+		await expect( {
+			selector: '.ast-builder-menu-1 .menu-item.current-menu-item > .menu-link',
+			property: 'color',
+		} ).cssValueToBe( `${ primaryMenuColor[ 'header-menu1-a-color-responsive' ].desktop }` );
+	} );
+} );
