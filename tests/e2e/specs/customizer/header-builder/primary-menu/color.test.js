66ca50c29e5a6504a590217daa080aafa97673f9
Snehal Kolhe <snehalk@bsf.io>
2022-04-21 09:56:34 +0530

    modified code to dfix timeout error

--- tests/e2e/specs/customizer/header-builder/primary-menu/color.test.js
+++ tests/e2e/specs/customizer/header-builder/primary-menu/color.test.js
@@ -1,25 +1,27 @@
 import { createURL, createNewPost } from '@wordpress/e2e-test-utils';
 import { publishPost } from '../../../../utils/publish-post';
 import { setCustomize } from '../../../../utils/customize';
+import { createNewMenu } from '../../../../utils/create-menu';
 describe( 'Primary menu setting in customizer', () => {
 	it( 'primary menu color should apply correctly', async () => {
+		await createNewMenu();
 		const primaryMenuColor = {
 			'header-menu1-color-responsive': {
 				desktop: 'rgb(11, 129, 52)',
 			},
 		};
 		await setCustomize( primaryMenuColor );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test-1' } );
-			ppStatus = await publishPost();
-		}
+		// let ppStatus = false;
+		// while ( false === ppStatus ) {
+		// 	await createNewPost( { postType: 'page', title: 'test' } );
+		// 	ppStatus = await publishPost();
+		// }
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.ast-builder-menu-1 .menu-item > .menu-link' );
+		await page.waitForSelector( '#ast-hf-menu-1 .menu-item > a' );
 		await expect( {
-			selector: '.ast-builder-menu-1 .menu-item > .menu-link',
+			selector: '#ast-hf-menu-1 .menu-item > a',
 			property: 'color',
 		} ).cssValueToBe( `${ primaryMenuColor[ 'header-menu1-color-responsive' ].desktop }` );
 	} );
@@ -31,12 +33,12 @@ describe( 'Primary menu setting in customizer', () => {
 			},
 		};
 		await setCustomize( primaryMenuColor );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test-1' } );
-			ppStatus = await publishPost();
-		}
-		await page.goto( createURL( '/test-1' ), {
+		// let ppStatus = false;
+		// while ( false === ppStatus ) {
+		// 	await createNewPost( { postType: 'page', title: 'test-1' } );
+		// 	ppStatus = await publishPost();
+		// }
+		await page.goto( createURL( 'test-page' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await page.waitForSelector( '.ast-builder-menu-1 .menu-item.current-menu-item > .menu-link' );
