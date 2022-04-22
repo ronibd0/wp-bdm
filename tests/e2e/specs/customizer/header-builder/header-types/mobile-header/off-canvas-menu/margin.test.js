ae5606254e6cacb4806530d6e86b62836da3e705
Simran <simranc@bsf.io>
2022-04-22 11:41:29 +0530

    changed selector

--- tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/margin.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/margin.test.js
@@ -3,7 +3,7 @@ import { setCustomize } from '../../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../../utils/set-browser-viewport';
 import { publishPost } from '../../../../../../utils/publish-post';
 describe( 'Off canvas menu margin settings in the customizer', () => {
-	it( 'margin should apply corectly for after header', async () => {
+	it( 'margin should apply correctly for after header', async () => {
 		const offCanvasMargin = {
 			'header-mobile-menu-margin': {
 				tablet: {
@@ -36,38 +36,38 @@ describe( 'Off canvas menu margin settings in the customizer', () => {
 		await setBrowserViewport( 'medium' );
 		await page.click( '.main-header-menu-toggle' );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-top',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].tablet.top }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-right',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].tablet.right }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-bottom',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].tablet.bottom }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-left',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].tablet.left }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'tablet-unit' ] }` );
 
 		await setBrowserViewport( 'small' );
 		await page.click( '.main-header-menu-toggle' );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-top',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].mobile.top }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-right',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].mobile.right }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-bottom',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].mobile.bottom }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: '.ast-builder-menu-mobile .main-header-menu',
+			selector: '.ast-builder-menu-mobile .main-header-menu, .ast-header-break-point .ast-builder-menu-mobile .main-header-menu',
 			property: 'margin-left',
 		} ).cssValueToBe( `${ offCanvasMargin[ 'header-mobile-menu-margin' ].mobile.left }${ offCanvasMargin[ 'header-mobile-menu-margin' ][ 'mobile-unit' ] }` );
 	} );
