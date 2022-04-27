c60cc0e7d25d9a8c659a21b11cbdc766539d82e7
Simran <simranc@bsf.io>
2022-04-27 09:44:04 +0530

    added code for line height * font size

--- tests/e2e/specs/customizer/header-builder/elements/button/font.test.js
+++ tests/e2e/specs/customizer/header-builder/elements/button/font.test.js
@@ -3,12 +3,12 @@ import { setCustomize } from '../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../utils/set-browser-viewport';
 import { responsiveFontSize } from '../../../../../utils/responsive-utils';
 describe( 'Header builder button setting in customizer', () => {
-	it( 'border width, color and radius should apply correctly', async () => {
+	it( 'button font should apply correctly', async () => {
 		const buttonFont = {
 			'header-button1-font-family': 'Aubrey, display',
 			'header-button1-font-weight': 400,
 			'header-button1-text-transform': 'uppercase',
-			'header-button1-line-height': '10px',
+			'header-button1-line-height': '2',
 			'header-button1-letter-spacing': 10,
 			'header-button1-font-size': {
 				desktop: 25,
@@ -56,7 +56,7 @@ describe( 'Header builder button setting in customizer', () => {
 		await expect( {
 			selector: '.ast-header-button-1[data-section*="section-hb-button-"] .ast-builder-button-wrap .ast-custom-button',
 			property: 'line-height',
-		} ).cssValueToBe( `${ buttonFont[ 'header-button1-line-height' ] }` );
+		} ).cssValueToBe( `${ buttonFont[ 'header-button1-line-height' ] * buttonFont[ 'header-button1-font-size' ].desktop }` + 'px' );
 
 		await expect( {
 			selector: '.ast-header-button-1[data-section*="section-hb-button-"] .ast-builder-button-wrap .ast-custom-button',
@@ -72,14 +72,12 @@ describe( 'Header builder button setting in customizer', () => {
 		await expect( {
 			selector: '.ast-header-button-1[data-section*="section-hb-button-"] .ast-builder-button-wrap .ast-custom-button',
 			property: 'font-size',
-		} ).cssValueToBe( `${ await responsiveFontSize( buttonFont[ 'header-button1-font-size' ].tablet,
-		) }${ buttonFont[ 'header-button1-font-size' ][ 'tablet-unit' ] }` );
+		} ).cssValueToBe( `${ buttonFont[ 'header-button1-font-size' ].tabelt }${ buttonFont[ 'header-button1-font-size' ][ 'tabelt-unit' ] }` );
 
 		await setBrowserViewport( 'small' );
 		await expect( {
 			selector: '.ast-header-button-1[data-section*="section-hb-button-"] .ast-builder-button-wrap .ast-custom-button',
 			property: 'font-size',
-		} ).cssValueToBe( `${ await responsiveFontSize( buttonFont[ 'header-button1-font-size' ].mobile,
-		) }${ buttonFont[ 'header-button1-font-size' ][ 'mobile-unit' ] }` );
+		} ).cssValueToBe( `${ buttonFont[ 'header-button1-font-size' ].mobile }${ buttonFont[ 'header-button1-font-size' ][ 'mobile-unit' ] }` );
 	} );
 } );
