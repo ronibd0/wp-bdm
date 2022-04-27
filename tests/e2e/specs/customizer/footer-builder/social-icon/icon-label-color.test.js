1d980d524d9f1ae303b6c217bc7348a1c5b7deb8
Simran <simranc@bsf.io>
2022-04-27 15:49:24 +0530

    modified some code

--- tests/e2e/specs/customizer/footer-builder/social-icon/icon-label-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/social-icon/icon-label-color.test.js
@@ -3,13 +3,11 @@ import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Social icons show label in the customizer', () => {
-	it( 'icon label color should apply correctly', async () => {
+	it( 'icon label color for desktop should apply correctly', async () => {
 		const socialIconLabelColor = {
 			'footer-social-1-label-toggle': '1',
 			'footer-social-1-label-color': {
-				desktop: 'rgb(191, 28, 28)',
-				tablet: 'rgb(64, 1, 1)',
-				mobile: 'rgb(121, 7, 7)',
+				desktop: 'rgb(118, 65, 4)',
 			},
 			'footer-desktop-items': {
 				primary: {
@@ -30,65 +28,59 @@ describe( 'Social icons show label in the customizer', () => {
 			selector: '.ast-footer-social-1-wrap .ast-social-color-type-custom .social-item-label',
 			property: 'color',
 		} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-color' ].desktop }` );
+	} );
 
+	it( 'icon label color for tablet should apply correctly', async () => {
+		const socialIconLabelColor = {
+			'footer-social-1-label-toggle': '1',
+			'footer-social-1-label-color': {
+				tablet: 'rgb(126, 2, 103)',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( socialIconLabelColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-footer-social-1-wrap .ast-social-color-type-custom .social-item-label' );
 		await expect( {
 			selector: '.ast-footer-social-1-wrap .ast-social-color-type-custom .social-item-label',
 			property: 'color',
 		} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-color' ].tablet }` );
+	} );
 
+	it( 'icon label color for mobile should apply correctly', async () => {
+		const socialIconLabelColor = {
+			'footer-social-1-label-toggle': '1',
+			'footer-social-1-label-color': {
+				mobile: 'rgb(3, 104, 23)',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+					},
+				},
+			},
+		};
+		await setCustomize( socialIconLabelColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.ast-footer-social-1-wrap .ast-social-color-type-custom .social-item-label' );
 		await expect( {
 			selector: '.ast-footer-social-1-wrap .ast-social-color-type-custom .social-item-label',
 			property: 'color',
 		} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-color' ].mobile }` );
 	} );
-
-	// eslint-disable-next-line eslint-comments/disable-enable-pair
-	/* eslint-disable jest/no-commented-out-tests */
-	// GitHub action E2E fail case
-	// it( 'icon label hover color should apply correctly', async () => {
-	// 	const socialIconLabelColor = {
-	// 		'footer-social-1-label-toggle': '1',
-	// 		'footer-social-1-label-h-color': {
-	// 			desktop: 'rgb(175, 138, 5)',
-	// 			tablet: 'rgb(28, 156, 136)',
-	// 			mobile: 'rgb(35, 135, 14)',
-	// 		},
-	// 		'footer-desktop-items': {
-	// 			primary: {
-	// 				primary_2: {
-	// 					0: 'social-icons-1',
-	// 				},
-	// 			},
-	// 		},
-	// 	};
-	// 	await setCustomize( socialIconLabelColor );
-	// 	await page.goto( createURL( '/' ), {
-	// 		waitUntil: 'networkidle0',
-	// 	} );
-	// 	await setBrowserViewport( 'large' );
-	// 	await scrollToElement( '#colophon' );
-	// 	await page.waitForSelector( '.ast-footer-social-1-wrap .ast-builder-social-element:hover .social-item-label' );
-	// 	await expect( {
-	// 		selector: '.ast-footer-social-1-wrap .ast-builder-social-element:hover .social-item-label',
-	// 		property: 'color',
-	// 	} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-h-color' ].desktop }` );
-
-	// 	await setBrowserViewport( 'medium' );
-	// 	await scrollToElement( '#colophon' );
-	// 	await expect( {
-	// 		selector: '.ast-footer-social-1-wrap .ast-builder-social-element:hover .social-item-label',
-	// 		property: 'color',
-	// 	} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-h-color' ].tablet }` );
-
-	// 	await setBrowserViewport( 'small' );
-	// 	await scrollToElement( '#colophon' );
-	// 	await expect( {
-	// 		selector: '.ast-footer-social-1-wrap .ast-builder-social-element:hover .social-item-label',
-	// 		property: 'color',
-	// 	} ).cssValueToBe( `${ socialIconLabelColor[ 'footer-social-1-label-h-color' ].mobile }` );
-	// } );
 } );
