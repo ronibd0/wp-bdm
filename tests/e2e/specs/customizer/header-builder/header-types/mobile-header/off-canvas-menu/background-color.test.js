4b5ae192d08de2135993092ac1ee2ce091b02089
Simran <simranc@bsf.io>
2022-04-28 12:07:06 +0530

    Resolved PR commets

--- tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/background-color.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/background-color.test.js
@@ -3,29 +3,32 @@ import { setCustomize } from '../../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../../utils/set-browser-viewport';
 import { publishPost } from '../../../../../../utils/publish-post';
 describe( 'Off canvas menu background color settings in the customizer', () => {
-	it( 'background color should apply correctly for after header', async () => {
+	it( 'background color should apply correctly', async () => {
 		const offCanvasBgColor = {
 			'header-mobile-menu-bg-obj-responsive': {
 				desktop: {
 					'background-color': '',
-					'background-repeat': '',
-					'background-position': '',
-					'background-size': '',
-					'background-attachment': '',
+					'background-repeat': 'repeat',
+					'background-position': 'center center',
+					'background-size': 'auto',
+					'background-attachment': 'scroll',
+					'background-type': 'color',
 				},
 				tablet: {
-					'background-color': 'rgb(239, 239, 239)',
+					'background-color': 'rgb(246, 235, 249)',
 					'background-repeat': 'repeat',
 					'background-position': 'center center',
 					'background-size': 'auto',
 					'background-attachment': 'scroll',
+					'background-type': 'color',
 				},
 				mobile: {
-					'background-color': 'rgb(239, 240, 237)',
-					'background-repeat': 'repeat',
+					'background-color': 'rgb(243, 243, 185)'
+,					'background-repeat': 'repeat',
 					'background-position': 'center center',
 					'background-size': 'auto',
 					'background-attachment': 'scroll',
+					'background-type': 'color',
 				},
 			},
 		};
@@ -55,41 +58,5 @@ describe( 'Off canvas menu background color settings in the customizer', () => {
 			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu',
 			property: 'background-color',
 		} ).cssValueToBe( `${ offCanvasBgColor[ 'header-mobile-menu-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
-	} );
-
-	it( 'active background color should apply correctly for after header', async () => {
-		const offCanvasBgColor = {
-			'header-mobile-menu-a-bg-color-responsive': {
-				desktop: '',
-				tablet: 'rgb(255, 227, 227)',
-				mobile: 'rgb(249, 235, 249)',
-			},
-		};
-		await setCustomize( offCanvasBgColor );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test-1' } );
-			ppStatus = await publishPost();
-			await createNewPost( { postType: 'page', title: 'test-2' } );
-			ppStatus = await publishPost();
-		}
-		await page.goto( createURL( '/test-1' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await setBrowserViewport( 'medium' );
-		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link' );
-		await expect( {
-			selector: '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link',
-			property: 'background-color',
-		} ).cssValueToBe( `${ offCanvasBgColor[ 'header-mobile-menu-a-bg-color-responsive' ].tablet[ 'background-color' ] }` );
-
-		await setBrowserViewport( 'small' );
-		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link' );
-		await expect( {
-			selector: '.ast-builder-menu-mobile .main-navigation .menu-item.current-menu-item > .menu-link',
-			property: 'background-color',
-		} ).cssValueToBe( `${ offCanvasBgColor[ 'header-mobile-menu-a-bg-color-responsive' ].mobile[ 'background-color' ] }` );
 	} );
 } );
