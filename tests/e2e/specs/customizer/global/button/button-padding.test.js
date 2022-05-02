36bb03fd170c0030fb508144ed843791ec274406
Snehal Kolhe <snehalk@bsf.io>
2022-05-02 12:28:16 +0530

    Addedmultiple selectors

--- tests/e2e/specs/customizer/global/button/button-padding.test.js
+++ tests/e2e/specs/customizer/global/button/button-padding.test.js
@@ -1,6 +1,7 @@
 import { setCustomize } from '../../../../utils/customize';
-import { createURL } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost,insertBlock } from '@wordpress/e2e-test-utils';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+import { publishPost } from '../../../../utils/publish-post';
 describe( 'Global button setting under the Customizer', () => {
 	it( 'button padding should apply correctly', async () => {
 		const buttonPadding = {
@@ -12,16 +13,16 @@ describe( 'Global button setting under the Customizer', () => {
 					left: '80',
 				},
 				tablet: {
-					top: '80',
-					right: '80',
-					bottom: '80',
-					left: '80',
+					top: '40',
+					right: '40',
+					bottom: '40',
+					left: '40',
 				},
 				mobile: {
-					top: '80',
-					right: '80',
-					bottom: '80',
-					left: '80',
+					top: '30',
+					right: '30',
+					bottom: '30',
+					left: '30',
 				},
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
@@ -29,60 +30,70 @@ describe( 'Global button setting under the Customizer', () => {
 			},
 		};
 		await setCustomize( buttonPadding );
-		await page.goto( createURL( '/' ), {
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( {
+				postType: 'post',
+				title: 'buttonPadding',
+			} );
+			await insertBlock( 'Buttons' );
+			await page.keyboard.type( 'Login' );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( 'buttonPadding' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button' );
+		await page.waitForSelector( '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button' );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-top',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].desktop.top }${ buttonPadding[ 'theme-button-padding' ][ 'desktop-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-right',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].desktop.right }${ buttonPadding[ 'theme-button-padding' ][ 'desktop-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-bottom',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].desktop.bottom }${ buttonPadding[ 'theme-button-padding' ][ 'desktop-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-left',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].desktop.left }${ buttonPadding[ 'theme-button-padding' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-top',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].tablet.top }${ buttonPadding[ 'theme-button-padding' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-right',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].tablet.right }${ buttonPadding[ 'theme-button-padding' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-bottom',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].tablet.bottom }${ buttonPadding[ 'theme-button-padding' ][ 'tablet-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-left',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].tablet.left }${ buttonPadding[ 'theme-button-padding' ][ 'tablet-unit' ] }` );
 
 		await setBrowserViewport( 'small' );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-top',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].mobile.top }${ buttonPadding[ 'theme-button-padding' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-right',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].mobile.right }${ buttonPadding[ 'theme-button-padding' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-bottom',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].mobile.bottom }${ buttonPadding[ 'theme-button-padding' ][ 'mobile-unit' ] }` );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-buttons .wp-block-button .wp-block-button__link, #block-2 .wp-block-search__button',
 			property: 'padding-left',
 		} ).cssValueToBe( `${ buttonPadding[ 'theme-button-padding' ].mobile.left }${ buttonPadding[ 'theme-button-padding' ][ 'mobile-unit' ] }` );
 	} );
