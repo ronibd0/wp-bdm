e417f400dbf0c92f15a846615fa838b43c173f48
Snehal Kolhe <snehalk@bsf.io>
2022-05-02 12:09:19 +0530

    added multiple selectors

--- tests/e2e/specs/customizer/global/button/button-border.test.js
+++ tests/e2e/specs/customizer/global/button/button-border.test.js
@@ -1,5 +1,6 @@
 import { setCustomize } from '../../../../utils/customize';
-import { createURL } from '@wordpress/e2e-test-utils';
+import { createURL, createNewPost, insertBlock } from '@wordpress/e2e-test-utils';
+import { publishPost } from '../../../../utils/publish-post';
 describe( 'Global button setting under the Customizer', () => {
 	it( 'button border width should apply correctly', async () => {
 		const borderWidth = {
@@ -11,24 +12,34 @@ describe( 'Global button setting under the Customizer', () => {
 			},
 		};
 		await setCustomize( borderWidth );
-		await page.goto( createURL( '/' ), {
+		let ppStatus = false;
+		while ( false === ppStatus ) {
+			await createNewPost( {
+				postType: 'post',
+				title: 'buttonBorder',
+			} );
+			await insertBlock( 'Buttons' );
+			await page.keyboard.type( 'Login' );
+			ppStatus = await publishPost();
+		}
+		await page.goto( createURL( 'buttonBorder' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button' );
+		await page.waitForSelector( '#block-2 .wp-block-search__button, .wp-block-button .wp-block-button__link' );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '#block-2 .wp-block-search__button, .wp-block-button .wp-block-button__link',
 			property: 'border-top-width',
 		} ).cssValueToBe( `${ borderWidth[ 'theme-button-border-group-border-size' ].top + 'px' }` );
 		await expect( {
-			selector: '#block-2 .wp-block-search__button',
+			selector: '#block-2 .wp-block-search__button, .wp-block-button .wp-block-button__link',
 			property: 'border-right-width',
 		} ).cssValueToBe( `${ borderWidth[ 'theme-button-border-group-border-size' ].right + 'px' }` );
 		await expect( {
-			selector: '#block-2 .wp-block-search__button',
+			selector: '#block-2 .wp-block-search__button, .wp-block-button .wp-block-button__link',
 			property: 'border-bottom-width',
 		} ).cssValueToBe( `${ borderWidth[ 'theme-button-border-group-border-size' ].bottom + 'px' }` );
 		await expect( {
-			selector: '#block-2 .wp-block-search__button',
+			selector: '#block-2 .wp-block-search__button, .wp-block-button .wp-block-button__link',
 			property: 'border-left-width',
 		} ).cssValueToBe( `${ borderWidth[ 'theme-button-border-group-border-size' ].left + 'px' }` );
 	} );
@@ -38,12 +49,12 @@ describe( 'Global button setting under the Customizer', () => {
 			'theme-button-border-group-border-color': 'rgb(4, 7, 11)',
 		};
 		await setCustomize( borderColor );
-		await page.goto( createURL( '/' ), {
+		await page.goto( createURL( 'buttonBorder' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.wp-block-search__button' );
+		await page.waitForSelector( '.wp-block-search__button, .wp-block-button .wp-block-button__link' );
 		await expect( {
-			selector: '.wp-block-search__button',
+			selector: '.wp-block-search__button, .wp-block-button .wp-block-button__link',
 			property: 'border-color',
 		} ).cssValueToBe( `${ borderColor[ 'theme-button-border-group-border-color' ] }` );
 	} );
@@ -53,12 +64,12 @@ describe( 'Global button setting under the Customizer', () => {
 			'button-radius': '30',
 		};
 		await setCustomize( borderRadius );
-		await page.goto( createURL( '/' ), {
+		await page.goto( createURL( 'buttonBorder' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button' );
+		await page.waitForSelector( '.wp-block-button .wp-block-button__link, form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button' );
 		await expect( {
-			selector: 'form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
+			selector: '.wp-block-button .wp-block-button__link, form[CLASS*="wp-block-search__"].wp-block-search .wp-block-search__inside-wrapper .wp-block-search__button',
 			property: 'border-radius',
 		} ).cssValueToBe( `${ borderRadius[ 'button-radius' ] + 'px' }` );
 	} );
