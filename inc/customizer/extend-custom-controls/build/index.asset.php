09b942991f44be91e8d00dfc66347633a04a25a4
Navanath Bhosale <navanathb@bsf.io>
2022-05-04 19:53:45 +0530

    chore: build

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => 'b9d5d054c1af898db87e688ab798d6b1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => 'd3c965b96159f79216f3c991d4b17e78');
