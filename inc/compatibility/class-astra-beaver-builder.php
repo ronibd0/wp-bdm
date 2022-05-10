8100e02499301d1b68cff282b9dbba5424b9aea5
Navanath Bhosale <navanathb@bsf.io>
2022-05-10 19:24:03 +0530

    chore: resolving code analysis

--- inc/compatibility/class-astra-beaver-builder.php
+++ inc/compatibility/class-astra-beaver-builder.php
@@ -96,18 +96,39 @@ if ( ! class_exists( 'Astra_Beaver_Builder' ) ) :
 
 					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 					update_post_meta( $id, '_astra_content_layout_flag', 'disabled' );
-					update_post_meta( $id, 'site-post-title', 'disabled' );
-					update_post_meta( $id, 'ast-title-bar-display', 'disabled' );
-					update_post_meta( $id, 'ast-featured-img', 'disabled' );
-					$content_layout = get_post_meta( $id, 'site-content-layout', true );
-					if ( empty( $content_layout ) || 'default' == $content_layout ) {
-						update_post_meta( $id, 'site-content-layout', 'page-builder' );
-					}
-					$sidebar_layout = get_post_meta( $id, 'site-sidebar-layout', true );
-					if ( empty( $sidebar_layout ) || 'default' == $sidebar_layout ) {
-						update_post_meta( $id, 'site-sidebar-layout', 'no-sidebar' );
-					}
 					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					update_post_meta( $id, 'site-post-title', 'disabled' );
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					update_post_meta( $id, 'ast-title-bar-display', 'disabled' );
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					update_post_meta( $id, 'ast-featured-img', 'disabled' );
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					$content_layout = get_post_meta( $id, 'site-content-layout', true );
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					if ( empty( $content_layout ) || 'default' == $content_layout ) {
+						/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+						update_post_meta( $id, 'site-content-layout', 'page-builder' );
+						/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					}
+
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					$sidebar_layout = get_post_meta( $id, 'site-sidebar-layout', true );
+					/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+					if ( empty( $sidebar_layout ) || 'default' == $sidebar_layout ) {
+						/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+						update_post_meta( $id, 'site-sidebar-layout', 'no-sidebar' );
+						/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					}
 				}
 			}
 		}
