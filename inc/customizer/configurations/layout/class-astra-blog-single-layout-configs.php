d2403059b702efd2d8e298b66f88fc1bd98e1d68
premanshup <premanshup@bsf.io>
2022-05-10 16:31:43 +0530

    Fix: Psalm errors.

--- inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
+++ inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
@@ -30,6 +30,10 @@ if ( ! class_exists( 'Astra_Blog_Single_Layout_Configs' ) ) {
 		 */
 		public function register_configuration( $configurations, $wp_customize ) {
 
+			/** @psalm-suppress DocblockTypeContradiction */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+			$tab_config = ( true === Astra_Builder_Helper::$is_header_footer_builder_active ) ?
+					Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config;
+
 			$_configs = array(
 
 				/**
@@ -122,8 +126,7 @@ if ( ! class_exists( 'Astra_Blog_Single_Layout_Configs' ) ) {
 					),
 					'priority'          => 25,
 					'divider'           => array( 'ast_class' => 'ast-top-divider ast-bottom-divider' ),
-					'context'           => ( true === Astra_Builder_Helper::$is_header_footer_builder_active ) ?
-					Astra_Builder_Helper::$design_tab_config : Astra_Builder_Helper::$general_tab_config,
+					'context'           => ,
 				),
 
 			);
