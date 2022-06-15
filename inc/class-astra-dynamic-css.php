4204f754902f376a2dc3493800b4a65209f0a372
nileshc-bsf <nileshc@bsf.io>
2022-06-15 11:34:21 +0530

    Fix: Table block not aligned centrally for Legacy block editor setup.

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -1312,6 +1312,18 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 					$parse_css .= astra_parse_css( $mobile_screen_max_gb_css, '', astra_get_mobile_breakpoint() );
 				}
 
+				$is_legacy_setup = ( 'legacy' === astra_get_option( 'wp-blocks-ui' ) ) ? true : false;
+				if ( $is_legacy_setup ) {
+					// Table block align center CSS.
+					$table_block_center_align = array(
+						'.wp-block-table.aligncenter' => array(
+							'margin-left'  => 'auto',
+							'margin-right' => 'auto',
+						),
+					);
+					$parse_css               .= astra_parse_css( $table_block_center_align );
+				}
+
 				if ( self::gutenberg_media_text_block_css_compat() ) {
 					$media_text_block_padding_css = array(
 						// Media & Text block CSS compatibility (min-width: mobile-breakpoint) CSS.
