6868a855f031f416e99aa9f146a825c17ace1b5c
Gaurav Khupse <44017457+GauravKhupse@users.noreply.github.com>
2022-06-15 12:17:46 +0530

    Phpcs done

--- inc/compatibility/class-astra-gutenberg.php
+++ inc/compatibility/class-astra-gutenberg.php
@@ -90,7 +90,7 @@ class Astra_Gutenberg {
 			return $block_content;
 		}
 		/** @psalm-suppress PossiblyUndefinedStringArrayOffset */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-		if ( (isset( $block['blockName']) && 'core/group' === $block['blockName']) && ! empty( $block['attrs'] ) && isset( $block['attrs']['layout'] ) && isset( $block['attrs']['layout']['type'] ) && 'flex' === $block['attrs']['layout']['type'] ) {
+		if ( ( isset( $block['blockName'] ) && 'core/group' === $block['blockName'] ) && ! empty( $block['attrs'] ) && isset( $block['attrs']['layout'] ) && isset( $block['attrs']['layout']['type'] ) && 'flex' === $block['attrs']['layout']['type'] ) {
 			return $block_content;
 		}
 
