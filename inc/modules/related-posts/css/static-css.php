dd2a276c39fb9b495af425401c4940e4d508d777
Navanath Bhosale <navanathb@bsf.io>
2022-06-20 20:03:38 +0530

    fix: related posts margin bottom issue fixed

--- inc/modules/related-posts/css/static-css.php
+++ inc/modules/related-posts/css/static-css.php
@@ -63,7 +63,7 @@ function astra_related_posts_static_css( $dynamic_css ) {
 			$dynamic_css .= '
 				.ast-single-related-posts-container {
 					border-top: 1px solid var(--ast-global-color-6);
-					margin: 2em 0;
+					margin: 2em 0 0;
 				}
 				.ast-single-related-posts-container {
 					padding-top: 2em;
