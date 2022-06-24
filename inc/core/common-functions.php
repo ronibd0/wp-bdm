c333f9de9d92ee565c8b355a35a1299b4ed7acf0
Navanath Bhosale <navanathb@bsf.io>
2022-06-24 14:04:44 +0530

    chore: filter provision updated

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -1584,5 +1584,5 @@ function astra_block_based_legacy_setup() {
  */
 function astra_check_is_structural_setup() {
 	$astra_settings = get_option( ASTRA_THEME_SETTINGS );
-	return isset( $astra_settings['customizer-default-layout-update'] ) ? false : true;
+	return apply_filters( 'astra_get_option_customizer-default-layout-update', isset( $astra_settings['customizer-default-layout-update'] ) ? false : true );
 }
