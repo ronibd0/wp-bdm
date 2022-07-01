5608285768f57c2a3e1035981c2dad8af726a277
Navanath Bhosale <navanathb@bsf.io>
2022-07-01 09:43:46 +0530

    chore: default sidebar setting removed from default & color pickers gap reduced

--- inc/customizer/configurations/layout/class-astra-sidebar-layout-configs.php
+++ inc/customizer/configurations/layout/class-astra-sidebar-layout-configs.php
@@ -45,10 +45,6 @@ if ( ! class_exists( 'Astra_Sidebar_Layout_Configs' ) ) {
 					'priority'          => 5,
 					'title'             => __( 'Default Layout', 'astra' ),
 					'choices'           => array(
-						'default'       => array(
-							'label' => __( 'Default', 'astra' ),
-							'path'  => ( class_exists( 'Astra_Builder_UI_Controller' ) ) ? Astra_Builder_UI_Controller::fetch_svg_icon( 'layout-default', false ) : '',
-						),
 						'no-sidebar'    => array(
 							'label' => __( 'No Sidebar', 'astra' ),
 							'path'  => ( class_exists( 'Astra_Builder_UI_Controller' ) ) ? Astra_Builder_UI_Controller::fetch_svg_icon( 'no-sidebar', false ) : '',
