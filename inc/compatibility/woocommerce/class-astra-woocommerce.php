4e0b0bbfd92d8959da401b84d680825b07d796b9
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-07-09 16:39:16 +0530

    Chor : Ran PHPCS

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -2290,7 +2290,7 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			if ( $cart_button_text ) {
 				?>
 					<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="checkout-button button alt wc-forward">
-					<?php esc_attr_e( $cart_button_text ); ?>
+					<?php echo esc_attr( $cart_button_text ); ?>
 					</a>
 				<?php
 			}
