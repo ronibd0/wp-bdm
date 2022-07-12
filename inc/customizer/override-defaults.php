e1e636332742a1665f7f67bd91d2e7fae63e31f6
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-07-12 12:11:35 +0530

    refactor : Removed unwanted description added by woocommerce

--- inc/customizer/override-defaults.php
+++ inc/customizer/override-defaults.php
@@ -65,5 +65,6 @@ if ( class_exists( 'WooCommerce' ) ) {
 	$wp_customize->get_section( 'woocommerce_store_notice' )->priority    = 26;
 	$wp_customize->get_section( 'woocommerce_product_catalog' )->priority = 11;
 	$wp_customize->get_section( 'woocommerce_checkout' )->priority        = 21;
+	$wp_customize->get_section( 'woocommerce_checkout' )->description     = '';
 	$wp_customize->get_panel( 'woocommerce' )->priority                   = 70;
 }
