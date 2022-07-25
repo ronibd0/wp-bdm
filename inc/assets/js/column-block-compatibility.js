9667e9d4ae1f7d69935d248ec4339d5b0dc70334
Gaurav Khupse <44017457+GauravKhupse@users.noreply.github.com>
2022-07-25 14:58:36 +0530

    Fixed row block converted into group on onload editor view

--- inc/assets/js/column-block-compatibility.js
+++ inc/assets/js/column-block-compatibility.js
@@ -39,7 +39,7 @@ wp.hooks.addFilter(
 			return attributes;
 		}
 
-		if (blockType.name == 'core/group' && undefined != attributes.layout && false == attributes.layout.inherit ) {
+		if (blockType.name == 'core/group' && undefined != attributes.layout && 'flex' === attributes.layout.type) {
 			return attributes;
 		}
 
