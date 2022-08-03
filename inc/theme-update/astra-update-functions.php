ce1236fff26103688a3ede045b7cb20e98e5c64b
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-08-03 09:02:34 +0530

    chor : removed var dump added due to debugging

--- inc/theme-update/astra-update-functions.php
+++ inc/theme-update/astra-update-functions.php
@@ -1039,7 +1039,6 @@ function astra_responsive_shop_content_alignment() {
  * @return void
  */
 function astra_shop_style_design_layout() {
-	var_dump('innn');
 
 	$theme_options = get_option( 'astra-settings', array() );
 	if ( ! isset( $theme_options['woo-shop-style-flag'] ) ) {
