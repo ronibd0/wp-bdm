0c42e76b4face08b4fbf2d409f194f7f0be98492
Neeraj <neerajmalwal10@gmail.com>
2022-08-04 18:50:11 +0530

    minified files, and change in opacity

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '0df94eb9a13d4c04d002dbd6ff27a519');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '53362ecf8b3e5e2802084c95a9dd6477');
