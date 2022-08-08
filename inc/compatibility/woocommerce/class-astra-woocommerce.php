e0c4ac1b25b5063d6ef6549add03694f4116a77a
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-08-08 14:24:31 +0530

    chor : fix psalm issues

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -2371,9 +2371,8 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			}
 
 			// Don't show if the user cannot edit a given customize_changeset post currently being previewed.
-			/** @psalm-suppress PossiblyNullPropertyFetch PossiblyNullArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			if ( is_customize_preview() && $wp_customize->changeset_post_id()
-				&& ! current_user_can( get_post_type_object( 'customize_changeset' )->cap->edit_post, $wp_customize->changeset_post_id() )
+				&& ! current_user_can( /** @psalm-suppress PossiblyNullPropertyFetch PossiblyNullArgument */  get_post_type_object( 'customize_changeset' )->cap->edit_post, $wp_customize->changeset_post_id() ) // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			) {
 				return;
 			}
@@ -2382,7 +2381,8 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			if ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) {
 
 				/** @psalm-suppress PossiblyInvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-				$http_host   = esc_url_raw( wp_unslash( $_SERVER['HTTP_HOST'] ) ); 
+				$http_host = esc_url_raw( wp_unslash( $_SERVER['HTTP_HOST'] ) ); 
+				/** @psalm-suppress PossiblyInvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				$request_uri = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
 
 				$current_url = $http_host . $request_uri;
