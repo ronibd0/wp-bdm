f4c78000bd635b4c7365ce9f87e35c29f3116043
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-08-08 15:21:37 +0530

    chor : fix psalm issues

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -2371,11 +2371,12 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			}
 
 			// Don't show if the user cannot edit a given customize_changeset post currently being previewed.
-			/** @psalm-suppress PossiblyNullPropertyFetch PossiblyNullArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-			$edit_post = get_post_type_object( 'customize_changeset' )->cap->edit_post;
+			/** @psalm-suppress PossiblyNullArgument PossiblyNullPropertyFetch */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+			$edit_post = current_user_can( get_post_type_object( 'customize_changeset' )->cap->edit_post, $wp_customize->changeset_post_id() );
+			/** @psalm-suppress PossiblyNullArgument PossiblyNullPropertyFetch */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 
 			if ( is_customize_preview() && $wp_customize->changeset_post_id()
-				&& ! current_user_can( /** @psalm-suppress PossiblyNullArgument */ $edit_post, $wp_customize->changeset_post_id() ) // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+				&& ! $edit_post 
 			) {
 				return;
 			}
