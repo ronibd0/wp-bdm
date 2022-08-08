8ca7ad7d463ad6a9b6c9f74e74f299889a5e7168
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-08-08 16:17:21 +0530

    chor : fix psalm issues

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -2374,8 +2374,9 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 		public function astra_woo_woocommerce_template_product_title( $product_type ) {
 
 			if ( 'quick-view' === $product_type ) {
-
-				echo '<a href="' . /** @psalm-suppress PossiblyFalseArgument */ esc_url( get_the_permalink() ) . '" class="ast-loop-product__link">';  // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+				/** @psalm-suppress PossiblyFalseArgument  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+				echo '<a href="' . esc_url( get_the_permalink() ) . '" class="ast-loop-product__link">'; 
+				/** @psalm-suppress PossiblyFalseArgument  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			}
 
 			woocommerce_template_single_title();
@@ -2408,9 +2409,14 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 
 			if ( is_array( $single_structure ) && ! empty( $single_structure ) ) {
 				
-				/** @psalm-suppress UndefinedClass InvalidScalarArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+				// @codingStandardsIgnoreStart
+				/**
+				 * @psalm-suppress UndefinedClass 
+				 * @psalm-suppress InvalidScalarArgument  
+				 */
 				$astra_addons_condition = defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'woocommerce' ) && is_callable( ASTRA_Ext_WooCommerce_Markup::get_instance(), 'single_product_content_structure' );
-				/** @psalm-suppress UndefinedClass InvalidScalarArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+				// @codingStandardsIgnoreEnd
 				foreach ( $single_structure as $value ) {
 
 					switch ( $value ) {
@@ -2448,14 +2454,17 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 							woocommerce_template_single_add_to_cart();
 							do_action( 'astra_woo_single_add_to_cart_after' );
 							break;
-						/** @psalm-suppress RedundantCondition */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+							/** @psalm-suppress RedundantCondition */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 						case 'summary-extras' && $astra_addons_condition:
+							/** @psalm-suppress RedundantCondition */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 							do_action( 'astra_woo_single_extras_before' );
 							/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 							ASTRA_Ext_WooCommerce_Markup::get_instance()->single_product_extras();
 							do_action( 'astra_woo_single_extras_after' );
 							break;
+							/** @psalm-suppress RedundantCondition  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 						case 'single-product-payments' && $astra_addons_condition:
+							/** @psalm-suppress RedundantCondition  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 							do_action( 'astra_woo_single_product_payments_before' );
 							/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 							ASTRA_Ext_WooCommerce_Markup::get_instance()->woocommerce_product_single_payments();
