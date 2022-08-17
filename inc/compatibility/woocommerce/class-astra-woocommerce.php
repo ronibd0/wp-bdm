245e05e29f7965d1c635d3688d0935128565bd05
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-08-17 14:34:46 +0530

    fix : conditionally load if astra addon version matches

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -131,16 +131,19 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 
 			add_action( 'wp', array( $this, 'woocommerce_proceed_to_checkout_button' ) );
 
-			// Remove Default actions.
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+			if ( astra_addon_check_version( '3.9.2', '>=' ) ) {
 
-			/* Add single product content */
-			add_action( 'woocommerce_single_product_summary', array( $this, 'single_product_content_structure' ), 10 );
+				// Remove Default actions.
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+				remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+
+				/* Add single product content */
+				add_action( 'woocommerce_single_product_summary', array( $this, 'single_product_content_structure' ), 10 );
+			}
 		}
 
 		/**
