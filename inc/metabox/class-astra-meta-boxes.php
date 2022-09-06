eb418f69f21379eea96be015a7536044aa324ef3
Yogesh Sambare <bloggingscan@gmail.com>
2022-09-06 11:44:34 +0530

    fix phpcs errors

--- inc/metabox/class-astra-meta-boxes.php
+++ inc/metabox/class-astra-meta-boxes.php
@@ -478,14 +478,14 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 			
 			global $post;
 			$astra_hide_content_layout_theme_settings = false;
-			$astra_hide_sidebar_theme_settings = false;
-			if ($post) {
+			$astra_hide_sidebar_theme_settings        = false;
+			if ( $post ) {
 				$page_for_posts = get_option( 'page_for_posts' );
 
-				if ($post->ID == $page_for_posts) {
+				if ( $post->ID == $page_for_posts ) {
 
 					$astra_hide_content_layout_theme_settings = true;
-					$astra_hide_sidebar_theme_settings = true;
+					$astra_hide_sidebar_theme_settings        = true;
 				}
 			}
 			
@@ -515,7 +515,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 					'is_addon_activated'       => defined( 'ASTRA_EXT_VER' ) ? true : false,
 					'sticky_addon_enabled'     => ( $astra_ext_extension_class_exists && Astra_Ext_Extension::is_active( 'sticky-header' ) ) ? true : false,
 					'register_astra_metabox'   => apply_filters( 'astra_settings_metabox_register', true ),
-					'is_hide_sidebar'   	   => $astra_hide_content_layout_theme_settings,
+					'is_hide_sidebar'          => $astra_hide_content_layout_theme_settings,
 					'is_hide_contnet_layout'   => $astra_hide_sidebar_theme_settings,
 				)
 			);
