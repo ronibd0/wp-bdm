c168506da2af811f8716bbd306f35c04a2ddcbf6
Neeraj <neerajmalwal10@gmail.com>
2022-10-06 14:34:28 +0530

    fix: css patch

--- inc/core/class-astra-wp-editor-css.php
+++ inc/core/class-astra-wp-editor-css.php
@@ -520,7 +520,8 @@ class Astra_WP_Editor_CSS {
 
 		// Boxed, Content Boxed, Contained.
 		$desktop_css['body .editor-styles-wrapper.editor-styles-wrapper .edit-post-visual-editor__post-title-wrapper > .alignfull, body .editor-styles-wrapper.editor-styles-wrapper .block-editor-block-list__layout.is-root-container > .alignfull'] = array(
-			'max-width' => 'var(--wp--custom--ast-content-width-size)',
+			'max-width'            => 'var(--wp--custom--ast-content-width-size)',
+			'padding-' . $ltr_left => '0 !important',
 		);
 		// Full Width/Stretched.
 		$desktop_css['.editor-styles-wrapper .wp-block-uagb-container.alignfull.uagb-is-root-container .uagb-container-inner-blocks-wrap'] = array(
