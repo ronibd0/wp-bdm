9376afcf7f4d2c550a0aa936f94d30a6671129a2
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-10-07 16:40:33 +0530

    feat : remove input dotted focus

--- inc/core/class-astra-theme-options.php
+++ inc/core/class-astra-theme-options.php
@@ -432,6 +432,7 @@ if ( ! class_exists( 'Astra_Theme_Options' ) ) {
 					'body-line-height'                     => '',
 					'para-margin-bottom'                   => '',
 					'underline-content-links'              => true,
+					'underline-content-links'              => false,
 					'body-text-transform'                  => '',
 					'headings-font-family'                 => 'inherit',
 					'headings-font-weight'                 => 'inherit',
