7a7a3a76d4baf22459eba4cb9587cd9f3fb97615
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-10-07 18:25:32 +0530

    Update inc/core/class-astra-theme-options.php
    Co-authored-by: Navanath Bhosale <navanathb@bsf.io>

--- inc/core/class-astra-theme-options.php
+++ inc/core/class-astra-theme-options.php
@@ -432,7 +432,7 @@ if ( ! class_exists( 'Astra_Theme_Options' ) ) {
 					'body-line-height'                     => '',
 					'para-margin-bottom'                   => '',
 					'underline-content-links'              => true,
-					'underline-content-links'              => false,
+					'disable-dotted-focus'              => false,
 					'body-text-transform'                  => '',
 					'headings-font-family'                 => 'inherit',
 					'headings-font-weight'                 => 'inherit',
