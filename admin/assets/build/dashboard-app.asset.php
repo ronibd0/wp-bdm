e62400be92de123f58aca112db852b03545e5e2e
er-roshan <roshan.adhikari.se@gmail.com>
2022-11-11 13:18:42 +0545

    AST-2445 Changed string when astra addon is activated

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '37edb800610801e27aea');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '904f4d60c54687c4f271');
