818d7e591de799b652d8909fc325f79d758711d5
er-roshan <roshan.adhikari.se@gmail.com>
2022-11-13 17:27:12 +0545

    AST-2465 Fixed issue with custom layout list table

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b3babcb3ecd2bf8ac6ca');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '8dd77cdd14507e8c3a2b');
