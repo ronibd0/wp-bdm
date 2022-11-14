5faca5a85bb3174778da5ed68772a0bcbc739ebb
neerajmasai <neerajmalwal10@gmail.com>
2022-11-14 16:49:18 +0530

    fix: code refactoring

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -1625,8 +1625,8 @@ function astra_load_woocommerce_login_form_password_icon() {
  * Function to add narrow width properties in the frontend.
  *
  * @since x.x.x
- * @param string  $location Location where this needs to be added.
- * @param integer $narrow_container_max_width  Container Width in px.
+ * @param string  $location container layout for single-post, archives, pages, page meta.
+ * @param integer $narrow_container_max_width  dynamic container width in px.
  * @return string|null css based on $location and $narrow_container_max_width.
  */
 function astra_narrow_container_width( $location, $narrow_container_max_width ) {
