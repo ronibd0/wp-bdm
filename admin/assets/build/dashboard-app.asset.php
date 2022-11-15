b3328413a5c6854051e6d4c6b57831964f517033
Navanath Bhosale <navanathb@bsf.io>
2022-11-15 12:07:44 +0530

    fix: upgrade to pro notices issue fixed

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1c219ba82ca895051a37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'fe1e4a8db2eb341d4a39');
