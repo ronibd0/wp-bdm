5aeca796d5a3afb272bb17d2513204ee57050e1e
Navanath Bhosale <navanathb@bsf.io>
2022-11-15 12:25:48 +0530

    chore: build

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'fe1e4a8db2eb341d4a39');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '831de7d2b1cb2e366626');
