3c23adb422e31835991fe918995f870985840ee9
Neeraj Malwal <86408295+neerajmasai@users.noreply.github.com>
2022-11-15 12:36:24 +0530

    Update inc/core/common-functions.php
    Co-authored-by: Navanath Bhosale <navanathb@bsf.io>

--- inc/core/common-functions.php
+++ inc/core/common-functions.php
@@ -1627,7 +1627,7 @@ function astra_load_woocommerce_login_form_password_icon() {
  * @since x.x.x
  * @param string  $location container layout for single-post, archives, pages, page meta.
  * @param string $narrow_container_max_width  dynamic container width in px.
- * @return string|null css based on $location and $narrow_container_max_width.
+ * @return string Parsed CSS based on $location and $narrow_container_max_width.
  */
 function astra_narrow_container_width( $location, $narrow_container_max_width ) {
 
