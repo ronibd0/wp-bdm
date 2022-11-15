b099dd931b5059d85c80e1aaa87c4aa8ace5578c
Navanath Bhosale <navanathb@bsf.io>
2022-11-15 16:55:11 +0530

    chore: textdomain support added to free-vs-pro features data

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '85c76622c72858e82e69');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'e0a8d678129ebda6d98c');
