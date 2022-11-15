21a88fb1ed813d12a5cc10e5e65ffcdaa2a9ef58
Navanath Bhosale <navanathb@bsf.io>
2022-11-15 09:38:24 +0530

    fix: RTL fixes done

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '49093037f680df0017e5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1c219ba82ca895051a37');
