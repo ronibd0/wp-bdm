d1b60bc304a3c1d262ac9d6201e010c6742319c3
Navanath Bhosale <navanathb@bsf.io>
2022-11-20 11:55:09 +0530

    chore: build admin changes

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '21a8c7a5c9fc387a0163');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '649146dd993a7736231f');
