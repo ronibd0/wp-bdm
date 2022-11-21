e311d1167cc457bd707f5f1efd00e95a59d25c30
er-roshan <roshan.adhikari.se@gmail.com>
2022-11-21 07:47:33 +0545

    Fixed toggle control style on astra theme part

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f4141e74dac41357d7f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5d98a0894f83c618facc');
--- admin/assets/src/dashboard-app/pages/settings/PreloadLocalFonts.js
+++ admin/assets/src/dashboard-app/pages/settings/PreloadLocalFonts.js
@@ -54,14 +54,22 @@ const PreloadLocalFonts = () => {
 					onChange={ updatePreloadLocalFontsStatus }
 					className={ classNames(
 						enablePreloadLocalFontsStatus ? 'bg-astra' : 'bg-slate-200',
-						'relative inline-flex flex-shrink-0 h-5 w-[2.4rem] items-center border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
+						'group relative inline-flex h-4 w-9 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-astra focus:ring-offset-2'
 					) }
-					>
+				>
+					<span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-white" />
+					<span
+						aria-hidden="true"
+						className={ classNames(
+							enablePreloadLocalFontsStatus ? 'bg-astra' : 'bg-gray-200',
+							'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out'
+						) }
+					/>
 					<span
 						aria-hidden="true"
 						className={ classNames(
 							enablePreloadLocalFontsStatus ? 'translate-x-5' : 'translate-x-0',
-							'pointer-events-none inline-block h-3.5 w-3.5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+							'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out'
 						) }
 					/>
 				</Switch>
