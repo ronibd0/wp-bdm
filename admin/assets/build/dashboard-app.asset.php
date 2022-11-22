c1a71cc63f8ea16ca2bfad3f5f77b2af406eca1f
Navanath Bhosale <navanathb@bsf.io>
2022-11-22 14:39:05 +0530

    fix: pr reported points covered

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ae580421f7ebad431457');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a579df36c087bcb2664e');
