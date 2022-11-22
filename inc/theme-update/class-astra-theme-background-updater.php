0c4a32ae410792fa3b7482d3874508f993f68abd
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-11-22 10:51:05 +0530

    Update inc/theme-update/class-astra-theme-background-updater.php
    Co-authored-by: Navanath Bhosale <navanathb@bsf.io>

--- inc/theme-update/class-astra-theme-background-updater.php
+++ inc/theme-update/class-astra-theme-background-updater.php
@@ -144,7 +144,7 @@ if ( ! class_exists( 'Astra_Theme_Background_Updater' ) ) {
 				'astra_theme_background_updater_3_9_4',
 			),
 			'4.0.1' => array(
-				'astra_theme_background_updater_4_0_1',
+				'astra_theme_background_updater_4_0_0',
 			),
 		);
 
