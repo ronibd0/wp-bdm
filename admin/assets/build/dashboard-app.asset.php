0f2487b9acec47f192a7772ad1725b7fcd151075
Navanath Bhosale <navanathb@bsf.io>
2022-11-28 17:25:20 +0530

    fix: starter templates redirection slug corrected

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c78ebccfb20ccfdb78b2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '469ee363b7a1ff6916bc');
