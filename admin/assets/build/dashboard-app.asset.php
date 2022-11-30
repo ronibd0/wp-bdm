c09207ef3cffbc686e823a3c9c059d96a4738545
Navanath Bhosale <navanathb@bsf.io>
2022-11-30 11:35:16 +0530

    fix: free vs pro redirection link corrected for upgrade to pro

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '469ee363b7a1ff6916bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '87709bd96ce99ce96876');
