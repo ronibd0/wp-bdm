3f54a87b39a47cd9f59697b61db2fa3e65bbafd8
Navanath Bhosale <navanathb@bsf.io>
2022-12-05 17:25:12 +0530

    chore: UTM links added for tracking conversion from dashboard app

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '730d61f5db08f34e90e9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c5eee564ed7bbd8665b4');
