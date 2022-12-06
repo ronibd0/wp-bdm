edc44380cee457a7b78adf252dddb5354c7498cc
Navanath Bhosale <navanathb@bsf.io>
2022-12-06 17:50:18 +0530

    chore: codeanalysis passing

--- inc/blog/blog-config.php
+++ inc/blog/blog-config.php
@@ -138,8 +138,8 @@ function astra_get_dynamic_post_format() {
 	$date_type   = astra_get_option( 'ast-dynamic-single-' . esc_attr( $post_type ) . '-meta-date-type', 'published' );
 	$date_format = apply_filters( 'astra_post_date_format', astra_get_option( 'ast-dynamic-single-' . esc_attr( $post_type ) . '-date-format', get_option( 'date_format' ) ) );
 
-	$published_date = esc_html( get_the_date( $date_format ) );
-	$modified_date  = esc_html( get_the_modified_date( $date_format ) );
+	$published_date = get_the_date( $date_format );
+	$modified_date  = get_the_modified_date( $date_format );
 
 	if ( 'updated' === $date_type ) {
 		$class    = 'updated';
