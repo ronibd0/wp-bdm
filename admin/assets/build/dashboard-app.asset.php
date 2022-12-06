d86596f689eda3f3bc3836e7ad14f46f5e62ebf1
Navanath Bhosale <navanathb@bsf.io>
2022-12-06 10:03:06 +0530

    refactor: if Astra Pro is present then instead of showing upgrade now displaying activate now

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c5eee564ed7bbd8665b4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b7d755cc0a1fcf2e559f');
--- admin/assets/src/dashboard-app/pages/settings/PreloadLocalFonts.js
+++ admin/assets/src/dashboard-app/pages/settings/PreloadLocalFonts.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { useSelector, useDispatch } from 'react-redux';
-import { Switch } from '@headlessui/react'
+import { Switch } from '@headlessui/react';
 import apiFetch from '@wordpress/api-fetch';
 
 function classNames( ...classes ) {
