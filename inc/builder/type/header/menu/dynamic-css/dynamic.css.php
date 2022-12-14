309788c0f4aa4b66cabca9f1a057613400bb2cc0
er-roshan <roshan.adhikari.se@gmail.com>
2022-12-14 10:14:17 +0545

    AST-2069 Removed ignorance comments from the function call

--- inc/builder/type/header/menu/dynamic-css/dynamic.css.php
+++ inc/builder/type/header/menu/dynamic-css/dynamic.css.php
@@ -78,16 +78,12 @@ function astra_hb_menu_dynamic_css( $dynamic_css, $dynamic_css_filtered = '' ) {
 		$menu_resp_bg_color_active_mobile  = ( isset( $menu_resp_bg_color_active['mobile'] ) ) ? $menu_resp_bg_color_active['mobile'] : '';
 
 		// Typography.
-		$menu_font_family = astra_get_option( 'header-' . $_prefix . '-font-family' );
-		$menu_font_size   = astra_get_option( 'header-' . $_prefix . '-font-size' );
-		$menu_font_weight = astra_get_option( 'header-' . $_prefix . '-font-weight' );
-		/** @psalm-suppress UndefinedFunction  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-		$menu_text_transform = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'text-transform' );
-		/** @psalm-suppress UndefinedFunction  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-		$menu_line_height = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'line-height', 'line-height-unit' );
-		/** @psalm-suppress UndefinedFunction  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-		$menu_letter_spacing = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'letter-spacing', 'letter-spacing-unit' );
-		/** @psalm-suppress UndefinedFunction  */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+		$menu_font_family     = astra_get_option( 'header-' . $_prefix . '-font-family' );
+		$menu_font_size       = astra_get_option( 'header-' . $_prefix . '-font-size' );
+		$menu_font_weight     = astra_get_option( 'header-' . $_prefix . '-font-weight' );
+		$menu_text_transform  = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'text-transform' );
+		$menu_line_height     = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'line-height', 'line-height-unit' );
+		$menu_letter_spacing  = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'letter-spacing', 'letter-spacing-unit' );
 		$menu_text_decoration = astra_get_font_extras( astra_get_option( 'header-' . $_prefix . '-font-extras' ), 'text-decoration' );
 
 		$menu_font_size_desktop      = ( isset( $menu_font_size['desktop'] ) ) ? $menu_font_size['desktop'] : '';
