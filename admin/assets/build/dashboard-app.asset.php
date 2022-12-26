34905c112ace66e793846fe505964bf06ca5e958
Navanath Bhosale <navanathb@bsf.io>
2022-12-26 16:13:27 +0530

    fix: versions should be visible on dashboard even if whitelable

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3cf4baee5c05dec4ec73');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ebbf43ef6231967b5ade');
