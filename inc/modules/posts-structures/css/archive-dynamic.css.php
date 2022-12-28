873ef54cfea709a61eb8ca22d674bd161a847791
Navanath Bhosale <navanathb@bsf.io>
2022-12-28 11:57:37 +0530

    fix: bg settings not working for layout-1 in archive section

--- inc/modules/posts-structures/css/archive-dynamic.css.php
+++ inc/modules/posts-structures/css/archive-dynamic.css.php
@@ -168,9 +168,9 @@ function astra_post_archive_structure_dynamic_css( $dynamic_css, $dynamic_css_fi
 			if ( class_exists( 'WooCommerce' ) && 'product' === $current_post_type ) {
 				if ( 'custom' === $background_type ) {
 					$custom_background                                 = astra_get_option( 'ast-dynamic-archive-' . $current_post_type . '-banner-custom-bg' );
-					$css_output_desktop[ 'body.archive ' . $selector ] = astra_get_responsive_background_obj( $custom_background, 'desktop' );
-					$css_output_tablet[ 'body.archive ' . $selector ]  = astra_get_responsive_background_obj( $custom_background, 'tablet' );
-					$css_output_mobile[ 'body.archive ' . $selector ]  = astra_get_responsive_background_obj( $custom_background, 'mobile' );
+					$css_output_desktop[ '.archive .ast-archive-description' ] = astra_get_responsive_background_obj( $custom_background, 'desktop' );
+					$css_output_tablet[ '.archive .ast-archive-description' ]  = astra_get_responsive_background_obj( $custom_background, 'tablet' );
+					$css_output_mobile[ '.archive .ast-archive-description' ]  = astra_get_responsive_background_obj( $custom_background, 'mobile' );
 				} else {
 					/**
 					 * @psalm-suppress RedundantCondition
@@ -184,13 +184,13 @@ function astra_post_archive_structure_dynamic_css( $dynamic_css, $dynamic_css_fi
 					$overlay_color = astra_get_option( 'ast-dynamic-archive-' . $current_post_type . '-banner-featured-overlay', '' );
 					$taxonomy      = $wp_query->get_queried_object();
 					if ( is_callable( 'is_shop' ) && is_shop() && '' !== $overlay_color ) {
-						$css_output_desktop[ $selector . '[data-banner-background-type="featured"]' ]['background'] = $overlay_color;
+						$css_output_desktop[ '.archive .ast-archive-description' ]['background'] = $overlay_color;
 					}
 					if ( ! empty( $taxonomy->term_id ) ) {
 						$thumbnail_id   = get_term_meta( $taxonomy->term_id, 'thumbnail_id', true );
 						$feat_image_src = wp_get_attachment_url( $thumbnail_id );
 						if ( $feat_image_src ) {
-							$css_output_desktop[ $selector . '[data-banner-background-type="featured"]' ] = array(
+							$css_output_desktop[ '.archive .ast-archive-description' ] = array(
 								'background'            => 'url( ' . esc_url( $feat_image_src ) . ' )',
 								'background-repeat'     => 'no-repeat',
 								'background-attachment' => 'scroll',
@@ -198,8 +198,8 @@ function astra_post_archive_structure_dynamic_css( $dynamic_css, $dynamic_css_fi
 								'background-size'       => 'cover',
 							);
 							if ( '' !== $overlay_color ) {
-								$css_output_desktop[ $selector . '[data-banner-background-type="featured"]' ]['background']            = 'url( ' . esc_url( $feat_image_src ) . ' ) ' . $overlay_color;
-								$css_output_desktop[ $selector . '[data-banner-background-type="featured"]' ]['background-blend-mode'] = 'multiply';
+								$css_output_desktop[ '.archive .ast-archive-description' ]['background']            = 'url( ' . esc_url( $feat_image_src ) . ' ) ' . $overlay_color;
+								$css_output_desktop[ '.archive .ast-archive-description' ]['background-blend-mode'] = 'multiply';
 							}
 						}
 					}
