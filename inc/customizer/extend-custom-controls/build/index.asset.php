fd61b7a17dd4702e1b3585fea047313fb124eea9
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2022-12-30 15:25:44 +0530

    fix : customizer link control issue

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '2074134d3aa904b57ed2f5c847c2bd57');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'a502ab14a003471027471e694d43cbde');
