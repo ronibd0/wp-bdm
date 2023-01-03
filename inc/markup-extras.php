84665e176c3a9ddd1bb8981f3db640c351513836
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2023-01-03 10:58:57 +0530

    Update markup-extras.php

--- inc/markup-extras.php
+++ inc/markup-extras.php
@@ -1679,7 +1679,6 @@ add_filter( 'astra_entry_header_class', 'astra_entry_header_class_custom', 1, 99
  * Hide elementor title.
  *
  * @param array $classes Array of elementor edit mode check.
- * @return number Post ID. 
  *
  * @since x.x.x
  */
