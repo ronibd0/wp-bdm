a60279fe27a9e32ebb25ac6c4dcebaca01a5a525
Yogesh Sambare <bloggingscan@gmail.com>
2023-01-03 09:37:24 +0530

    fix:phpcs errors

--- inc/markup-extras.php
+++ inc/markup-extras.php
@@ -1679,11 +1679,12 @@ add_filter( 'astra_entry_header_class', 'astra_entry_header_class_custom', 1, 99
  * Hide elementor title.
  *
  * @param array $classes Array of elementor edit mode check.
+ * @param int   $post_id  Post ID.
  *
  * @since x.x.x
  */
 function astra_entry_header_class_custom( $classes ) {
-	$post_id   = astra_get_post_id(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	$post_id   = astra_get_post_id(); 
 	$edit_mode = get_post_meta( $post_id, '_elementor_edit_mode', true ); 
 
 	if ( $edit_mode && $edit_mode == 'builder' ) {
