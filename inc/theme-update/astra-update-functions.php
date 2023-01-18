da277ade49e3baafb69161e22787713321a3182a
neerajmasai <neerajmalwal10@gmail.com>
2023-01-18 12:43:19 +0530

    chore: code refactor

--- inc/theme-update/astra-update-functions.php
+++ inc/theme-update/astra-update-functions.php
@@ -1003,8 +1003,8 @@ function astra_theme_background_updater_4_0_0() {
 function astra_theme_background_updater_4_0_2() {
 	$theme_options = get_option( 'astra-settings', array() );
 	if ( isset( $theme_options['blog-single-meta'] ) && in_array( 'read-time', $theme_options['blog-single-meta'] ) ) {
-		if( ! isset( $theme_options['v4.0.2-flag'] ) && is_array( $theme_options[ 'ast-dynamic-single-post-metadata' ] ) && ! in_array( 'read-time', $theme_options[ 'ast-dynamic-single-post-metadata' ] ) ) {
-			$theme_options[ 'ast-dynamic-single-post-metadata' ][] = 'read-time';
+		if( ! isset( $theme_options['v4.0.2-flag'] ) && is_array( $theme_options['ast-dynamic-single-post-metadata'] ) && ! in_array( 'read-time', $theme_options['ast-dynamic-single-post-metadata'] ) ) {
+			$theme_options['ast-dynamic-single-post-metadata'][] = 'read-time';
 			$theme_options['v4.0.2-flag'] = false;
 			update_option( 'astra-settings', $theme_options );
 		}
