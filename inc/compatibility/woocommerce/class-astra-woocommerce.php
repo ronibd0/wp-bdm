f12328f371a4c3690a8bdd578cd72ecf9addfc58
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-01-27 16:08:59 +0530

    feat: added new filter

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -1624,9 +1624,9 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			}
 
 			// Backward compatibility for old users for h2 tag global fonts.
-			if ( astra_get_option( 'woo-global-h2-flag', false ) ) {
+			if ( apply_filters( 'astra_theme_woocommerce_h2_font', astra_get_option( 'woo-global-h2-flag', false ) ) ) {
 				
-				$css_desktop_output['..woocommerce .up-sells h2, .woocommerce .related.products h2, .woocommerce .woocommerce-tabs h2'] = array(
+				$css_desktop_output['.woocommerce .up-sells h2, .woocommerce .related.products h2, .woocommerce .woocommerce-tabs h2'] = array(
 					'font-size' => '1.5rem',
 				);
 
