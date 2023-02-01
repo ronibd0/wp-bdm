40e54bfad302bc73447ad3a5fa421ef904e0bea6
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-02-01 19:27:38 +0530

    chor : ran psalm

--- inc/compatibility/edd/edd-common-functions.php
+++ inc/compatibility/edd/edd-common-functions.php
@@ -211,10 +211,17 @@ if ( ! function_exists( 'astra_edd_terms_list' ) ) {
 			<?php 
 				$get_term_link = get_term_link( $term, $taxonomy_name );
 				$term_link     = $get_term_link ? $get_term_link : '';
-				/** @psalm-suppress PossiblyInvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
+			// @codingStandardsIgnoreStart
+			/**
+			 * @psalm-suppress PossiblyInvalidArgument
+			 */
 			?>
 			<a href="<?php echo esc_url( $term_link ); ?>" title="<?php echo esc_attr( $term->name ); ?>"> <?php echo esc_html( $term->name ); ?> </a>
-		<?php endforeach; ?>
+		<?php
+		// @codingStandardsIgnoreEnd
+		endforeach; 
+		?>
 	</div>
 		<?php
 	}
