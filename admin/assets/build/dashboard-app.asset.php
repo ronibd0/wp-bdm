da0c7044c5234852af5fabbb4740f579a0fcebb1
Navanath Bhosale <navanathb@bsf.io>
2023-02-02 15:03:22 +0530

    refactor: starter templates admin nav menu link compatible with whitelabelled

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b86f31ef7a73e5ff3a3d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '7fa8a13fec12e314c8d0');
